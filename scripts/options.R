91a99b8fc363c52d697f1e3c210e4b24643141b1
Ivo Kwee <ivo.kwee@gmail.com>
2019-05-06 20:26:46 +0200

    sync
    Former-commit-id: aa8af085b083fcacac6a1741eb33ec75979460d3 [formerly 5166e164535f8cccb068357099b127e81eb87c19]
    Former-commit-id: dd09072c101f135dabb55f3392ee89ff67aa65e0

--- scripts/options.R
+++ scripts/options.R
@@ -3,15 +3,16 @@
 FILES="../lib/"
 RDIR="../R/"
 
-PROCESS.DATA=1
-DIFF.EXPRESSION=1
-COMPUTE.EXTRA=1
+PROCESS.DATA    = 1
+DIFF.EXPRESSION = 1
+COMPUTE.EXTRA   = 1
 
-SMALL=4000
+SMALL       = 4000
+MAX.GENES   = 4000
+MAX.SAMPLES = 1000
 
-USER.GENETEST.METHODS=c("trend.limma","edger.qlf","deseq2.wald")
-##USER.GENETEST.METHODS=c("trend.limma","edger.qlf","edger.lrt")
-USER.GENESETTEST.METHODS=c("gsva","camera","fgsea")
+USER.GENETEST.METHODS    = c("trend.limma","edger.qlf","deseq2.wald")
+USER.GENESETTEST.METHODS = c("gsva","camera","fgsea")
 
-BATCH.CORRECT = FALSE
+##BATCH.CORRECT = FALSE
 EXT = ""
