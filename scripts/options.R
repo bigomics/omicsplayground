52e01d4e4cc86843b26ee70c8a3a6bb8b10d733e
Ivo Kwee <ivo.kwee@gmail.com>
2019-05-10 11:42:46 +0200

    update options.R

--- scripts/options.R
+++ scripts/options.R
@@ -8,8 +8,7 @@ DIFF.EXPRESSION = 1
 COMPUTE.EXTRA   = 1
 
 SMALL       = 8000
-MAX.GENES   = 4000
-MAX.GENES   = 9999
+MAX.GENES   = 8000
 MAX.SAMPLES = 1000
 
 USER.GENETEST.METHODS    = c("trend.limma","edger.qlf","deseq2.wald")
