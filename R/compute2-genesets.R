d0e82e6125f3b9aeb7480c9687094ee4e3831f19
Ivo Kwee <ivo.kwee@gmail.com>
2019-05-27 04:26:52 +0200

    prepare for multi-omics

---
+++ R/compute2-genesets.R
@@ -0,0 +1,189 @@
+## library(limma)
+## source("../R/gx-heatmap.r")
+## source("../R/gx-limma.r")
+## source("../R/gx-util.r")
+## source("../R/gset-fisher.r")
+## source("../R/gset-gsea.r")
+## source("../R/gset-meta.r")
+
+SAVE.PARAMS <- ls()
+
+compute.testGenesets <- function(ngs, max.features=1000,
+                                 test.methods = c("gsva","camera","fgsea"))
+{
+    
+    ##-----------------------------------------------------------
+    ## Load gene sets
+    ##-----------------------------------------------------------    
+    pp <- rownames(ngs$counts)
+    is.mouse = (mean(grepl("[a-z]",gsub(".*:|.*\\]","",pp))) > 0.8)
+    is.mouse
+    if(is.mouse) {
+        cat("Loading mouse gene sets...\n")
+        load(file=file.path(FILES,"gmt-all-mouse.rda"))
+    } else {
+        cat("Loading human gene sets...\n")
+        load(file=file.path(FILES,"gmt-all.rda"))
+    }
+    table(sub(":.*","",names(gmt.all)))
+    summary(sapply(gmt.all,length))
+    
+    ##-----------------------------------------------------------
+    ## Filter gene sets
+    ##-----------------------------------------------------------
+    cat("Filtering gene sets...\n")
+    
+    require(Matrix)
+    require(org.Hs.eg.db)
+    ##GENE.TITLE = unlist(as.list(org.Hs.egGENENAME))
+    ##genes = head(as.character(unlist(as.list(org.Hs.egSYMBOL))),1000)
+    genes = unique(as.character(ngs$genes$gene_name))
+    gmt.all = mclapply(gmt.all, function(gs) intersect(gs, genes))
+    table(sub(":.*","",names(gmt.all)))
+    
+    ## filter gene sets on size
+    gmt.size = sapply(gmt.all,length)
+    summary(gmt.size)
+    gmt.all = gmt.all[which(gmt.size >= 15 & gmt.size <= 1e3)]
+    gmt.size = sapply(gmt.all,length)
+    gmt.all = gmt.all[order(-gmt.size)]
+    summary(gmt.size)
+    gmt.all = gmt.all[!duplicated(names(gmt.all))]
+    length(gmt.all)
+    table(sub(":.*","",names(gmt.all)))
+    
+    ##-----------------------------------------------------------
+    ## create the full GENE matrix (always collapsed by gene)
+    ##-----------------------------------------------------------
+    single.omics <- !any(grepl("\\[",rownames(ngs$counts)))
+    single.omics
+    if(single.omics) {
+        X <- ngs$counts
+    } else {
+        data.type <- gsub("\\[|\\].*","",rownames(ngs$counts))
+        jj <- which(data.type %in% c("gx","mrna"))
+        length(jj)
+        if(length(jj)==0) {
+            stop("FATAL. could not find gx/mrna values.")
+        }
+        X <- ngs$counts[jj,]
+    }
+    xgenes = as.character(ngs$genes[rownames(X),"gene_name"])
+    X <- apply(X, 2, function(x) tapply(x, xgenes, sum))
+    dim(X)
+    X <- edgeR::cpm(X, log=TRUE )
+    X <- X[which(apply(X,1,sd)>0),,drop=FALSE]
+    X <- X[!(rownames(X) %in% c(NA,""," ") ),,drop=FALSE]
+    X <- limma::normalizeQuantiles(X)
+    dim(X)
+    
+    ##-----------------------------------------------------------
+    ## create the GENESETxGENE matrix
+    ##-----------------------------------------------------------
+    cat("Building gene set matrix...\n")
+    ##GMT = sapply( gmt.all, function(s) 1*(rownames(X) %in% s))
+    ##GMT = Matrix(GMT, sparse=TRUE)
+    genes <- sort(unique(xgenes))
+    GMT <- gmt2mat.nocheck(gmt.all[], bg=genes)  ## in gset-gsea.r
+    dim(GMT)
+    dim(X)
+    table(xgenes %in% rownames(GMT))
+    table(names(gmt.all) %in% colnames(GMT))
+    GMT[1:4,1:4]
+    
+    ## align GMT to X (or ngs$X??)
+    GMT <- GMT[rownames(X),names(gmt.all)]
+    summary(Matrix::colSums(GMT))
+    dim(GMT)
+    
+    ##-----------------------------------------------------------
+    ## Prioritize gene sets by fast rank-correlation
+    ##-----------------------------------------------------------
+    
+    if(is.null(max.features)) max.features <- 20000
+    if(max.features < 0) max.features <- 20000
+    max.features
+    
+    if(max.features > 0) {
+        require(limma)
+        ## Reduce gene sets by selecting top varying genesets. We use the
+        ## very fast sparse rank-correlation for approximate single sample
+        ## geneset activation.
+        cX <- X - rowMeans(X, na.rm=TRUE)
+        gsetX = qlcMatrix::corSparse( GMT[,], apply( cX[,],2,rank) )
+        gsetX = limma::normalizeQuantiles(gsetX) ##???
+        grp <- ngs$samples$group
+        gsetX.bygroup <- t(apply(gsetX,1,function(x) tapply(x,grp,mean)))
+        sdx <- apply(gsetX.bygroup,1,sd)
+        names(sdx) <- colnames(GMT)
+        jj = head(order(-sdx), max.features) 
+        must.include <- "hallmark|kegg|^go|^celltype"
+        jj = unique( c(jj, grep(must.include,colnames(GMT),ignore.case=TRUE)))
+        jj = jj[order(colnames(GMT)[jj])]
+        length(jj)
+        GMT = GMT[,jj]
+        gmt.all = gmt.all[colnames(GMT)]
+    }
+    dim(GMT)
+    
+    sum(duplicated(rownames(GMT)))
+    dim(X)
+    dim(GMT)
+    ngs$GMT <- GMT
+    ngs$gmt.all <- gmt.all
+    
+    ##-----------------------------------------------------------
+    ## get design and contrast matrix
+    ##-----------------------------------------------------------
+    design = ngs$model.parameters$design
+    contr.matrix = ngs$model.parameters$contr.matrix
+    ##contr.matrix
+    ##exp.matrix = (design %*% contr.matrix)
+    
+    all.gset.methods=c("fisher","ssgsea","gsva", "spearman", "camera", "fry",
+                       "fgsea","gsea.permPH","gsea.permGS","gseaPR")
+    ##test.methods = c("fisher","fgsea")
+    ## test.methods = c("fisher","gsva","ssgsea","spearman","camera","fry","fgsea") ## no GSEA
+    ## if(!is.null(USER.GENESETTEST.METHODS)) test.methods = USER.GENESETTEST.METHODS
+    ## ##if(test.methods[1]=="*") test.methods = all.gset.methods
+    test.methods
+    
+    ##-----------------------------------------------------------
+    ## Run methods
+    ##-----------------------------------------------------------
+    cat(">>> Testing gene sets with methods:",test.methods,"\n")
+    test.methods
+    
+    Y <- ngs$samples
+    ##gmt=ngs$gmt.all[1:100]
+    gmt=ngs$gmt.all[]
+    gc()
+    
+    gset.meta = gset.fitContrastsWithAllMethods(
+        gmt = ngs$gmt.all, X = X, Y = Y, design=design, ## genes=GENES,
+        contr.matrix=contr.matrix, methods=test.methods,
+        mc.threads=1, mc.cores=NULL, batch.correct=TRUE )
+    
+    print(gset.meta$timings)
+    
+    rownames(gset.meta$timings) <- paste0("[test.genesets]",rownames(gset.meta$timings))
+    ngs$timings <- rbind(ngs$timings, gset.meta$timings)
+    ngs$gset.meta <- gset.meta
+    ngs$gsetX = ngs$gset.meta$matrices[["meta"]]  ## META??!
+    
+    ##-----------------------------------------------------------------------
+    ##------------------------ clean up -------------------------------------
+    ##-----------------------------------------------------------------------
+    
+    ## remove large outputs... (uncomment if needed)
+    ngs$gset.meta$outputs <- NULL
+    
+    remove(X)
+    remove(Y)
+
+    return(ngs)
+}
+
+
+##rm(list=setdiff(ls(),SAVE.PARAMS))
+
---
+++ scripts/pgx2-tcga.R
@@ -0,0 +1,197 @@
+##rm(list=setdiff(ls(),run.param))
+library(knitr)
+library(limma)
+library(edgeR)
+library(RColorBrewer)
+library(gplots)
+library(matrixTests)
+library(kableExtra)
+library(knitr)
+
+source("../R/gx-heatmap.r")
+source("../R/gx-limma.r")
+source("../R/gx-util.r")
+source("../R/ngs-cook.r")
+source("../R/ngs-fit.r")
+source("../R/gset-fisher.r")
+source("../R/gset-gsea.r")
+source("../R/gset-meta.r")
+source("../R/pgx-graph.R")
+source("../R/pgx-functions.R")
+source("../R/ngs-functions.R")
+
+source("options.R")
+MAX.GENES
+BATCH.CORRECT=TRUE
+
+## run all available methods 
+USER.GENETEST.METHODS = "*"
+USER.GENESETTEST.METHODS = c("gsva","fisher","camera","fgsea","fry","spearman")
+
+rda.file="../pgx/TCGA-brca_pub.pgx"
+rda.file
+
+##load(file=rda.file, verbose=1)
+ngs <- list()  ## empty object
+ngs$datatype = "multi-omics"
+ngs$description = "BRCA-TCGA public data set (from cBioPortal)."
+
+## READ/PARSE DATA
+if(PROCESS.DATA) {
+
+    ## ##############################################################
+    ## Old OMX data
+    ##
+
+    load("~/OMX/shiny-omx/omxdata/tcga/brca_tcga_pub-omx.rda",verbose=1)
+    names(omx)
+    names(omx$level[[1]]$mat)
+    lapply(omx$level[[1]]$mat,dim)
+    mat <- omx$level[[1]]$mat[1:5]
+    names(mat)
+
+    ## impute missing values
+    sapply(mat,function(x) sum(is.na(x)))
+    mat <- lapply(mat, imputeMedian)
+    sapply(mat,function(x) sum(is.na(x)))
+    
+    samples <- Reduce(intersect, sapply(mat[1:5],colnames))
+    ##samples <- Reduce(union, sapply(mat[1:5],colnames))
+    samples
+    names(mat)
+    lapply(mat,dim)
+    for(i in 1:length(mat)) {
+        mat[[i]] <- mat[[i]][,match(samples,colnames(mat[[i]]))]
+        colnames(mat[[i]]) <- samples
+        prefix <- paste0("[",names(mat)[i],"]")
+        rownames(mat[[i]]) <- paste0(prefix,rownames(mat[[i]]))
+    }
+    head(rownames(mat[[2]]))        
+    X <- do.call(rbind, mat)
+    dim(X)
+    
+    samples <- sort(intersect(samples,rownames(omx$pheno)))
+    sampleTable <- omx$pheno[samples,]
+    X <- X[,samples]
+    
+    ##-------------------------------------------------------------------
+    ## gene annotation
+    ##-------------------------------------------------------------------
+    require(org.Hs.eg.db)
+    GENE.TITLE = unlist(as.list(org.Hs.egGENENAME))
+    gene.symbol = unlist(as.list(org.Hs.egSYMBOL))
+    names(GENE.TITLE) = gene.symbol
+    head(GENE.TITLE)
+    gene <- sub("\\[.*\\]","",rownames(X))
+    gene_title <- GENE.TITLE[gene]
+
+    ## get chromosome locations
+    chrloc = as.list(org.Hs.egCHRLOC)
+    names(chrloc) = gene.symbol
+    chrloc <- chrloc[gene]
+    ##loc <- abs(as.numeric(unlist(sapply(chrloc, "[", 1))))
+    chrom <- sapply(chrloc, function(s) names(s)[1])
+    chrom[sapply(chrom,is.null)] <- NA
+    chrom <- as.vector(unlist(chrom))
+    
+    dtype <- gsub("\\[|\\].*","",rownames(X))
+    genes = data.frame(
+        data_type = dtype,
+        gene_name=gene,
+        gene_title=gene_title,
+        chr=chrom)
+    ##genes = apply(genes,2,as.character)
+    head(genes)
+    rownames(genes) = rownames(X)
+    
+    ##-------------------------------------------------------------------
+    ## Now create an DGEList object  (see tximport Vignette)
+    ##-------------------------------------------------------------------
+    ngs$counts  <- as.matrix(2**X)  ## treat as counts
+    ngs$samples <- sampleTable
+    ngs$genes   <- genes
+    ##lib.size <- colSums(data$counts / 1e6)  ## get original summed intensity as lib.size
+    ##ngs$samples$batch <- NULL  ##???
+    ##ngs$samples$batch <- as.integer(lib.size2)
+
+    ## tagged rownames
+    ##row.id = paste0("tag",1:nrow(ngs$genes),":",ngs$genes[,"gene_name"])  
+    ##row.id = ngs$genes[,"gene_name"]
+    ##rownames(ngs$genes) = rownames(ngs$counts) = row.id
+    ##names(ngs)
+
+    ##-------------------------------------------------------------------
+    ## collapse multiple row for genes by summing up counts
+    ##-------------------------------------------------------------------
+    sum(duplicated(rownames(X)))
+    ## x1 = apply(ngs$counts, 2, function(x) tapply(x, ngs$genes$gene_name, sum))
+    ## ngs$genes = ngs$genes[match(rownames(x1),ngs$genes$gene_name),]
+    ## ngs$counts = x1
+    ## dim(x1)
+    ## rownames(ngs$genes) = rownames(ngs$counts) = rownames(x1)
+    ## remove(x1)
+    ##ngs <- ngs.collapseByGene(ngs)
+    ##dim(ngs$counts)
+        
+    ##-------------------------------------------------------------------
+    ## Pre-calculate t-SNE for and get clusters early so we can use it
+    ## for doing differential analysis.
+    ##-------------------------------------------------------------------
+    ngs <- pgx.clusterSamples(ngs, skipifexists=FALSE, prefix="C",
+                              perplexity=5)
+    head(ngs$samples)
+    
+    ##-------------------------------------------------------------------
+    ## save
+    ##-------------------------------------------------------------------    
+    rda.file
+    save(ngs, file=rda.file)
+}
+
+
+if(DIFF.EXPRESSION) {
+
+    load(file=rda.file, verbose=1)
+    
+    head(ngs$samples)
+    ngs$samples$group <- as.character(ngs$samples$HER2_STATUS)
+    levels = unique(ngs$samples$group)
+    levels
+    colnames(ngs$samples)
+    
+    contr.matrix <- makeDirectContrasts(
+        ngs$samples[,c("PR_STATUS","ER_STATUS","HER2_STATUS")],
+        ref = c("0","0","0"))
+    dim(contr.matrix)
+    head(contr.matrix)
+    ##contr.matrix = contr.matrix[,1:3]
+    
+    source("../R/compute2-genes.R")
+    test.methods = c("trend.limma","ttest.welch","ttest")
+    ngs <- compute.testGenes2(
+        ngs, contr.matrix, max.features=1000,
+        test.methods=test.methods)
+    head(ngs$gx.meta$meta[[1]])
+
+    source("../R/compute2-genesets.R")
+    test.methods = c("gsva","camera","fgsea")
+    ngs <- compute.testGenesets(
+        ngs, max.features=1000,
+        test.methods=test.methods)
+    head(ngs$gset.meta$meta[[1]])
+    
+    source("../R/compute-extra.R")
+    
+}
+
+## save
+rda.file
+ngs.save(ngs, file=rda.file)
+
+
+
+
+
+
+
+
