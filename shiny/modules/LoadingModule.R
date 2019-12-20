9d333137aebd34654105fa527bedc1e3466eb281
Ivo Kwee <ivo.kwee@gmail.com>
2019-12-20 02:06:18 +0100

    corrected social buttons
    Former-commit-id: 5671e9a4c35a7d56e3fea83e0d43fa790aa4f776 [formerly 4e36495593e5229861be7f5fc1e5c00f51e9677c]
    Former-commit-id: a476f9a79e0dfa0eeb30a59937a3973e4f8e1029

--- shiny/modules/LoadingModule.R
+++ shiny/modules/LoadingModule.R
@@ -4,6 +4,7 @@ LoadingInputs <- function(id) {
     tagList(
         uiOutput(ns("description")),
         uiOutput(ns("inputsUI"))
+        ##uiOutput(ns("socialButtons"))
     )
 }
 
@@ -22,6 +23,7 @@ LoadingUI <- function(id) {
         )
     )
 }
+
 
 LoadingModule <- function(input, output, session, hideUserMode=FALSE)
 {
@@ -42,32 +44,6 @@ LoadingModule <- function(input, output, session, hideUserMode=FALSE)
                                   header=TRUE, stringsAsFactors=FALSE)
         LOGIN_AUTHENTICATION = "password"
     }
-
-    ##-----------------------------------------------------------------------------
-    ## Row of social media buttons
-    ##-----------------------------------------------------------------------------
-    social_buttons <- function() {
-        div(
-            id="social-buttons",
-            tagList(
-                tipify( tags$a( href="https://omicsplayground.readthedocs.io", icon("book"), target="_blank"),
-                       "Read our online documentation at Read-the-docs", placement="top"),
-                tipify( tags$a( href="https://www.youtube.com/watch?v=_Q2LJmb2ihU&list=PLxQDY_RmvM2JYPjdJnyLUpOStnXkWTSQ-",
-                               icon("youtube"), target="_blank"),
-                       "Watch our tutorials on YouTube", placement="top"),
-                tipify( tags$a( href="https://github.com/bigomics/omicsplayground",
-                               icon("github"), target="_blank"),
-                       "Get the source code or report a bug at GitHub", placement="top"),
-                tipify( tags$a( href="https://hub.docker.com/r/bigomics/omicsplayground",
-                               icon("docker"), target="_blank"),
-                       "Pull our docker from Docker", placement="top"),
-                tipify( tags$a( href="https://groups.google.com/d/forum/omicsplayground",
-                               icon("users"), target="_blank"),
-                       "Get help at our user forum", placement="top")            
-            )
-        )
-    }
-    social_buttons()
 
     ##-----------------------------------------------------------------------------
     ## Show current dataset on each page
