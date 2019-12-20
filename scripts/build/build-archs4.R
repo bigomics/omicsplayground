23755cb2b9a8187e7987c280abcefa1add39ea0b
Ivo Kwee <ivo.kwee@gmail.com>
2019-12-20 18:37:25 +0100

    changes
    Former-commit-id: 77fcfe8a2956ae78d759e4d5201e63cf7dad96be [formerly ed5c849826e9b81945ee55d07a968dc924d78052]
    Former-commit-id: acf79e91276d9a10238ad997c003593cdef84943

--- scripts/build/build-archs4.R
+++ scripts/build/build-archs4.R
@@ -6,27 +6,30 @@
 ##BiocManager::install("denalitherapeutics/archs4")
 library("archs4")
 
-archs4dir <- "~/.archs4data"
+archs4dir <- "~/archs4data"
 archs4_local_data_dir_create(archs4dir)
 
 cwd = getwd()
+cwd
 setwd(archs4dir)
 system("wget https://s3.amazonaws.com/mssm-seq-matrix/human_matrix.h5")
-##system("wget https://s3.amazonaws.com/mssm-seq-matrix/human_hiseq_transcript_v2.h5")
+system("wget https://s3.amazonaws.com/mssm-seq-matrix/human_hiseq_transcript_v2.h5")
 system("wget https://s3.amazonaws.com/mssm-seq-matrix/mouse_matrix.h5")
-##system("wget https://s3.amazonaws.com/mssm-seq-matrix/mouse_hiseq_transcript_v2.h5")
+system("wget https://s3.amazonaws.com/mssm-seq-matrix/mouse_hiseq_transcript_v2.h5")
 ##system("wget ftp://ftp.ensembl.org/pub/release-90/gtf/homo_sapiens/Homo_sapiens.GRCh38.90.gtf.gz")
 ##system("wget ftp://ftp.ensembl.org/pub/release-90/gtf/mus_musculus/Mus_musculus.GRCm38.90.gtf.gz")
 
 setwd(cwd)
 
 ## create_augmented_feature_info(archs4dir)
-archs4_local_data_dir_validate()
+archs4_local_data_dir_validate(datadir=archs4dir)
 
 
+## EXAMPLES
+##
 
 library(archs4)
-a4 <- Archs4Repository()
+a4 <- Archs4Repository(archs4dir)
 ids <- c('GSE89189', 'GSE29943', "GSM1095128", "GSM1095129", "GSM1095130")
 sample.info <- sample_info(a4, ids)
 head(sample.info)
