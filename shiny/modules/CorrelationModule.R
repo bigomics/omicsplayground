04dd0e0c3f8d5fcc86f82765f2d6f44db1381061
Ivo Kwee <ivo.kwee@gmail.com>
2020-01-07 23:32:01 +0100

    add info text to correlation module

--- shiny/modules/CorrelationModule.R
+++ shiny/modules/CorrelationModule.R
@@ -8,13 +8,24 @@ CorrelationInputs <- function(id) {
 
 CorrelationUI <- function(id) {
     ns <- NS(id)  ## namespace
-    fillCol(
+    ui <- fillCol(
         height = 750,
         tabsetPanel(
             id = ns("tabs1"),
             tabPanel("Correlation",uiOutput(ns("corAnalysis_UI")))
         )
-    )
+    )    
+    if(DEV.VERSION) {
+        ui <- fillCol(
+            height = 750,
+            tabsetPanel(
+                id = ns("tabs1"),
+                tabPanel("Correlation",uiOutput(ns("corAnalysis_UI"))),
+                tabPanel("Functional",uiOutput(ns("corFunctional_UI")))
+            )
+        )
+    }
+    ui
 }
 
 CorrelationModule <- function(input, output, session, env)
@@ -65,6 +76,25 @@ between genes and find coregulated modules."
         )
     })
 
+
+    corfunctional_caption = "<b>Cluster annotation.</b> Functional annotation of the gene clusters as defined by the hierchical clustered heatmap. <b>(a)</b> Top ranked annotation features (by correlation) for each gene cluster. Length of the bar corresponds to its average correlation. <b>(b)</b> Table of average correlation values of annotation features, for each gene cluster."
+
+    output$corFunctional_UI <- renderUI({
+        fillRow(
+            flex = c(1,1),
+            height = fullH,
+            fillCol(
+                flex = c(1,0.05,0.75,NA),
+                plotWidget(ns("corfunctional_plots")),
+                br(),
+                tableWidget(ns("corfunctional_table")),
+                div(HTML(corfunctional_caption), class="caption")
+            ),
+            fillCol()
+        )
+    })
+    ##outputOptions(output, "hm_annotateUI", suspendWhenHidden=FALSE) ## important!!!
+    
     ##================================================================================
     ##========================= INPUTS UI ============================================
     ##================================================================================
@@ -160,7 +190,7 @@ between genes and find coregulated modules."
         res <- pgx.computePartialCorrelationAroundGene(
             X, gene, method=methods, nmax=100, fast=FALSE)    
 
-        rho1 <- min(abs(head(res$cor[gene,-1],18)))
+        rho1 <- min(abs(head(res$cor[gene,-1],20)))
         updateSliderInput(session, "cor_graph_threshold", value=rho1)
 
         res
@@ -202,12 +232,14 @@ between genes and find coregulated modules."
                      inline=TRUE )
     )
 
+    cor_barplot.info = "<b>Top correlated genes.</b> Highest correlated genes in respect to the selected gene. The height of the bars correspond to the Pearson correlation value. The dark grey bars correspond to the 'partial correlation' which essentially corrects the correlation value for indirect effects and tries to estimate the amount of direct interaction."
+    
     callModule(
         plotModule,
         id = "cor_barplot", 
         func = cor_barplot.PLOTFUN,
         func2 = cor_barplot.PLOTFUN,        
-        ##info.text = topEnriched_text,
+        info.text = cor_barplot.info,
         options = cor_barplot.opts,
         title = "Top correlation", label = "a",
         caption2 = "Top correlated features.",
@@ -246,16 +278,18 @@ between genes and find coregulated modules."
         selectInput(ns('cor_graph_layout'),'layout:', choices=GRAPH.LAYOUTS)
         )
 
+    cor_graph_info <- "</b>Correlation network.</b> Correlation graph centered on selected gene with top most correlated features. Red edges correspond to negative (marginal) correlation, grey edges to positive correlation. Width of the edges is proportional to the absolute partial correlation of the gene pair."
+    
     callModule(
         plotModule,
         id = "cor_graph", label = "b",
         func = cor_graph.PLOTFUN,
         func2 = cor_graph.PLOTFUN,        
-        ##info.text = topEnriched_text,
+        info.text = cor_graph_info,
         options = cor_graph.opts,
         title = "Correlation network",
-        ##caption = topEnriched_caption
-        caption2 = "</b>Correlation network.</b> Correlation graph centered on selected gene with top most correlated features. Red edges correspond to negative (marginal) correlation, grey edges to positive correlation. Width of the edges is proportional to the absolute partial correlation of the gene pair.",
+        ## caption = topEnriched_caption
+        caption2 = cor_graph_info,
         ##pdf.width = 14, pdf.height = 4, 
         height = c(0.45*fullH,720),
         width = c('auto',1000),
@@ -290,12 +324,14 @@ between genes and find coregulated modules."
 
     cor_scatter.opts <- tagList()
 
+    cor_scatter.info = "<b>Correlation scatter plots.</b> The plots show the pairwise scatter plots for the expression values of the gene pairs across the samples. The red line correspond to the (linear) regression fit."
+    
     callModule(
         plotModule,
         id = "cor_scatter", label = "c",
         func = cor_scatter.PLOTFUN,
         func2 = cor_scatter.PLOTFUN,        
-        ##info.text = topEnriched_text,
+        info.text = cor_scatter.info,
         options = cor_scatter.opts,
         title = "Correlation scatter plots",
         ##caption = topEnriched_caption
@@ -305,6 +341,160 @@ between genes and find coregulated modules."
         res = c(80,85)
     )
 
+    
+    ##--------------------------------------------------------------------------------
+    ## Functional correlation
+    ##--------------------------------------------------------------------------------
+    getFunctionalCorrelation <- reactive({
+        ngs <- inputData()
+        gene = "CD4"
+        gene <- input$cor_gene
+        gx <- ngs$X[gene,]
+        rho <- cor( t(ngs$gsetX), gx, use="pairwise")[,1]         
+        jj <- head(order(-abs(rho)),1000)
+        rho <- rho[jj]
+        sort(rho,decreasing=TRUE)
+    })
+    
+    corfunctional_plots.PLOTLY <- reactive({
+        require(RColorBrewer)
+        rho = getFunctionalCorrelation()
+        ##if(is.null(rho)) return(NULL)
+        req(rho)
+
+        ## filter with table filter
+        ii <- corfunctional_table$rows_all()
+        req(ii)
+        rho <- rho[ii]
+        
+        ##par(mfrow=c(2,3), mar=c(3.5,2,2,1), mgp=c(2,0.8,0))
+        NTERMS = 6
+        NTERMS = 12
+
+        require(RColorBrewer)
+        klrpal = rep(brewer.pal(8,"Set2"),2)
+        ##klrpal = paste0(klrpal,"88")
+        col.addalpha <- function(clr,a=100)
+            paste0("rgba(",paste(col2rgb(clr)[,1],collapse=","),",",a,")")
+        ##klrpal = as.character(sapply(klrpal, col.addalpha, a=50))
+        klrpal <- paste0(klrpal,"55")
+        
+        jj <- unique(c(head(order(rho),NTERMS), head(order(-rho),NTERMS)))
+        x = sort(rho[jj])
+        names(x) = sub(".*:","",names(x))
+        names(x) = gsub(GSET.PREFIX.REGEX,"",names(x))
+
+        y = tagDuplicates(names(x))
+        y = factor(y, levels=y)
+        anntitle <- function(tt) {
+            list(text=tt, font = list(size=13),
+                 xref="paper", yref="paper",
+                 yanchor = "bottom", xanchor = "center",
+                 align = "center", x=0.5, y=1.02 , showarrow = FALSE )
+        }
+        slen = 60
+        ##x <- as.numeric(x)
+        ##y <- as.numeric(x)
+        klr1 <- klrpal[1 + 1*(sign(x)>0)]
+        
+        plt <- plot_ly(
+            x = x, y = y, type='bar',  orientation='h',
+            text = y, hoverinfo = 'text',
+            hovertemplate = paste0("%{y}<extra> </extra>"),
+            ##hovertemplate = "%{y}",
+            marker = list( color= klr1 ) )  %>%
+            layout(
+                showlegend = FALSE,
+                ## annotations = anntitle(names(rho)),
+                ##annotations = list(text="TITLE"),
+                xaxis = list(range = c(-1,1),
+                             titlefont = list(size=11),
+                             tickfont = list(size=10),
+                             showgrid=FALSE,
+                             title = "correlation (R)" ),
+                yaxis = list(title = "",
+                             showgrid = FALSE,
+                             showline = FALSE,
+                             showticklabels = FALSE,
+                             showgrid=FALSE,
+                             zeroline = FALSE)
+            )  
+
+        xanchor <- c('right','left')[1 + 1*(x>0)]
+        plt <- plt %>%
+            ## labeling the y-axis inside bars
+            add_annotations(xref = 'x', yref = 'y',
+                            x = 0.015 * sign(x), y = y,  xanchor=xanchor,
+                            text = shortstring(y,slen), 
+                            font = list(size = 11),
+                            showarrow = FALSE, align = 'right')
+        ##layout(margin = c(0,0,0,0))
+    })
+
+    corfunctional_plots_opts = tagList(
+        tipify( selectInput( ns("xann.refset"), "Reference set:", choices="", width='80%'),
+               "Specify a reference set to be used in the annotation.",
+               placement="left",options = list(container = "body"))
+    )
+
+    ##corfunctional_plots_module <- plotModule(
+    callModule(
+        plotModule, 
+        id = "corfunctional_plots", ##ns=ns,
+        func = corfunctional_plots.PLOTLY, plotlib="plotly",
+        download.fmt = c("png","pdf","html"),
+        options = corfunctional_plots_opts,
+        ##info.text = corfunctional_plots_text,        
+        title="Functional correlation", label="a",
+        height = c(0.5*fullH,700), width = c('auto',1000),
+        pdf.width=8, pdf.height=5, res=80        
+    )
+    ## output <- attachModule(output, corfunctional_plots_module)
+
+    
+    corfunctional_table.RENDER <- reactive({
+        
+        rho = getFunctionalCorrelation()
+        if(is.null(rho)) return(NULL)
+        
+        ##rownames(rho) = shortstring(rownames(rho),50)
+        ##rho.name = shortstring(sub(".*:","",names(rho)),60)
+        rho.name = shortstring(names(rho),50)
+        ##rho = data.frame(cbind( name=rho.name, rho))
+        link <- wrapHyperLink(rep("link",length(rho)), names(rho))
+        df = data.frame( feature=rho.name, link=link, rho=round(rho,digits=3))
+        rownames(df) = names(rho)
+
+        DT::datatable(
+                df, rownames=FALSE, escape = c(-1,-2),
+                extensions = c('Buttons','Scroller'),
+                selection=list(mode='single', target='row', selected=c(1)),
+                class = 'compact cell-border stripe hover',
+                fillContainer = TRUE,
+                options=list(
+                    dom = 'lfrtip', 
+                    ##pageLength = 20,##  lengthMenu = c(20, 30, 40, 60, 100, 250),
+                    scrollX = TRUE, ##scrollY = TRUE,
+                    ##scrollY = 170,
+                    scrollY = '70vh',
+                    scroller=TRUE,
+                    deferRender=TRUE
+                )  ## end of options.list 
+            ) %>%
+            DT::formatStyle(0, target='row', fontSize='11px', lineHeight='70%') 
+    })
+
+    corfunctional_table_info = "In this table, users can check mean correlation values of features in the clusters with respect to the annotation references database selected in the settings."
+
+    corfunctional_table <- callModule(
+        tableModule, 
+        id = "corfunctional_table", 
+        func = corfunctional_table.RENDER,
+        info.text = corfunctional_table_info,
+        title="Correlation table", label="b",
+        height = c(240,700), width=c('auto',1000),
+        ##caption = corfunctional_caption
+    )
     ##--------------------------------------------------------------------------------
     ## WGCNA
     ##--------------------------------------------------------------------------------
