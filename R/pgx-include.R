ca03aca67b948932a50426ee9096e1be575d801f
Ivo Kwee <ivo.kwee@gmail.com>
2020-01-10 03:15:13 +0100

    seperate pgx-include.R from pgx-init.R
    Former-commit-id: 4b6d6c46bbdc08be25ac8ee86f5c11030636bad3 [formerly 00c9a4a2ac30849936d9c803eac7c0c44e12321a]
    Former-commit-id: da6e9e304de17d728f00840212911a7c4a16f67a

---
+++ R/pgx-include.R
@@ -0,0 +1,75 @@
+library(shiny)
+library(shinyjs)
+library(shinyBS)
+library(shinyjqui)
+library(rmarkdown)
+library(shinycssloaders)
+library(dragulaR)
+library(shinyWidgets)
+library(pryr)
+
+library(survival)
+library(knitr)
+library(scatterD3)
+library(fastcluster)
+library(ComplexHeatmap)
+library(plotly)
+library(Matrix)
+library(igraph)
+library(DT)
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(org.Hs.eg.db)
+## library(Cairo)
+
+##useShinyjs(rmd=TRUE)  
+useShinyjs()
+ht_global_opt(fast_hclust = TRUE)
+options(shiny.maxRequestSize = 500*1024^2)  ## max 200Mb upload
+
+dbg <- function(... ) {
+    if(DEV.VERSION) {
+        ##msg = paste0(ifelse(is.null(module),"",paste0("<",module,"> ")),msg)
+        msg = sapply( list(...),paste,collapse=" ")
+        cat(paste0("DBG ",sub("\n$","",paste(msg,collapse=" ")),"\n"))
+    }
+}
+
+source(file.path(RDIR,"gx-heatmap.r"))
+source(file.path(RDIR,"gx-plot.r"))
+source(file.path(RDIR,"gx-limma.r"))
+source(file.path(RDIR,"gx-volcano.r"))
+source(file.path(RDIR,"gx-combat.r"))
+source(file.path(RDIR,"gx-util.r"))
+       
+source(file.path(RDIR,"gset-gsea.r"))
+source(file.path(RDIR,"gset-fisher.r"))
+source(file.path(RDIR,"gset-meta.r"))
+
+source(file.path(RDIR,"ngs-cook.r"))
+source(file.path(RDIR,"ngs-fit.r"))
+
+source(file.path(RDIR,"pgx-functions.R"))
+source(file.path(RDIR,"pgx-contrasts.R"))
+source(file.path(RDIR,"pgx-graph.R"))
+source(file.path(RDIR,"pgx-deconv.R"))
+source(file.path(RDIR,"pgx-cna.R"))
+source(file.path(RDIR,"pgx-plotting.R"))
+source(file.path(RDIR,"pgx-correct.R"))
+source(file.path(RDIR,"pgx-predict.R"))
+source(file.path(RDIR,"pgx-links.R"))
+source(file.path(RDIR,"pgx-modules.R"))
+source(file.path(RDIR,"pgx-upload.R"))
+source(file.path(RDIR,"pgx-proteomics.R"))
+source(file.path(RDIR,"pgx-drugs.R"))
+source(file.path(RDIR,"pgx-files.R"))
+source(file.path(RDIR,"pgx-wordcloud.R"))
+
+source(file.path(RDIR,"xcr-graph.r"))
+source(file.path(RDIR,"ui-code.R"))
+
+source( file.path(RDIR,"compute2-genes.R"))
+source( file.path(RDIR,"compute2-genesets.R"))
+source( file.path(RDIR,"compute2-extra.R"))
+
