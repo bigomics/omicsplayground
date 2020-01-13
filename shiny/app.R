ffb1ec666022f607f475534d18e66cf1c014f4d3
Ivo Kwee <ivo.kwee@gmail.com>
2020-01-13 23:57:05 +0100

    sync
    Former-commit-id: 8edf4d3c8705a5ecde00aed587e1d2b7247ef436 [formerly 3a2f71271cf15d8c9448c24acef2026ab2caeb27]
    Former-commit-id: 681bf38031389f4bc47745ef09e0c72006673238

--- shiny/app.R
+++ shiny/app.R
@@ -29,7 +29,7 @@ source("../R/pgx-init.R", local=TRUE)  ## pass local vars
 options(shiny.maxRequestSize = 500*1024^2)  ##max 500Mb upload
 OPTIONS <- pgx.readOptions(file="OPTIONS")
 
-DEV.VERSION = TRUE
+## DEV.VERSION = TRUE
 if(!dir.exists("../../omicsplayground-dev")) DEV.VERSION = FALSE
 
 if(0) {
