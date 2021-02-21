d812830f4428bd5148b16d2a8ca5ab39f4db0410
info@bigomics.ch <ivo.kwee@gmail.com>
2021-02-21 16:27:56 +0100

    include TcgaBoard.R
    Former-commit-id: 1f2c189309de9eaccf5c0b5b6385b406d1e3923b [formerly ad6e78d4c9c73552c49d9751db8c7cb445ed5d9c]
    Former-commit-id: 3fb43996da757be06d9e02d862ac8a7077dcc5e0

---
+++ shiny/boards/TcgaBoard.R
@@ -0,0 +1,192 @@
+TcgaInputs <- function(id) {
+    ns <- NS(id)  ## namespace
+    tagList(
+        uiOutput(ns("description")),
+        uiOutput(ns("inputsUI"))
+    )
+}
+
+TcgaUI <- function(id) {
+    ns <- NS(id)  ## namespace
+    fillCol(
+        height = 750,
+        tabsetPanel(
+            id = ns("tabs1"),
+            tabPanel("TCGA survival", uiOutput(ns("TCGAanalysis_UI")))
+        )
+    )
+}
+
+TcgaBoard <- function(input, output, session, env)
+{
+    ns <- session$ns ## NAMESPACE
+    fullH = 750       # row height of panel 
+    tabH = '70vh'
+    
+    ## reactive functions from shared environment
+    inputData <- env[["load"]][["inputData"]]
+    selected_gxmethods <- env[["expr"]][["selected_gxmethods"]]
+    selected_gsetmethods <- env[["enrich"]][["selected_gsetmethods"]]
+    
+    description = "<b>Tcga analysis</b>. Compare experiments by intersecting
+their signature genes. The main goal is to identify contrasts showing
+similar profiles. Find genes that are commonly up/down regulated
+between two contrasts."
+    output$description <- renderUI(HTML(description))
+
+    tcga_infotext =
+        "The <strong>Tcga analysis module</strong> enables users to compare their data to other datasets.
+
+<br><br>For the selected contrasts, the platform provides volcano plots and pairwise correlation plots between the profiles in the <strong>Pairs</strong> panel. Simultaneously, a Venn diagram with the number of intersecting genes between the profiles is plotted in <strong>Venn diagram</strong> panel. Details of intersecting genes are also reported in an interactive table. A more detailed scatter plot of two profiles is possible under the <strong>Two-pairs</strong> panel. Users can check the pairwise correlations of the contrasts under the <b>Contrast heatmap</b> panel. Alternatively, the <strong>Connectivity Map (CMap)</strong> shows the similarity of the contrasts profiles as a t-SNE plot.
+
+<br><br><br><br>
+<center><iframe width='500' height='333' src='https://www.youtube.com/embed/watch?v=qCNcWRKj03w&list=PLxQDY_RmvM2JYPjdJnyLUpOStnXkWTSQ-&index=5' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></center>
+
+"
+    ##================================================================================
+    ##========================= INPUTS UI ============================================
+    ##================================================================================
+    
+
+    output$inputsUI <- renderUI({
+        ui <- tagList(
+            tipify( actionLink(ns("tcga_info"), "Info", icon = icon("info-circle")),
+                   "Show more information about this module"),
+            hr(), br(),             
+            tipify( selectInput(ns('tcga_contrast'),'Contrast:', choices=NULL, multiple=FALSE),
+                   "Select the contrast that you want to compare.",
+                   placement="right", options = list(container = "body")
+                   ),
+
+            tipify( actionLink(ns("tcga_options"), "Options", icon=icon("cog", lib = "glyphicon")),
+                   "Toggle advanced options.",
+                   placement="top", options = list(container = "body")),
+            br(),br(),            
+            conditionalPanel(
+                "input.tcga_options % 2 == 1", ns=ns,
+                tipify( selectInput(ns('tcga_profiledb'),"Profile DB:",
+                                    choices=NULL, multiple=TRUE),
+                       "Select external database for reference profiles.",
+                       placement="top", options = list(container = "body"))
+            )
+        )
+        ui
+    })
+    outputOptions(output, "inputsUI", suspendWhenHidden=FALSE) ## important!!!
+        
+    
+    ##================================================================================
+    ##======================= OBSERVE FUNCTIONS ======================================
+    ##================================================================================
+    
+    observeEvent( input$tcga_info, {
+        showModal(modalDialog(
+            title = HTML("<strong>TCGA Analysis Board</strong>"),
+            HTML(tcga_infotext),
+            easyClose = TRUE, size="l" ))
+    })
+
+    ## update choices upon change of data set 
+    observe({
+        ngs <- inputData()
+        ##req(ngs)
+        if(is.null(ngs)) return(NULL)
+        comparisons <- colnames(ngs$model.parameters$contr.matrix)
+        comparisons <- sort(comparisons)
+        updateSelectInput(session, "tcga_contrast", choices=comparisons,
+                          selected = head(comparisons,1))
+        
+    })
+    
+    ##================================================================================
+    ## TCGA survival
+    ##================================================================================
+
+    search.path <- function(paths, file) {
+        dir <- paths[which(file.exists(file.path(paths,file)))]
+        if(length(dir)==0) return(NULL)
+        file.path(dir[1],file)
+    }
+    
+    tcga_tcgasurv.RENDER %<a-% reactive({
+
+        ngs <- inputData()
+        req(ngs)
+        
+        contrast = 1
+        contrast <- input$tcga_contrast
+        res = pgx.getMetaFoldChangeMatrix(ngs, what="meta")
+        names(res)
+        sig <- res$fc[,contrast]
+        ##matrix_file = search.path(c(FILES,ARCHS4.DIR),"tcga_matrix.h5")
+        ##FILESX <- sub("lib$","libx",FILES)
+        matrix_file = search.path(c(FILES,FILESX),"tcga_matrix.h5")
+        if(!file.exists(matrix_file)) {
+            showNotification("FATAL ERROR: could not find tcga_matrix.h5")
+            return(NULL)
+        }
+        dbg("[tcga_tcgasurv.RENDER] reading TCGA data from",matrix_file)
+        showNotification("computing survival probabilities...")
+        sortby="name";ntop=100
+        sortby <- input$tcga_tcgasurv_sortby
+        ntop <- as.integer(input$tcga_tcgasurv_ntop)
+        deceased.only <- input$tcga_surv_deceasedonly
+        
+        pgx.testTCGAsurvival(
+            sig,
+            matrix_file,
+            lib.dir = FILES,
+            ntop = ntop,
+            sortby.p = FALSE,
+            deceased.only = deceased.only,
+            min.cases = 10            
+        )
+
+    })
+    
+    tcga_tcgasurv.opts = tagList(
+        tipify( checkboxInput(ns("tcga_surv_deceasedonly"), "deceased only", FALSE),
+               "Only include deceased cases in survival analysis, i.e. exclude censored cases (patients still alive at evaluation time). This compares strictly the deceased cases, early vs late.",
+               placement="left", options = list(container = "body")),
+        ## radioButtons(ns("tcga_tcgasurv_sortby"), "sort by:", c("name","p-value"), inline=TRUE),
+        tipify(radioButtons(ns("tcga_tcgasurv_ntop"), "N cor genes:", c(25,100,250,1000),
+                            selected=100, inline=TRUE),
+               "Number of top genes for calculating the correlation.", placement="left",
+               options = list(container = "body"))        
+    )
+    tcga_tcgasurv_info = "<strong>TCGA survival analysis.</strong> Survival probability of cancer patients in 33 TCGA cancer types. Each cohort is dichotomized into positively and negatively correlated with your signature. The survival probabilities are computed and tested using the Kaplan-Meier method."
+    tcga_tcgasurv_caption = tcga_tcgasurv_info
+    
+    callModule(
+        plotModule,
+        id = "tcga_tcgasurv",
+        title = "TCGA survival analysis", label="a",
+        func = tcga_tcgasurv.RENDER,
+        func2 = tcga_tcgasurv.RENDER,
+        info.text = tcga_tcgasurv_info,
+        ## caption = tcga_tcgasurv_info,
+        options = tcga_tcgasurv.opts,
+        download.fmt = c("pdf","png"),
+        pdf.width = 16, pdf.height = 8,
+        height = c(fullH-80, 700), width = c("auto",1350),
+        res=c(72,85)
+    )
+
+    
+    ##================================================================================
+    ##========================= OUTPUT UI ============================================
+    ##================================================================================
+
+    output$TCGAanalysis_UI <- renderUI({
+        fillCol(
+            ## id = ns("expr_topgenes"),
+            height = fullH,
+            flex = c(NA,0.05,1), ##height = 370,
+            div(HTML(tcga_tcgasurv_caption), class="caption"),
+            br(),
+            plotWidget(ns("tcga_tcgasurv"))
+        )
+    })
+    outputOptions(output, "TCGAanalysis_UI", suspendWhenHidden=FALSE) 
+    
+} ## end-of-Board 
