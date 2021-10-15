39c971fbfd0706c90e1640c0ff3141045299adc2
ivokwee <ivo.kwee@gmail.com>
2021-10-15 17:28:08 +0200

    authentication none as default

--- shiny/app.R
+++ shiny/app.R
@@ -83,7 +83,7 @@ opt <- pgx.readOptions(file="OPTIONS")
 ##opt$AUTHENTICATION = "none"
 ##opt$AUTHENTICATION = "password"
 ##opt$AUTHENTICATION = "register"
-opt$AUTHENTICATION = "firebase"
+##opt$AUTHENTICATION = "firebase"
 
 if(Sys.getenv("PLAYGROUND_AUTHENTICATION")!="") {
     auth <- Sys.getenv("PLAYGROUND_AUTHENTICATION")
