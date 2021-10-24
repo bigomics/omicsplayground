025bc80e3d1a6da3fc53af6d48f114b6202889ad
JohnCoene <jcoenep@gmail.com>
2021-10-24 17:07:04 +0200

    redo user board
    Former-commit-id: b81ccf745c6d7aa0ec240f1aa2e8566c671b7247 [formerly f92efd1d5ea6514b443885ef0375c5f4aae74be7]
    Former-commit-id: fa41ad36f8d2707d4eae8e06f1d7c4ad8fbde784

--- shiny/boards/UserBoard.R
+++ shiny/boards/UserBoard.R
@@ -6,22 +6,25 @@
 message(">>> sourcing UserBoard")
 
 UserInputs <- function(id) {
-    ns <- shiny::NS(id)  ## namespace
+    ns <- shiny::NS(id)
     shiny::tagList(
         shiny::uiOutput(ns("description"))
-        ## shiny::uiOutput(ns("inputsUI"))
     )
 }
 
 UserUI <- function(id) {
     ns <- shiny::NS(id)  ## namespace
-    shiny::fillCol(
-        height = 750,
-        shiny::tabsetPanel(
-            id = ns("tabs"),
-            shiny::tabPanel("User profile",uiOutput(ns("userinfo_UI")))
-            ## shiny::tabPanel("Visitors map",uiOutput(ns("usersmap_UI")))
-            ## shiny::tabPanel("Community forum",uiOutput(ns("forum_UI")))
+
+    shiny::div(
+        style = "padding-left:1rem!important;",
+        shiny::h1("User Profile"),
+        shiny::div(
+            class = "row",
+            shiny::div(
+                class = "col-md-3",
+                uiOutput(ns("username")),
+                uiOutput(ns("plan"))
+            )
         )
     )
 }
@@ -33,33 +36,23 @@ UserBoard <- function(input, output, session, env)
     user <- env[["load"]][["auth"]]
 
     dbg("[UserBoard] >>> initializing UserBoard...")
-    
-    ##-----------------------------------------------------------------------------
-    ## Description
-    ##-----------------------------------------------------------------------------
-    
-    output$description <- shiny::renderUI({
 
-        dbg("[UserBoard::description] names(user) = ",names(user) )
-        dbg("[UserBoard::description] user$name = ",user$name() )
-        dbg("[UserBoard::description] user$level = ",user$level() )
-        
-        description = "Signed in as<h2><b>NAME<b></h2><h4><b>EMAIL<b></h4><br><h4>LEVEL</h4>"
-        ##description = "Signed in as<h4><b>EMAIL<b></h4>"        
-        description <- sub("EMAIL",  as.character(user$email()),  description)
-        description <- sub("NAME",  as.character(user$name()),  description)        
-        description <- sub("LEVEL", as.character(user$level()), description)     
-        shiny::HTML(description)
-    })
-    
-    ##-----------------------------------------------------------------------------
-    ## User interface
-    ##-----------------------------------------------------------------------------
-    
-    output$inputsUI <- shiny::renderUI({        
+    output$username <- renderUI({
+        div(
+            h3(user$name()),
+            p(user$email())
+        )
     })
 
+    output$plan <- renderUI({
+        plan_class <- "info"
+        if(user$level() == "premium")
+            plan_class <- "success"
 
+        cl <- sprintf("badge badge-%s", plan_class)
+        span(class = cl, tools::toTitleCase(user$level()))
+    })
+    
     output$userdata <- renderTable({
         dbg("[UserBoard::userdata]  renderDataTable")
         values <- c(
@@ -73,133 +66,6 @@ UserBoard <- function(input, output, session, env)
         df <- data.frame(field=names(values), value=values)
     })
         
-    output$userinfo_UI <- shiny::renderUI({        
-        tableOutput(ns("userdata"))
-    })
-    shiny::outputOptions(output, "userinfo_UI", suspendWhenHidden=FALSE) ## important!
-
-    
-    ##---------------------------------------------------------------
-    ##--------------------- modules for UsersMap --------------------
-    ##---------------------------------------------------------------
-    
-    usersmap.RENDER %<a-% shiny::reactive({
-        
-        df <- ACCESS.LOG$visitors        
-        ## sPDF <- rworldmap::getMap()  
-        ## rworldmap::mapCountryData(sPDF, nameColumnToPlot='continent')
-        sPDF <- rworldmap::joinCountryData2Map(
-            df,
-            joinCode = "ISO2",
-            nameJoinColumn = "country_code")
-        
-        par(mai=c(0,0.4,0.2,1),xaxs="i",yaxs="i")
-        mapParams <- rworldmap::mapCountryData(
-            sPDF, nameColumnToPlot="count",
-            ##mapTitle = "Number of unique IPs",
-            mapTitle = "", addLegend='FALSE',
-            colourPalette = RColorBrewer::brewer.pal(9,"Blues"),
-            numCats=9, catMethod="logFixedWidth")   
-                   
-        ##add a modified legend using the same initial parameters as mapCountryData
-        do.call( rworldmap::addMapLegend,
-                c(mapParams, labelFontSize = 0.85, legendWidth = 1.2, legendShrink = 0.5,
-                  legendMar = 4, horizontal = FALSE, legendArgs = NULL, tcl = -0.5,
-                  sigFigs = 4, digits = 3)
-                )
-        
-    })
-    
-    usersmap_info = "<strong>Visitors map.</strong> The world map shows the number of users visiting this site by unique IP."
-    
-    shiny::callModule(
-        plotModule,
-        id = "usersmap", ## label="a", 
-        plotlib = "baseplot",
-        func = usersmap.RENDER,
-        func2 = usersmap.RENDER, 
-        info.text = usersmap_info,
-        ##options = usersmap_options,
-        pdf.width=12, pdf.height=7, pdf.pointsize=13,
-        height = c(450,600), width = c('auto',1000), res=72,
-        ##datacsv = enrich_getWordFreq,
-        title = "Number of visitors by country",
-        add.watermark = WATERMARK
-    )
-
-    ##usersmap_caption = "<b>(a)</b> <b>Geo locate.</b>"
-    output$usersmapInfo <- shiny::renderUI({
-
-        u <- ACCESS.LOG
-        df <- u$visitors
-        rownames(df) <-  df$country_name
-        tot.users <- sum(df$count)
-        freq <- df$count
-        names(freq) <- df$country_name
-        top.countries <- head(sort(freq,dec=TRUE),10)
-        top.countriesTT <- paste("<li>",names(top.countries),top.countries,collapse=" ")
-        
-        shiny::HTML(
-            "<b>Total visitors:</b>",tot.users,"<br><br>",
-            "<b>Top 10 countries:</b><br><ol>",top.countriesTT,"</ol><br>",
-            "<b>Period:</b><br>",u$period,"<br><br>"
-        )
-    })
-    
-    output$usersmap_UI <- shiny::renderUI({
-        shiny::fillCol(
-            height = 600,
-            shiny::fillRow(
-                flex = c(1,4.5),
-                shiny::wellPanel( shiny::uiOutput(ns("usersmapInfo"))),
-                plotWidget(ns("usersmap"))
-            )
-        )
-    })
-
-
-    ##---------------------------------------------------------------
-    ##----------------- modules for Forum ---------------------------
-    ##---------------------------------------------------------------
-    
-    output$forum <- shiny::renderUI({
-        parenturl <- paste0(session$clientData$url_protocol,
-                            "//",session$clientData$url_hostname,
-                            ":",session$clientData$url_port,
-                            session$clientData$url_pathname)
-        ## parenturl <- gsub("localhost","127.0.0.1",parenturl)
-        parenturl <- URLencode(parenturl, TRUE)
-        cat("[LoadingBoard:forum] parenturl =",parenturl,"\n")
-        src = paste0('https://groups.google.com/forum/embed/?place=forum/omicsplayground',
-                     '&showsearch=true&showpopout=true&parenturl=',parenturl)
-        cat("src = ",src,"\n")
-        shiny::tags$iframe(id="forum_embed", src=src, height=600, width='100%',
-                    ##seamless="seamless",
-                    frameborder='no')
-        ##HTML(src)
-    })
-         
-    output$tweet <- shiny::renderUI({
-        ## NOT WORKING YET...
-        shiny::tags$a(class="twitter-timeline",
-               href="https://twitter.com/bigomics?ref_src=twsrc%5Etfw")
-        ##shiny::tags$script('twttr.widgets.load(document.getElementById("tweet"));')
-    })
-            
-    output$forum_UI <- shiny::renderUI({
-        shiny::fillCol(
-            height = 550,
-            shiny::fillRow(
-                flex=c(4,0),
-                shiny::htmlOutput(ns("forum"))
-                ##uiOutput("tweet")
-            )
-        )
-    })
-    
-    ##------------------------------------------------
-    ## Board return object
-    ##------------------------------------------------
     res <- list()
     return(res)
 }
