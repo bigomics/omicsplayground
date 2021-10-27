c91655ce5191450036075e679ef702759dc78f6f
ivokwee <ivo.kwee@gmail.com>
2021-10-27 20:46:53 +0200

    add logs/html for old server logs
    Former-commit-id: 4237d38c25b6b6e4f088443441ee1e115d7f162d [formerly 3e8c08531192ad7b6e0cd0b41c8db48a7c51f258]
    Former-commit-id: cd8d42a8906c12b60b89ddf78197dcfeeb3dc06f

--- shiny/app-init.R
+++ shiny/app-init.R
@@ -9,7 +9,7 @@ message("===============================================================")
 
 ## Parse access logs
 access.dirs = c("/var/www/html/logs", "/var/log/apache2","/var/log/apache",
-                "/var/log/httpd","/var/log/nginx","../logs")
+                "../logs/html", "/var/log/httpd","/var/log/nginx")
 access.dirs <- access.dirs[dir.exists(access.dirs)]
 access.dirs
 ##ACCESS.LOG <- pgx.parseAccessLogs(access.dirs[], filter.get=NULL)
