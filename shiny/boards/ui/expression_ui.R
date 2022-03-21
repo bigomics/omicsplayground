4f24869c6a11e73bf43034dba62bb58b94c38dfd
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-03-21 14:11:06 +0100

    refactor expression board input ui
    Former-commit-id: 6d45c121ffe954fa1fba405c1fec74016d587e2f [formerly 1c95f97d45a09b62b215af5667ec83c9d20def48]
    Former-commit-id: 3cb05155e9a5daaf0d2dcb0a5122d58604d10122

--- shiny/boards/ui/expression_ui.R
+++ shiny/boards/ui/expression_ui.R
@@ -3,13 +3,48 @@
 ## Copyright (c) 2018-2022 BigOmics Analytics Sagl. All rights reserved.
 ##
 
-message(">>> sourcing ExpressionBoard")
 
 ExpressionInputs <- function(id) {
     ns <- shiny::NS(id)  ## namespace
     shiny::tagList(
-        shiny::uiOutput(ns("description")),
-        shiny::uiOutput(ns("inputsUI"))
+        tags$div(
+         HTML("<b>Differential Expression Analysis.</b> Compare expression between
+        two conditions. Determine which genes are significantly downregulated or overexpressed in one of the groups.")
+        ),
+        shiny::tagList(
+            shinyBS::tipify( shiny::actionLink(ns("gx_info"), "Tutorial", icon = shiny::icon("youtube")),
+                   "Show more information about this module."),
+            shiny::hr(), shiny::br(),             
+            shinyBS::tipify( shiny::selectInput(ns("gx_contrast"), "Contrast:", choices=NULL),
+                   "Select a contrast of interest for the analysis.", placement="top"),
+            shinyBS::tipify( shiny::selectInput(ns("gx_features"),"Gene family:", choices=NULL, multiple=FALSE),
+                   "Choose a specific gene family for the analysis.", placement="top"),
+            shiny::fillRow( flex=c(1,1),
+                    shinyBS::tipify( shiny::selectInput(ns("gx_fdr"),"FDR", choices=c(1e-9,1e-6,1e-3,0.01,0.05,0.1,0.2,0.5,1), selected=0.2),
+                           "Set the false discovery rate (FDR) threshold.", placement="top"),
+                    shinyBS::tipify( shiny::selectInput(ns("gx_lfc"),"logFC threshold",
+                                        choices=c(0,0.1,0.2,0.5,1,2,5), selected=0),
+                           "Set the logarithmic fold change (logFC) threshold.", placement="top")
+                    ),
+            shiny::br(),br(),br(),br(),
+            shinyBS::tipify( shiny::actionLink(ns("gx_options"), "Options", icon=icon("cog", lib = "glyphicon")),
+                   "Toggle advanced options.", placement="top"),
+            shiny::br(),br(),
+            shiny::conditionalPanel(
+                "input.gx_options % 2 == 1", ns=ns,
+                shiny::tagList(
+                    shinyBS::tipify(shiny::checkboxInput(ns("gx_showall"),"show all genes", FALSE),
+                           "Display all genes in the table. Disable filtering of significant genes.", 
+               placement="top", options = list(container = "body")),
+                    shinyBS::tipify( shiny::checkboxGroupInput(ns('gx_statmethod'),'Statistical methods:',
+                                               choices=NULL, inline=TRUE),
+                           "Select a method for the statistical test. To increase the statistical reliability of the Omics Playground,
+                            we perform the DE analysis using commonly accepted methods in the literature, including t-test (standard, Welch),
+                            limma (no trend, trend, voom), edgeR (QLF, LRT), and DESeq2 (Wald, LRT), and merge the results.",
+                             placement="right", options=list(container="body"))
+                )
+            )
+        )
     )
 }
 
