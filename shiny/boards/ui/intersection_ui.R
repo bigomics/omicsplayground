1cf56a92deef8ae868ebe48377fbd34b544ff9fb
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-03-22 09:45:37 +0100

    finished refactoring intersection board
    Former-commit-id: a7cc83f3f12fdf022271c8b7e1b00204dfe67b8c [formerly e59799c69db1f79914df5574303d32b4ca93c3ce]
    Former-commit-id: 1327895398cb87138ec6f73d9315595d40be3280

--- shiny/boards/ui/intersection_ui.R
+++ shiny/boards/ui/intersection_ui.R
@@ -8,8 +8,36 @@ message(">>> sourcing IntersectionBoard")
 IntersectionInputs <- function(id) {
     ns <- shiny::NS(id)  ## namespace
     shiny::tagList(
-        shiny::uiOutput(ns("description")),
-        shiny::uiOutput(ns("inputsUI"))
+        tags$div(
+         HTML("<h3>Compare signatures</h3>Find genes that are commonly up/down regulated
+          between two or more signatures. Compute similarity between contrasts.")
+        ),
+        shiny::tagList(
+            shinyBS::tipify( shiny::actionLink(ns("info"), "Tutorial", icon = shiny::icon("youtube")),
+                   "Show more information about this module"),
+            shiny::hr(), shiny::br(),             
+            shinyBS::tipify( shiny::selectInput(ns('comparisons'),'Contrasts:', choices=NULL, multiple=TRUE),
+                   "Select the contrasts that you want to compare. If you select N=2 contrast a single scatterplot will be drawn. For N>=3 a scatterplot matrix will be drawn.",
+                   placement="top"),
+
+            shinyBS::tipify( shiny::actionLink(ns("options"), "Options", icon=icon("cog", lib = "glyphicon")),
+                   "Toggle advanced options.", placement="top"),
+            shiny::br(),br(),
+            shiny::conditionalPanel(
+                "input.options % 2 == 1", ns=ns,
+                shinyBS::tipify( shiny::radioButtons(ns("level"),"Level:",
+                                     choices=c("gene","geneset"), inline=TRUE),
+                       "Select feature level: gene or geneset", placement="top"),                
+                shinyBS::tipify( shiny::selectInput(ns("filter"),"Filter:", choices=NULL, multiple=FALSE),
+                       "Filter features", placement="top"),
+                shiny::conditionalPanel(
+                    "input.filter == '<custom>'", ns=ns,
+                    shinyBS::tipify( shiny::textAreaInput(ns("customlist"), NULL, value = NULL,
+                                          rows=5, placeholder="Paste your custom gene list"),
+                           "Paste a custom list of genes to highlight.", placement="bottom")
+                )
+            )
+        )
     )
 }
 
@@ -19,8 +47,50 @@ IntersectionUI <- function(id) {
         height = 750,
         shiny::tabsetPanel(
             id = ns("tabs1"),
-            shiny::tabPanel("Pairwise scatter",uiOutput(ns("scatterPlotMatrix_UI"))),
-            shiny::tabPanel("Signature clustering",uiOutput(ns("ctClustering_UI")))
+            shiny::tabPanel("Pairwise scatter",
+                shiny::fillCol(
+                height = 800,
+                flex=c(NA,0.02,1),
+                tags$div(
+                 HTML("<h4>Pairwise scatter & Venn diagram</h4> <b>(a)</b> <b>Pairs plot.</b> Pairwise scatterplots
+                  for two or more differential expression profiles for multiple selected contrasts.
+                   Similar profiles will show high correlation with points close to the diagonal. 
+                   <b>(b)</b> <b>Venn diagram</b> showing the number of overlapping genes for multiple contrasts.
+                    <b>(c)</b> <b>Venn table.</b> Genes in the selected overlap region.")
+                ),
+                shiny::br(),
+                shiny::fillRow(
+                    flex=c(1.7,0.15,1),
+                    height = 800,
+                    plotWidget(ns("scatterPlotMatrix")),
+                    shiny::br(),
+                    shiny::fillCol(
+                        flex = c(1.4,1),
+                        plotWidget(ns("venndiagram")),
+                        tableWidget(ns("venntable"))
+                    )
+                )
+            )),
+            shiny::tabPanel("Signature clustering",
+                shiny::fillCol(
+                height = 800,
+                flex = c(NA,0.035,1),
+                tags$div(
+                 HTML("<h4>Signature clustering</h4> <b>(a)</b> <b>Signature heatmap.</b> Similarity of the
+                  signatures visualized as a clustered heatmap. The top plot shows the distribution of foldchange
+                   values as boxplots. <b>(b)</b> <b>Contrast correlation.</b> The numeric values in the cells
+                    correspond to the Pearson correlation coefficient. Red corresponds to positive correlation
+                     and blue to negative correlation.")
+                ),
+                shiny::br(),
+                shiny::fillRow(
+                    flex = c(2.2,0.01,1),
+                    height = 800,
+                    plotWidget(ns("FoldchangeHeatmap")),
+                    shiny::br(),
+                    plotWidget(ns("ctcorrplot"))
+                )
+            ))
         )
     )
 }
