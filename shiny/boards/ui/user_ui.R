d0ee98cf8c2e4dca96b6d91b06896ea98fe07e2b
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-03-22 15:43:15 +0100

    refactor user board

--- shiny/boards/ui/user_ui.R
+++ shiny/boards/ui/user_ui.R
@@ -8,7 +8,6 @@ UserInputs <- function(id) {
     shiny::tagList(
                HTML("<h3>User Settings</h3><br><br>"),
                shiny::uiOutput(ns("description"))
-               ## shiny::uiOutput(ns("inputsUI"))
            )
 }
 
@@ -18,9 +17,35 @@ UserUI <- function(id) {
                height = 750,
                shiny::tabsetPanel(
                           id = ns("tabs"),
-                          shiny::tabPanel("User settings",uiOutput(ns("userinfo_UI")))
-                          ## shiny::tabPanel("Visitors map",uiOutput(ns("usersmap_UI")))
-                          ## shiny::tabPanel("Community forum",uiOutput(ns("forum_UI")))
+                          shiny::tabPanel("User settings",
+                          fillRow(
+                            flex=c(0.8,0.2,1,0.2,1),
+                            tagList(
+                                shiny::h4("News"),            
+                                shiny::htmlOutput(ns("news"))
+                            ),br(),
+                            tagList(
+                                shiny::h4("Personal"),
+                                uiOutput(ns("plan")),                    
+                                shiny::tableOutput(ns("userdata"))
+                            ),br(),
+                            tagList(
+                                shiny::h4("Settings"),            
+                                shinyWidgets::prettySwitch(ns("enable_beta"),"enable beta features")
+                            )
+                        ))
+                        # Currently not used Stefan 22.03.22
+                        # shiny::tabPanel("Visitors map",
+                        #     shiny::fillCol(
+                        #         height = 600,
+                        #         shiny::fillRow(
+                        #             flex = c(1,4.5),
+                        #             shiny::wellPanel( shiny::uiOutput(ns("usersmapInfo"))),
+                        #             plotWidget(ns("usersmap"))
+                        #         )
+                        #     )
+                        # )
+                        #shiny::tabPanel("Community forum",uiOutput(ns("forum_UI")))
                       )
            )
 }
