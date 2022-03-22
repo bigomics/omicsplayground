ba01ff27ba08a2a6da87144f9cc35ed9c5af9c83
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-03-22 15:51:47 +0100

    beginning refactor wgcna board

--- shiny/boards/server/wgcna_server.R
+++ shiny/boards/server/wgcna_server.R
@@ -10,13 +10,9 @@ WgcnaBoard <- function(input, output, session, inputData)
     fullH = 700  ## full height of page
     rowH1 = 250  ## row 1 height
     rowH2 = 440  ## row 2 height
-    
-    description = "<b>WGCNA Analysis.</b> Weighted correlation network analysis (WGCNA) is a gene-level cluster analysis method based on pairwise correlations between genes. It allows one to define modules (clusters), intramodular hubs, and network nodes with regard to module membership, to study the relationships between co-expression modules."
-    output$description <- shiny::renderUI(shiny::HTML(description))
 
     infotext ="Weighted gene co-expression network analysis (WGCNA) is a systems biology method for describing the correlation patterns among genes across microarray samples. Weighted correlation network analysis can be used for finding clusters (modules) of highly correlated genes, for summarizing such clusters using the module eigengene or an intramodular hub gene, for relating modules to one another and to external sample traits (using eigengene network methodology), and for calculating module membership measures. Correlation networks facilitate network based gene screening methods that can be used to identify candidate biomarkers or therapeutic targets."
     
-
     ##================================================================================
     ##========================= OUTPUT UI ============================================
     ##================================================================================
@@ -117,45 +113,6 @@ WgcnaBoard <- function(input, output, session, inputData)
         )
         
     })
-    
-    ##================================================================================
-    ##========================= INPUTS UI ============================================
-    ##================================================================================
-
-    
-    output$inputsUI <- shiny::renderUI({
-        ui <- shiny::tagList(
-            shiny::actionLink(ns("info"), "Info", icon=icon("info-circle")),
-            shiny::hr(), shiny::br(),             
-            
-            ## data set parameters
-            shiny::selectInput(ns('selected_module'),'select module', choices=NULL),
-            shiny::actionButton(ns("compute"),"Compute!",icon=icon("running"),
-                         class="run-button"),            
-            shiny::br(),
-            shiny::br(),
-            shiny::actionLink(ns("options"), "Options", icon=icon("cog", lib = "glyphicon")),
-            shiny::br(),br(),br(),
-            shiny::conditionalPanel(
-                "input.options % 2 == 1", ns=ns,
-                shiny::tagList(
-                    shiny::selectInput(ns("ngenes"),"Number genes:",
-                                choices = c(500,1000,2000,4000,8000),
-                                selected = 1000),
-                    shiny::selectInput(ns("minmodsize"),"Min. module size",
-                                choices = c(10,30,100,250),
-                                selected = 30 ),
-                    shiny::selectInput(ns("power"),"Power", c(2,4,6,10), selected=6),
-                    shiny::selectInput(ns("deepsplit"),"deepsplit", 0:4, selected=2),
-                    shiny::selectInput(ns("cutheight"),"Merge cut height",
-                                choices = c(0.05, 0.10, 0.25, 0.5, 0.9, 0.999),
-                                selected = 0.25)
-                )
-            )
-        )
-
-    })
-    shiny::outputOptions(output, "inputsUI", suspendWhenHidden=FALSE) ## important!!!
     
     ##================================================================================
     ##======================= PRECOMPUTE FUNCTION ====================================
--- shiny/boards/ui/wgcna_ui.R
+++ shiny/boards/ui/wgcna_ui.R
@@ -6,8 +6,41 @@
 WgcnaInputs <- function(id) {
     ns <- shiny::NS(id)  ## namespace
     shiny::tagList(
-        shiny::uiOutput(ns("description")),
-        shiny::uiOutput(ns("inputsUI"))
+        tags$div(
+         HTML("<b>WGCNA Analysis.</b> Weighted correlation network analysis (WGCNA) is a gene-level cluster analysis
+        method based on pairwise correlations between genes. It allows one to define modules (clusters),
+        intramodular hubs, and network nodes with regard to module membership, to study the relationships
+        between co-expression modules.")
+        ),
+        shiny::tagList(
+            shiny::actionLink(ns("info"), "Info", icon=icon("info-circle")),
+            shiny::hr(), shiny::br(),             
+            
+            ## data set parameters
+            shiny::selectInput(ns('selected_module'),'select module', choices=NULL),
+            shiny::actionButton(ns("compute"),"Compute!",icon=icon("running"),
+                         class="run-button"),            
+            shiny::br(),
+            shiny::br(),
+            shiny::actionLink(ns("options"), "Options", icon=icon("cog", lib = "glyphicon")),
+            shiny::br(),br(),br(),
+            shiny::conditionalPanel(
+                "input.options % 2 == 1", ns=ns,
+                shiny::tagList(
+                    shiny::selectInput(ns("ngenes"),"Number genes:",
+                                choices = c(500,1000,2000,4000,8000),
+                                selected = 1000),
+                    shiny::selectInput(ns("minmodsize"),"Min. module size",
+                                choices = c(10,30,100,250),
+                                selected = 30 ),
+                    shiny::selectInput(ns("power"),"Power", c(2,4,6,10), selected=6),
+                    shiny::selectInput(ns("deepsplit"),"deepsplit", 0:4, selected=2),
+                    shiny::selectInput(ns("cutheight"),"Merge cut height",
+                                choices = c(0.05, 0.10, 0.25, 0.5, 0.9, 0.999),
+                                selected = 0.25)
+                )
+            )
+        )
     )
 }
 
