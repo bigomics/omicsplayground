d2fd1a7f82c52f9d1c9cd510fbeb58adbf062489
ivokwee <ivo.kwee@gmail.com>
2022-04-01 17:50:11 +0200

    remove old files

--- docker/start-orca.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker network rm omicsplayground-net
-docker network create omicsplayground-net
-docker run -d -p 9091:9091 \
-       --restart on-failure:10 \
-       --network omicsplayground-net \
-       --name orca-server \
-       quay.io/plotly/orca
