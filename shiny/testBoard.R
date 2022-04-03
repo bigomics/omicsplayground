b3ce690e25803046d25474b798465d81deb4f706
ivokwee <ivo.kwee@gmail.com>
2022-04-03 21:45:10 +0200

    add testBoard.R
    Former-commit-id: 40b8d88a9b27e6c28604426dc7b3d59048af9152 [formerly c4f50c02062c7721ce0fdb403d6cff2a6f0b2cac]
    Former-commit-id: 629a9f6c334645e096d371a56aef8c51fb53ce44

---
+++ shiny/testBoard.R
@@ -0,0 +1,30 @@
+##
+## This file is part of the Omics Playground project.
+## Copyright (c) 2018-2021 BigOmics Analytics Sagl. All rights reserved.
+##
+
+source("./global.R")  ## global variable
+
+load("../data/example-data.pgx",verbose=1)
+ngs = pgx.initialize(ngs)
+
+ui = fluidPage(
+    tabView("DataView",DataViewInputs("view"),DataViewUI("view"))
+)
+
+server = function(input, output, session) {
+
+    filterStates <- list(
+        search_gene = "ETAA1",
+        data_samplefilter = NULL,
+        data_type = "logCPM",
+        data_groupby = "condition"
+    )
+
+    inputData <- shiny::reactive(ngs)
+    shiny::callModule(DataViewBoard, "view", inputData, filterStates)
+}
+
+shiny::shinyApp(ui=ui, server=server)
+
+
