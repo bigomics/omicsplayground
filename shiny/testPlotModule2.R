3ebad58b78d886704917e08d31d489cc2ddb6e15
ivokwee <ivo.kwee@gmail.com>
2022-04-03 04:48:57 +0200

    add cachedexpr; add 2nd example
    Former-commit-id: 7c88e37256418236a27ff85b589d2851cc4e37b6 [formerly 2ac8dc7ae3ec1d8bc1df9176ec0ed19f46d00eb8]
    Former-commit-id: 517885f1a8ce886d67406a0b4f6e951aff68276f

--- shiny/testPlotModule2.R
+++ shiny/testPlotModule2.R
@@ -3,39 +3,50 @@
 ## Copyright (c) 2018-2021 BigOmics Analytics Sagl. All rights reserved.
 ##
 
+library(shiny)
 source("./global.R")  ## global variable
 source("./modules/plotModules/dataviewTSNEPlotModule2.R", encoding = "UTF-8")
 
 load("../data/example-data.pgx",verbose=1)
 ngs = pgx.initialize(ngs)
+colnames(ngs$Y)  ## possible group vectors
+genes <- rownames(ngs$X)
 
 ui = fluidPage(
     fillCol(
+        flex = c(1,1),
+        height = 800,
         fillRow(
+            tagList(
+                selectInput("gene","Gene",genes),
+                selectInput("groupby","Group",colnames(ngs$Y))
+            ),
             plotWidget("tSNEPlot"),
-            br(),br(),br()
+            br(),br()
         ),
-        fillRow(
-            br(),br(),br(),br()
-        )
+        fillRow()
     )
 )
 
 server = function(input, output, session) {
 
-    colnames(ngs$Y)  ## possible group vectors
-
-    filterStates <- list(
-        search_gene = "ETAA1",
-        data_samplefilter = NULL,
-        data_type = "logCPM",
-        data_groupby = "condition"
-    )
+    filterList <- reactive({
+        list(
+            search_gene = input$gene,
+            data_samplefilter = NULL,
+            data_type = "logCPM",
+            data_groupby = input$groupby
+        )
+    })
+    
     dataviewtSNEplotModule(
-        "tSNEPlot", filterStates, ngs,
+        "tSNEPlot", filterList, ngs,
         label="A", imgH=400, watermark=FALSE
     )
+        
 }
 
 shiny::shinyApp(ui, server=server)
+
+
 
