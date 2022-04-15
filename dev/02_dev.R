fc23c8e767cb1a2b7116b19aa4a845e1408da913
ivokwee <ivo.kwee@gmail.com>
2022-04-16 01:00:45 +0200

    add dev tests vignettes folders
    Former-commit-id: 2f5b8f2f9b714c9c6a029d9eb4da482135ffc7a8 [formerly 0d71e9c7324dfc02f07948b5451c417690876d3f]
    Former-commit-id: ae5c6aa0c7cda32ad15e110e595df11a12a856aa

---
+++ dev/02_dev.R
@@ -0,0 +1,105 @@
+# Building a Prod-Ready, Robust Shiny Application.
+#
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+#
+#
+###################################
+#### CURRENT FILE: DEV SCRIPT #####
+###################################
+
+setwd("~/R/golem/testpackage/")  ## set to package root!!
+
+# Hack for subfolders
+source("dev/create_headers.R")
+create_headers(c('R','shiny'), hdr.file='R/00Headers.R')
+
+## Dependencies ----
+## Amend DESCRIPTION with dependencies read from package code parsing
+attachment::att_amend_desc()
+file.show('NAMESPACE')
+
+## Add modules ----
+## Create a module infrastructure in R/
+golem::add_module(name = "module1", with_test = TRUE) # Name of the module
+golem::add_module(name = "module2", export=TRUE, with_test = TRUE) # Name of the module
+
+## Add helper functions ----
+## Creates fct_* and utils_*
+golem::add_fct("helpers", with_test = TRUE)
+golem::add_utils("helpers", with_test = TRUE)
+
+## External resources
+## Creates .js and .css files at inst/app/www
+golem::add_js_file("script")
+golem::add_js_handler("handlers")
+golem::add_css_file("custom")
+golem::add_sass_file("custom")
+
+## Add internal datasets ----
+## If you have data in your package
+usethis::use_data_raw(name = "my_dataset", open = FALSE)
+
+## Tests ----
+## Add one line by test you want to create
+usethis::use_test("app")
+
+# Documentation
+
+## Vignette ----
+usethis::use_vignette("testpackage")
+usethis::use_vignette("testpackage-howto")
+devtools::build_vignettes()     ## builds vignettes in doc folder
+devtools::build_manual(path='doc')    ## builds PDF reference 
+devtools::document()            ## builds Rd in man folder
+
+
+## Code Coverage----
+## Set the code coverage service ("codecov" or "coveralls")
+usethis::use_coverage()
+
+# Create a summary readme for the testthat subdirectory
+##remotes::install_github('yonicd/covrpage')
+covrpage::covrpage()
+
+## CI ----
+## Use this part of the script if you need to set up a CI
+## service for your application
+##
+## (You'll need GitHub there)
+usethis::use_github()
+
+# GitHub Actions
+usethis::use_github_action()
+# Chose one of the three
+# See https://usethis.r-lib.org/reference/use_github_action.html
+usethis::use_github_action_check_release()
+usethis::use_github_action_check_standard()
+usethis::use_github_action_check_full()
+# Add action for PR
+usethis::use_github_action_pr_commands()
+
+# Travis CI
+usethis::use_travis()
+usethis::use_travis_badge()
+
+# AppVeyor
+usethis::use_appveyor()
+usethis::use_appveyor_badge()
+
+# Circle CI
+usethis::use_circleci()
+usethis::use_circleci_badge()
+
+# Jenkins
+usethis::use_jenkins()
+
+# GitLab CI
+usethis::use_gitlab_ci()
+
+# You're now set! ----
+# go to dev/03_deploy.R
+rstudioapi::navigateToFile("dev/03_deploy.R")
---
+++ dev/create_headers.R
@@ -0,0 +1,46 @@
+create_headers <- function(path='R', hdr.file="R/00Headers.R") {
+
+    message("Creating header file: ",hdr.file)
+    rfiles <- list.files(path,pattern='.[rR]$', recursive=TRUE, full.names=TRUE)
+    ##hdr.file <- file.path(path,'R','00Headers.R')
+
+    ## exclude some files
+    rfiles <- setdiff(rfiles,hdr.file)
+    rfiles <- grep("global.R|00Headers.R",rfiles,invert=TRUE,value=TRUE)
+    
+    write('# Generated automatically: do not edit by hand', hdr.file, append=FALSE)    
+    i=1
+    for(i in 1:length(rfiles)) {
+        code <- readLines(rfiles[i])
+        nn <- grep('^#\'',code)
+        nn <- unique(c(nn,nn+1))
+        nn <- nn[which(nn >=1 & nn <= length(code))]
+        hdr <- code[sort(nn)]
+        hdr <- gsub("function\\(.*","function(){}",hdr)
+        write(hdr, hdr.file, append=TRUE)
+        src <- paste0("source(\'",rfiles[i],"\',local=TRUE)\n")
+        write(src, hdr.file, append=TRUE)
+    }
+}
+
+create_headers.SAVE <- function(path='.') {
+    message("Creating header file R/00Headers.R")
+    rfiles <- list.files(file.path(path,'R'),pattern='.[rR]$', recursive=TRUE, full.names=TRUE)
+    hdr.file <- file.path(path,'R','00Headers.R')
+    rfiles <- setdiff(rfiles,hdr.file)
+    write('# Generated automatically: do not edit by hand', hdr.file, append=FALSE)    
+    i=1
+    for(i in 1:length(rfiles)) {
+        code <- readLines(rfiles[i])
+        nn <- grep('^[#\']',code)
+        nn <- unique(c(nn,nn+1))
+        nn <- nn[which(nn >=1 & nn <= length(code))]
+        hdr <- code[sort(nn)]
+        hdr <- gsub("function\\(.*","function(){}",hdr)
+        write(hdr, hdr.file, append=TRUE)
+        src <- paste0("source(\'",rfiles[i],"\',local=TRUE)\n")
+        write(src, hdr.file, append=TRUE)
+    }
+}
+
+
