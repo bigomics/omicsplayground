4a4454f6182e1471e82cae74b03bff7297152fbc
JohnCoene <jcoenep@gmail.com>
2022-05-03 21:19:44 +0200

    refactor: css

--- components/board.loading/R/loading_ui.R
+++ components/board.loading/R/loading_ui.R
@@ -23,45 +23,30 @@ LoadingInputs <- function(id) {
 LoadingUI <- function(id) {
     ns <- shiny::NS(id)  ## namespace
 
-    shiny::tagList(
-        shiny::fillCol(
-          height = 750,
-          flex = c(NA,NA,NA,NA,NA,1),
-          ## shiny::fillRow(
-          ##   height=115,
-          ##   shiny::uiOutput(ns("valuebox1")),
-          ##   shiny::uiOutput(ns("valuebox2")), 
-          ##   shiny::uiOutput(ns("valuebox3"))
-          ## ),
-          uiOutput(ns("navheader")),
-          br(), br(),
-          div( id="load-action-buttons",
-          shiny::fillRow(
-            flex = c(NA,NA,NA,NA,1),
-            withTooltip(
-              shiny::actionButton(
-                ns("loadbutton"), label="Load dataset", icon=icon("file-import"),
-                class="btn btn-primary mx-2"),
-              "Click to load the selected dataset.", placement="bottom"),
-            withTooltip( shiny::downloadButton(
-              ns("downloadpgx"), label="Download PGX", ##icon=icon("download"),
-              class="btn btn-outline-primary mx-2")
-             ,"Download PGX file (binary).", placement="bottom"),
-            withTooltip( downloadButton2(
-              ns("downloadzip"), label="Download ZIP", icon=icon("file-csv"),
-              class="btn btn-outline-primary mx-2")
-             ,"Download CSV files (counts.csv, samples.csv, contrasts.csv).",
-              placement="bottom"),
-            withTooltip( shiny::actionButton(
-              ns("deletebutton"), label="Delete dataset", icon=icon("trash"),
-              class="btn btn-outline-primary mx-2")
-             ,"Delete the selected dataset.", placement="bottom"),
-            br()
-          )),
-
-          tableWidget(ns("pgxtable")),
-
-          br()
-        )
+    div(
+      class = "p-1",
+        uiOutput(ns("navheader")),
+        br(), br(),
+        div( id="load-action-buttons",
+        withTooltip(
+          shiny::actionButton(
+            ns("loadbutton"), label="Load dataset", icon=icon("file-import"),
+            class="btn btn-primary mx-2"),
+          "Click to load the selected dataset.", placement="bottom"),
+        withTooltip( shiny::downloadButton(
+          ns("downloadpgx"), label="Download PGX", ##icon=icon("download"),
+          class="btn btn-outline-primary mx-2")
+          ,"Download PGX file (binary).", placement="bottom"),
+        withTooltip( downloadButton2(
+          ns("downloadzip"), label="Download ZIP", icon=icon("file-csv"),
+          class="btn btn-outline-primary mx-2")
+          ,"Download CSV files (counts.csv, samples.csv, contrasts.csv).",
+          placement="bottom"),
+        withTooltip( shiny::actionButton(
+          ns("deletebutton"), label="Delete dataset", icon=icon("trash"),
+          class="btn btn-outline-primary mx-2")
+          ,"Delete the selected dataset.", placement="bottom")
+      ),
+      tableWidget(ns("pgxtable"))
     )
 }
