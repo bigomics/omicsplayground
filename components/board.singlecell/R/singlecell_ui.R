87b9fb0d15beb9217756d7b4948967679e6d0375
John Coene <jcoenep@gmail.com>
2022-05-28 09:36:55 +0200

    refactor: remove flex for bootstrap grid

--- components/board.singlecell/R/singlecell_ui.R
+++ components/board.singlecell/R/singlecell_ui.R
@@ -38,83 +38,81 @@ SingleCellUI <- function(id) {
         shiny::tabsetPanel(
             id = ns("tabs"),
             shiny::tabPanel("Cell type",
-                shiny::fillRow(
-                flex = c(1.5,0.08,1),
-                shiny::fillCol(
-                    height = 750,
-                    flex = c(NA,1),
-                    tags$div(
-                    HTML("<strong>Cell type profiling</strong> infers the type of cells using computational deconvolution
-                        methods and reference datasets from the literature. Currently, we have implemented a total
-                        of 8 methods and 9 reference datasets to predict immune cell types (4 datasets),
-                        tissue types (2 datasets), cell lines (2 datasets) and cancer types (1 dataset).
-                        However, we plan to expand the collection of methods and databases and to infer other cell types.")
+                div(
+                    class = "row",
+                    div(
+                        class = "col-md-6",
+                        tags$div(
+                            HTML(
+                                "<strong>Cell type profiling</strong> infers the type of cells using computational deconvolution
+                                methods and reference datasets from the literature. Currently, we have implemented a total
+                                of 8 methods and 9 reference datasets to predict immune cell types (4 datasets),
+                                tissue types (2 datasets), cell lines (2 datasets) and cancer types (1 dataset).
+                                However, we plan to expand the collection of methods and databases and to infer other cell types."
+                            )
+                        ),
+                        plotWidget(ns("icpplot"))
                     ),
-                    plotWidget(ns("icpplot"))
-                ),
-                shiny::br(),
-                shiny::fillCol(
-                    height = 750,
-                    flex = c(NA,1), 
-                    tags$div(
-                    HTML("<b>Phenotype plots.</b> The plots show the distribution of the phenotypes superposed on the
-                     t-SNE clustering. Often, we can expect the t-SNE distribution to be driven by the particular
-                      phenotype that is controlled by the experimental condition or unwanted batch effects.")
-                    ),
-                    plotWidget(ns("phenoplot"))
+                    div(
+                        class = "col-md-6",
+                        tags$div(
+                            HTML(
+                                "<b>Phenotype plots.</b> The plots show the distribution of the phenotypes superposed on the
+                                t-SNE clustering. Often, we can expect the t-SNE distribution to be driven by the particular
+                                phenotype that is controlled by the experimental condition or unwanted batch effects."
+                            )
+                        ),
+                        plotWidget(ns("phenoplot"))
+                    )
                 )
-            )),
+            ),
             shiny::tabPanel("Mapping",
-                shiny::fillRow(
-                flex = c(1.3,0.08,1),
-                shiny::fillCol(
-                    height = 750,
-                    flex = c(NA,1),
-                    tags$div(
-                    HTML("<b>Cell type mapping.</b> The inferred cell types can be by matched to the phenotype variable
-                    of the data set. The reference set can be a cell type reference database but also cancer types,
-                    tissue types or cell lines.")
+                div(
+                    class = "row",
+                    div(
+                        class = "col-md-6",
+                        tags$div(
+                            HTML("<b>Cell type mapping.</b> The inferred cell types can be by matched to the phenotype variable
+                            of the data set. The reference set can be a cell type reference database but also cancer types,
+                            tissue types or cell lines.")
+                        ),
+                        plotWidget(ns("mappingplot"))
                     ),
-                    plotWidget(ns("mappingplot"))
-                ),
-                shiny::br(),
-                shiny::fillCol(
-                    height = 750,
-                    flex = c(NA,1),
-                    tags$div(
-                    HTML("<b>Proportion plot.</b> Plot visualizing the overlap between two categorical variables
-                    (so-called cross tabulation). Although this feature is very suitable for a single-cell sequencing data,
-                    it provides useful information about the proportion of different cell types in samples
-                    obtained by the bulk sequencing method.")
-                    ),
-                    plotWidget(ns("crosstabPlot"))
+                    div(
+                        class = "col-md-6",
+                        tags$div(
+                            HTML("<b>Proportion plot.</b> Plot visualizing the overlap between two categorical variables
+                            (so-called cross tabulation). Although this feature is very suitable for a single-cell sequencing data,
+                            it provides useful information about the proportion of different cell types in samples
+                            obtained by the bulk sequencing method.")
+                        ),
+                        plotWidget(ns("crosstabPlot"))
+                    )
                 )
-            )),
+            ),
             shiny::tabPanel("Markers",
-                shiny::fillRow(
-                flex = c(1.3,0.08,1),
-                shiny::fillCol(
-                    height = 750,
-                    flex = c(NA,1),
-                    tags$div(
-                    HTML("<b>T-SNE distribution of expression of marker genes.</b> Good biomarkers will show a
-                    distribution pattern strongly correlated with some phenotype. The top genes with the highest
-                    standard deviation are shown. The red color shading is proportional to the (absolute)
-                    expression of the gene in corresponding samples.")
+                div(
+                    class = "row",
+                    div(
+                        class = "col-md-6",
+                        tags$div(
+                            HTML("<b>T-SNE distribution of expression of marker genes.</b> Good biomarkers will show a
+                            distribution pattern strongly correlated with some phenotype. The top genes with the highest
+                            standard deviation are shown. The red color shading is proportional to the (absolute)
+                            expression of the gene in corresponding samples.")
+                        ),
+                        plotWidget(ns("markersplot")) 
                     ),
-                    plotWidget(ns("markersplot")) 
-                ),
-                shiny::br(),
-                shiny::fillCol(
-                    height = 750,
-                    flex = c(NA,1),
-                    tags$div(
-                    HTML("<b>Cyto plot.</b> This plot shows the distribution of samples in relation to the expression
-                    of selected gene pairs. It mimics the scatter plots used for gating in flow cytometry analysis.")
-                    ),
-                    plotWidget(ns("cytoplot"))
+                    div(
+                        class = "col-md-6",
+                        tags$div(
+                            HTML("<b>Cyto plot.</b> This plot shows the distribution of samples in relation to the expression
+                            of selected gene pairs. It mimics the scatter plots used for gating in flow cytometry analysis.")
+                        ),
+                        plotWidget(ns("cytoplot"))
+                    )
                 )
-            ))
+            )
             # These tabPanels are not shown in the UI Stefan 22.03.2022
             # shiny::tabPanel("CNV",
             #     shiny::fillCol(
