b9d8d9c3154994a9efc0751ed499fbe0e9b534ff
z3tt <cedricphilippscherer@gmail.com>
2022-10-07 20:19:07 +0200

    polish count boxplots

--- components/board.dataview/R/dataview_plot_boxplot.R
+++ components/board.dataview/R/dataview_plot_boxplot.R
@@ -73,19 +73,35 @@ dataview_plot_boxplot_server <- function(id, parent.input, getCountsTable, water
 
             df <- res$counts[,]
             long.df <- reshape2::melt(df)
-            colnames(long.df) <- c("gene","sample","value")
+            colnames(long.df) <- c("gene", "sample", "value")
             
             ## boxplot
-            fig <- plotly::plot_ly(
+            fig <- 
+              plotly::plot_ly(
                 long.df,
                 x = ~sample,
                 y = ~value,
-                type = "box"
-            ) %>%
-                plotly::layout(
-                    yaxis = list(title = "counts (log2)"),
-                    xaxis = list(title = "")
-                )
+                type = "box",
+                # color = omics_colors("mid_blue"),
+                marker = list(
+                  color = omics_colors("mid_blue"),
+                  fillcolor = omics_colors("light_blue")
+                ),
+                line = list(color = omics_colors("mid_blue")),
+                hoverinfo = "y"
+                # hovertemplate = ~paste0(
+                #   "Sample: <b>", sample,
+                #   "</b><br>Counts (log2): <b>", value,
+                #   "</b><extra></extra>"
+                # )
+              ) %>%
+              plotly::layout(
+                yaxis = list(title = "Counts (log2)", hoverformat = '.2f'),
+                xaxis = list(title = FALSE),
+                font = list(family = "Lato"),
+                margin = list(l = 10, r = 10, b = 10, t = 10)   
+              ) %>% 
+              plotly_default1()
             fig
         }
                
