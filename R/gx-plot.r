da96c195ef859d38754daefa88ef987cbe243a72
Otho Mantegazza <otho.mantegazza@gmail.com>
2022-11-15 17:31:17 +0100

    refactor and redesign beeswarm

--- R/gx-plot.r
+++ R/gx-plot.r
@@ -76,123 +76,168 @@ gx.barplot <- function(x, main="", cex.main=1.2, cex.names=0.85,
 }
 
 
-##x=gx;y=ngs$samples$group;
-##bee=bar=TRUE;offx=3;sig.stars=FALSE;xoff=0;srt=60;max.points=-1;ymax=NULL;bee.cex=0.3;max.stars=5
-gx.b3plot <- function(x, y, first=NULL,
-                      width=1, bar=TRUE, bee=TRUE, sig.stars=FALSE,
-                      ymax=NULL, bee.cex=0.3, max.stars=5, srt=NULL, xoff=0,
-                      names.cex=1, names=TRUE, max.points=100, col="grey80",
+gx.b3plot <- function(x, y,
+                      first = NULL,
+                      width = 1,
+                      bar = TRUE,
+                      bee = TRUE,
+                      sig_stars = FALSE,
+                      ymax = NULL,
+                      bee_cex = 2,
+                      max_stars = 5,
+                      srt = NULL,
+                      xoff = 0,
+                      names_cex = 1,
+                      names = TRUE,
+                      max_points = 100,
+                      col = "#a6cee3F0",
+                      mult_x_lim = 0.11,
+                      mult_y_lim = 1.3,
+                      ylab = 'CPM',
+                      xlab = '',
+                      pch = 19,
                       ...)
 {
-    
-    ##
-    stats.segments <- function(x, y, xoffset=0, lwd=2) {
-        bx = boxplot(y ~ x, plot=FALSE)
-        nx = length(bx$n)
-        x0 = xoffset + (1:nx)
-        ##segments( x0-0.2, bx$stats[3,], x0+0.2, bx$stats[3,],lwd=lwd*1.5)
-        segments( x0-0.1, bx$conf[1,],  x0+0.1, bx$conf[1,],lwd=lwd)
-        segments( x0-0.1, bx$conf[2,],  x0+0.1, bx$conf[2,],lwd=lwd)
-        segments( x0,     bx$conf[1,],  x0,     bx$conf[2,],lwd=lwd*0.5)
+    # browser()
+
+    # use the boxplot function to extract whiskers --------------------
+    stats_segments <- function(x, y,
+                               xoffset = 0,
+                               lwd = 2) {
+        bx <- boxplot(y ~ x, plot = FALSE)
+        nx <- length(bx$n)
+        x0 <- xoffset + (1:nx)
+
+        segments(x0 - 0.1, bx$conf[1, ], x0 + 0.1, bx$conf[1, ], lwd = lwd)
+        segments(x0 - 0.1, bx$conf[2, ], x0 + 0.1, bx$conf[2, ], lwd = lwd)
+        segments(x0, bx$conf[1, ], x0, bx$conf[2, ], lwd = lwd * 0.5)
     }
+
+    # Define NAs in factor levels ---------------------------------------
     ylevel <- levels(y)
-    y = as.character(y)
-    if(any(is.na(y))) {
+    y <- as.character(y)
+
+    if (any(is.na(y))) {
         y[is.na(y)] <- 'NA'
         ylevel <- c(ylevel,'NA')
     }
-    y = factor(y, levels=ylevel, exclude=NULL)
-    if(!is.null(first)) y <- relevel(y, ref=first)
-    mx = tapply(x, y, median, na.rm=TRUE)
 
-    sig = yc = NULL
-    if(sig.stars) {
-        y.levels = unique(y)
-        yc = combn(y.levels,2)
-        pv <- rep(NA,ncol(yc))
-        i=1
-        for(i in 1:ncol(yc)) {
-            grp = yc[,i]
-            pv[i] = t.test(x[which(y==grp[1])], x[which(y==grp[2])])$p.value
+    y <- factor(y, levels = ylevel, exclude = NULL)
+    if (!is.null(first)) y <- relevel(y, ref = first)
+
+    # Condition medians --------------------------------------------------
+    mx <- tapply(x, y, median, na.rm = TRUE)
+
+    # ??? ----------------------------------------------------------------
+    sig <- yc <- NULL
+
+
+    # Not sure about this part -------------------------------------------
+    # does it add t-test stars? Is it ever used? -------------------------
+    # if (sig_stars) {
+    #     y_levels <- unique(y)
+    #     yc <- combn(y_levels, 2)
+    #     pv <- rep(NA, ncol(yc))
+    #     i <- s1
+    #     for (i in 1:ncol(yc)) {
+    #         grp <- yc[, i]
+    #         pv[i] <- t.test(x[which(y == grp[1])],
+    #                         x[which(y == grp[2])])$p.value
+    #     }
+    #     pv
+    #     sig <- c("", "*", "**", "***")[1 + 1 * (pv < 0.05) + 1 * (pv < 0.01) + 1 * (pv < 0.001)]
+    #     sig
+    #     nthree <- sum(sig == "***")
+    #     jj <- which(sig != "")
+    #     jj <- jj[order(pv[jj])] ## only top 4 ??
+    #     jj <- Matrix::head(jj, max(max.stars, nthree)) ## only top 5 ??
+    #     j <- 1
+    #     yc <- apply(yc, 2, as.integer)
+    #     yc
+    #     dd <- abs(as.vector(diff(yc)))
+    #     jj <- jj[order(dd[jj])]
+    #     sig <- sig[jj]
+    #     yc <- yc[, jj, drop = FALSE]
+    # }
+
+    # compute graphical paramenters ------------------------------------
+    dx <- max(x, na.rm = TRUE) * mult_x_lim
+    ylim <- c(xoff, max(x) * mult_y_lim)
+    if (!is.null(ymax)) ylim <- c(xoff, ymax)
+    if (min(x) < 0) {
+        ylim  <- c(mult_y_lim * min(c(x, xoff)),
+                   max(x) * mult_y_lim)
         }
-        pv
-        sig = c("","*","**","***")[ 1 + 1*(pv<0.05) + 1*(pv<0.01) + 1*(pv<0.001) ]
-        sig
-        nthree = sum(sig=="***")
-        jj = which(sig!="")
-        jj = jj[order(pv[jj])]  ## only top 4 ??
-        jj = Matrix::head(jj,max(max.stars,nthree)) ## only top 5 ??
-        j=1
-        yc = apply(yc, 2, as.integer)
-        yc
-        dd = abs(as.vector(diff(yc)))
-        jj = jj[order(dd[jj])]
-        sig = sig[jj]
-        yc = yc[,jj,drop=FALSE]
-    }
-    
-    ##dx = (max(x,na.rm=TRUE)-min(x,na.rm=TRUE))*0.11
-    dx = max(x,na.rm=TRUE)*0.11
-    ylim = c(xoff,max(x)*1.3)
-    if(!is.null(ymax)) ylim = c(xoff,ymax)
-    if(min(x) < 0) ylim = c(1.3*min(c(x,xoff)),max(x)*1.3)
-    if(sig.stars) {
-        if(ncol(yc) > 8) dx = dx/5
-        ylim[2] = ylim[2]*1.05 + (2+NCOL(yc))*dx
-    }
 
-    if(length(col)==1) {
-        col <- rep(col,length(mx))
+    if(length(col) == 1) {
+        col <- rep(col, length(mx))
         names(col) <- names(mx)
     } else {
-        col <- col[match(names(mx),names(col))]
+        col <- col[match(names(mx), names(col))]
         col[is.na(col)] <- "grey90"
     }
-    
-    ##par(mfrow=c(1,1));srt=60
-    ##bx = barplot( mx-xoff, width=0.6666, space=0.5, ylim=ylim, offset=xoff, names.arg=NA)
-    bx = barplot( mx, width=0.6666, space=0.5, ylim=ylim, offset=xoff,
-                 names.arg=NA, col=col, ... )
-    if(is.null(srt)) {
-        nnchar <- sum(sapply(unique(y),nchar))
-        srt <- ifelse(nnchar > 24, 30, 0)
-    }
-    pos <- ifelse(srt==0, 1, 2)
 
-    n = length(unique(y))
-    if(names==TRUE) {
-        y0 = min(ylim) - diff(ylim)*0.05
-        text( bx[,1], y0, names(mx), cex=names.cex,
-             srt=srt, adj=ifelse(srt==0,0.5,0.965), xpd=TRUE,
-             pos=pos, offset=0)
-    }
-    if(bee) {
-        jj <- 1:length(x)
-        ##jj <- which(y %in% names(which(table(y)>2)))
-        ##j1 <- which(table(jj)==1)
-        ##if(length(j1)) jj <- c(jj,j1)
-        if(max.points>0 && length(jj)>max.points) {
-            ## jj <- sample(jj,max.points)
-            jj <- unlist(tapply(jj, y, function(i) Matrix::head(sample(i),max.points)))
+    # plot beeswarm -----------------------------------------------------
+    jj <- 1:length(x)
+    if (max_points > 0 &&
+          length(jj) > max_points) {
+            jj <- unlist(
+                    tapply(jj, y, function(i) {
+                        Matrix::head(sample(i),
+                                     max_points)
+                      })
+                    )
         }
-        ## !!!!!!!!! NEED CHECK!! can be very slow if jj is large !!!!!!!!!!!
-        beeswarm::beeswarm(x[jj] ~ y[jj], add=TRUE, at=1:n-0.33, pch=19, cex=bee.cex, col="grey20")
-        ## sinaplot( x[jj] ~ y[jj], add=TRUE, pch=19, cex=bee.cex, col="grey20")
-    }
-    if(bar) stats.segments(y, x, xoffset=-0.333, lwd=1.4)
 
-    if(sig.stars) {
-        i=1
-        for(i in 1:NCOL(yc)) {
-            grp = yc[,i]
-            xmax = max(x,na.rm=TRUE)*1.05 + dx*i
-            j1 = grp[1] - 0.4
-            j2 = grp[2] - 0.4
-            segments( j1, xmax, j2, xmax, lwd=0.5)
-            if(ncol(yc)<=8)
-                text((j1+j2)/2, xmax, labels=sig[i], pos=1, offset=-0.33, adj=0, cex=1.4)
-        }
-    }
+    # svg()
+    x_levels <- factor(ylevel,
+                       levels = ylevel)
+    boxplot(mx ~ x_levels,
+            ylim = ylim,
+            xlab = xlab,
+            ylab = ylab,
+            notch = TRUE, # make median lines smaller
+            frame.plot = FALSE,
+            offset = xoff)
+    beeswarm::beeswarm(x[jj] ~ y[jj],
+                       ylim = ylim,
+                       pch = pch,
+                       cex = bee_cex,
+                       corral = "random",
+                       col = col,
+                       add = TRUE)
+    # dev.off()
+
+
+    # if(is.null(srt)) {
+    #     nnchar <- sum(sapply(unique(y),nchar))
+    #     srt <- ifelse(nnchar > 24, 30, 0)
+    # }
+    # 
+    # pos <- ifelse(srt==0, 1, 2)
+    # 
+    # n = length(unique(y))
+    # if(names==TRUE) {
+    #     y0 = min(ylim) - diff(ylim)*0.05
+    #     text( bx[,1], y0, names(mx), cex=names_cex,
+    #          srt=srt, adj=ifelse(srt==0,0.5,0.965), xpd=TRUE,
+    #          pos=pos, offset=0)
+    # }
+    # 
+    # if(bar) 
+    # 
+    # if(sig_stars) {
+    #     i=1
+    #     for(i in 1:NCOL(yc)) {
+    #         grp = yc[,i]
+    #         xmax = max(x,na.rm=TRUE)*1.05 + dx*i
+    #         j1 = grp[1] - 0.4
+    #         j2 = grp[2] - 0.4
+    #         segments( j1, xmax, j2, xmax, lwd=0.5)
+    #         if(ncol(yc)<=8)
+    #             text((j1+j2)/2, xmax, labels=sig[i], pos=1, offset=-0.33, adj=0, cex=1.4)
+    #     }
+    # }
 }
 
 gx.hist <- function(gx, main="",ylim=NULL) {
