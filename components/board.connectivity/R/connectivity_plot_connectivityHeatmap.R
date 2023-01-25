8a93002dcee0d38098391df5c7b966f44497313c
ncullen93 <nicholas.cullen@med.lu.se>
2023-01-25 13:56:14 +0100

    created template files for all figures/tables to be converted in board.connectivity

---
+++ components/board.connectivity/R/connectivity_plot_connectivityHeatmap.R
@@ -0,0 +1,168 @@
+##
+## This file is part of the Omics Playground project.
+## Copyright (c) 2018-2022 BigOmics Analytics Sagl. All rights reserved.
+##
+
+#' Importance plot UI input function
+#'
+#' @description A shiny Module for plotting (UI code).
+#'
+#' @param id
+#' @param label
+#' @param height
+#'
+#' @export
+connectivity_plot_connectivityHeatmap_ui <- function(id,
+                                          label = "",
+                                          rowH = 660) {
+  ns <- shiny::NS(id)
+  info_text <- strwrap("<strong>KEGG pathways</strong> are a collection of
+    manually curated pathways representing the current knowledge of molecular
+    interactions, reactions and relation networks as pathway maps. In the
+    pathway map, genes are colored according to their upregulation (red) or
+    downregulation (blue) in the contrast profile. Each pathway is scored for
+    the selected contrast profile and reported in the table below.")
+
+  PlotModuleUI(ns("plot"),
+               title = "Kegg pathway map",
+               label = label,
+               plotlib = "image",
+               info.text = info_text,
+               info.width = "350px",
+               options = NULL,
+               download.fmt = "png",
+               height = c(0.53 * rowH, 700),
+               width = c("100%", 1280),
+  )
+}
+
+#' Importance plot Server function
+#'
+#' @description A shiny Module for plotting (server code).
+#'
+#' @param id
+#'
+#' @return
+#' @export
+connectivity_plot_connectivityHeatmap_server <- function(id,
+                                              inputData,
+                                              getFilteredKeggTable,
+                                              kegg_table,
+                                              fa_contrast,
+                                              watermark = FALSE) {
+  moduleServer(
+    id, function(input, output, session) {
+      plot_data <- shiny::reactive({
+        res <- list(
+          pgx = inputData(),
+          df = getFilteredKeggTable(),
+          kegg_table = kegg_table,
+          fa_contrast = fa_contrast()
+        )
+        return(res)
+      })
+
+      plot_RENDER <- shiny::reactive({
+        res <- plot_data()
+        pgx <- res$pgx
+        df <- res$df
+        fa_contrast <- res$fa_contrast
+        kegg_table <- res$kegg_table
+
+        ###############
+
+        NULL.IMG <- list(src = "", contentType = "image/png")
+        if (is.null(pgx)) {
+          return(NULL.IMG)
+        }
+
+        comparison <- fa_contrast
+        if (is.null(comparison) || length(comparison) == 0) {
+          return(NULL.IMG)
+        }
+        if (comparison == "") {
+          return(NULL.IMG)
+        }
+
+        ## get fold-change vector
+        fc <- pgx$gx.meta$meta[[comparison]]$meta.fx
+        pp <- rownames(pgx$gx.meta$meta[[comparison]])
+
+        if ("hgnc_symbol" %in% colnames(pgx$genes)) {
+          names(fc) <- pgx$genes[pp, "hgnc_symbol"]
+        } else {
+          names(fc) <- toupper(pgx$genes[pp, "gene_name"])
+        }
+        fc <- fc[order(-abs(fc))]
+        fc <- fc[which(!duplicated(names(fc)) & names(fc) != "")]
+
+        ## get selected KEGG id
+        #df <- getFilteredKeggTable()
+        if (is.null(df)) {
+          return(NULL.IMG)
+        }
+
+        sel.row <- kegg_table$rows_selected()
+        if (is.null(sel.row) || length(sel.row) == 0) {
+          return(NULL.IMG)
+        }
+        sel.row <- as.integer(sel.row)
+
+        pathway.id <- "04110" ## CELL CYCLE
+        pathway.name <- pw.genes <- "x"
+        if (is.null(sel.row) || length(sel.row) == 0) {
+          return(NULL.IMG)
+        }
+
+        if (!is.null(sel.row) && length(sel.row) > 0) {
+          pathway.id <- df[sel.row, "kegg.id"]
+          pathway.name <- df[sel.row, "pathway"]
+          pw.genes <- unlist(getGSETS(as.character(pathway.name)))
+        }
+
+        ## folder with predownloaded XML files
+        xml.dir <- file.path(FILES, "kegg-xml")
+        xml.dir <- normalizePath(xml.dir) ## absolute path
+
+        ## We temporarily switch the working directory to always readable
+        ## TMP folder
+        curwd <- getwd()
+        tmpdir <- tempdir()
+        setwd(tmpdir)
+        pv.out <- pathview::pathview(
+          gene.data = fc, pathway.id = pathway.id, gene.idtype = "SYMBOL",
+          gene.annotpkg = "org.Hs.eg.db", species = "hsa",
+          out.suffix = "pathview", limit = list(gene = 2, cpd = 1),
+          low = list(gene = "dodgerblue2", cpd = "purple"),
+          high = list(gene = "firebrick2", cpd = "yellow"),
+          kegg.dir = xml.dir, kegg.native = TRUE, same.layer = FALSE
+        )
+        Sys.sleep(0.2) ## wait for graph
+
+        ## back to previous working folder
+        setwd(curwd)
+
+        outfile <- file.path(tmpdir, paste0("hsa", pathway.id, ".pathview.png"))
+        if (!file.exists(outfile)) {
+          return(NULL.IMG)
+        }
+
+        list(
+          src = outfile,
+          contentType = "image/png",
+          width = "100%", height = "100%", ## actual size: 1040x800
+          alt = "pathview image"
+        )
+      })
+
+      PlotModuleServer(
+        "plot",
+        plotlib = "image",
+        func = plot_RENDER,
+        func2 = plot_RENDER,
+        csvFunc = plot_data,
+        add.watermark = watermark
+      )
+    } ## end of moduleServer
+  )
+}
---
+++ components/board.connectivity/R/connectivity_table_connectivityScoreTable2.R
@@ -0,0 +1,117 @@
+##
+## This file is part of the Omics Playground project.
+## Copyright (c) 2018-2022 BigOmics Analytics Sagl. All rights reserved.
+##
+
+
+connectivity_table_connectivityScoreTable2_ui <- function(id) {
+  ns <- shiny::NS(id)
+  tableWidget(ns("table"))
+}
+
+
+connectivity_table_connectivityScoreTable2_server <- function(id,
+                                             inputData,
+                                             fa_contrast,
+                                             tabH,
+                                             selected_gsetmethods)
+{
+  moduleServer(id, function(input, output, session)
+  {
+    ns <- session$ns
+
+    matchGOid2gset <- function(id, gsets) {
+      gsets.id <- sub("\\)$", "", sub(".*\\(GO_", "GO:", gsets))
+      match(id, gsets.id)
+    }
+
+    table_data <- shiny::reactive({
+      res <- list(
+        pgx = inputData(),
+        fa_contrast = fa_contrast()
+      )
+      return(res)
+    })
+
+    table_RENDER <- function() {
+      res <- table_data()
+      pgx <- res$pgx
+      comparison <- res$fa_contrast
+
+      if (is.null(pgx$meta.go)) {
+        return(NULL)
+      }
+      if (is.null(comparison)) {
+        return(NULL)
+      }
+
+      go <- pgx$meta.go$graph
+      scores <- pgx$meta.go$pathscore[, comparison]
+
+      scores <- scores[which(!is.na(scores) & !is.infinite(scores))]
+      scores <- round(scores, digits = 3)
+      scores <- scores[order(-abs(scores))]
+      go.term <- igraph::V(go)[names(scores)]$Term
+
+      ## get FC and q-value.  match with enrichment table
+      gs.meta <- pgx$gset.meta$meta[[comparison]]
+      ii <- matchGOid2gset(names(scores), rownames(gs.meta))
+      gs.meta <- gs.meta[ii, , drop = FALSE]
+      gs.meta$GO.id <- rownames(scores)
+      mm <- selected_gsetmethods()
+      mm <- intersect(mm, colnames(gs.meta$q))
+      qv <- apply(gs.meta$q[, mm, drop = FALSE], 1, max, na.rm = TRUE) ## meta-q
+      fx <- gs.meta$meta.fx
+
+      go.term1 <- substring(go.term, 1, 80)
+      dt1 <- round(cbind(score = scores, logFC = fx, meta.q = qv), digits = 4)
+      dt <- data.frame(id = names(scores), term = go.term1, dt1, stringsAsFactors = FALSE)
+      id2 <- paste0("abc(", sub(":", "_", dt$id), ")") ## to match with wrapHyperLink
+      dt$id <- wrapHyperLink(as.character(dt$id), id2) ## add link
+
+      numeric.cols <- colnames(dt)[which(sapply(dt, is.numeric))]
+
+      DT::datatable(dt,
+                    rownames = FALSE, escape = c(-1, -2),
+                    class = "compact cell-border stripe hover",
+                    extensions = c("Scroller"),
+                    selection = list(mode = "single", target = "row", selected = 1),
+                    fillContainer = TRUE,
+                    options = list(
+                      dom = "lfrtip",
+                      scrollX = TRUE,
+                      scrollY = tabH, scroller = TRUE, deferRender = TRUE
+                    ) ## end of options.list
+      ) %>%
+        DT::formatSignif(numeric.cols, 4) %>%
+        DT::formatStyle(0, target = "row", fontSize = "11px", lineHeight = "70%") %>%
+        DT::formatStyle("score",
+                        background = color_from_middle(dt1[, "score"],
+                                                       "lightblue",
+                                                       "#f5aeae"),
+                                                       backgroundSize = "98% 88%", backgroundRepeat = "no-repeat",
+                        backgroundPosition = "center"
+        )
+    }
+
+    info_text <- strwrap("<strong>GO score table.</strong> The scoring of a GO
+                         term is performed by considering the cumulative score
+                         of all terms from that term to the root node. That
+                         means that GO terms that are supported by higher level
+                         terms levels are preferentially scored.")
+
+    table_opts <- shiny::tagList()
+
+    shiny::callModule(
+      tableModule,
+      id = "table",
+      label = "",
+      func = table_RENDER,
+      options = table_opts,
+      info.text = info_text,
+      title = "GO score table",
+      height = c(270, 700)
+    )
+
+  })  ## end of moduleServer
+} ## end of server
