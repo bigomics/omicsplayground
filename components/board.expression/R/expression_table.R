88ee2ad3c6cb4bb0e4e5cd851d164cfa10c8f733
Mauro Miguel Masiero <mauromiguelm@hotmail.com>
2023-01-28 15:10:52 +0100

    update expression_table to expression board

--- components/board.expression/R/expression_table.R
+++ components/board.expression/R/expression_table.R
@@ -1,55 +1,55 @@
-#' ##
-#' ## This file is part of the Omics Playground project.
-#' ## Copyright (c) 2018-2022 BigOmics Analytics Sagl. All rights reserved.
-#' ##
+##
+## This file is part of the Omics Playground project.
+## Copyright (c) 2018-2022 BigOmics Analytics Sagl. All rights reserved.
+##
+
+#' UI code for table code: expression board
 #'
-#' #' UI code for table code: expression board
-#' #'
-#' #' @param id
-#' #' @param label
-#' #' @param height
-#' #' @param width
-#' #'
-#' #' @export
-#' compare_table_corr_score_ui <- function(id,
-#'                                         label='',
-#'                                         height,
-#'                                         width) {
+#' @param id
+#' @param label
+#' @param height
+#' @param width
 #'
-#'   ns <- shiny::NS(id)
+#' @export
+expression_table_TableName_ui <- function(id,
+                                        label='',
+                                        height,
+                                        width) {
+
+  ns <- shiny::NS(id)
+
+  tableWidget(ns("table"))
+
+}
+
+#' Server side table code: expression board
 #'
-#'   tableWidget(ns("table"))
+#' @param id
+#' @param watermark
 #'
-#' }
-#'
-#' #' Server side table code: expression board
-#' #'
-#' #' @param id
-#' #' @param watermark
-#' #'
-#' #' @export
-#' compare_table_corr_score_server <- function(id,
-#'                                             watermark=FALSE){
-#'   moduleServer( id, function(input, output, session) {
-#'
-#'     score_table.RENDER <- shiny::reactive({
-#'
-#'       #code here
-#'
-#'     })
-#'
-#'     score_table_info = ""
-#'
-#'     score_table <- shiny::callModule(
-#'       tableModule, id = "table",
-#'       func = "", ## ns=ns,
-#'       info.text = table_info,
-#'       title = tags$div(
-#'         HTML('')),
-#'       height = c(,),
-#'       width = c(,)
-#'     )
-#'     return(score_table)
-#'   }
-#'   )
-#' }
+#' @export
+expression_table_TableName_server <- function(id,
+                                              watermark=FALSE){
+  moduleServer( id, function(input, output, session) {
+
+    table.RENDER <- shiny::reactive({
+
+      #code here
+
+    })
+
+    table_info = ""
+
+    score_table <- shiny::callModule(
+      tableModule, id = "table",
+      func = "table.RENDER", ## ns=ns,
+      info.text = table_info,
+      title = tags$div(
+        HTML('')),
+      height = c(,),
+      width = c(,)
+    )
+    return(score_table)
+  }
+  )
+}
