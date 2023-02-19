f7bf1f5c4301d82856cb110a83ac238a7b78d218
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-02-20 00:57:51 +0100

    feat: add fileInput2
    copy of shiny fileInput but changing the class of the button it creates. Maybe on future releases of shiny they will allow us to place our own class on that button, not at the moment.
    
    With that the button matches the rest

--- components/base/R/ui-code.R
+++ components/base/R/ui-code.R
@@ -128,3 +128,42 @@ DropdowMenu <- function (..., size = "default", status = "default", icon = NULL,
   tags$script(HTML(jsCode(id)))
   )
 }
+
+
+# Copied code from shiny::fileInput but changed the class of the button
+# it creates to btn-outline-primary. 
+# Maybe on future versions of that function they will add
+# a `class` argument so we can submit our own class to that button.
+fileInput2 <- function(inputId, label, multiple = FALSE, accept = NULL, width = NULL, 
+    buttonLabel = "Browse...", placeholder = "No file selected", 
+    capture = NULL) 
+{
+    restoredValue <- restoreInput(id = inputId, default = NULL)
+    if (!is.null(restoredValue) && !is.data.frame(restoredValue)) {
+        warning("Restored value for ", inputId, " has incorrect format.")
+        restoredValue <- NULL
+    }
+    if (!is.null(restoredValue)) {
+        restoredValue <- toJSON(restoredValue, strict_atomic = FALSE)
+    }
+    inputTag <- tags$input(id = inputId, name = inputId, type = "file", 
+        style = "position: absolute !important; top: -99999px !important; left: -99999px !important;", 
+        `data-restore` = restoredValue)
+    if (multiple) 
+        inputTag$attribs$multiple <- "multiple"
+    if (length(accept) > 0) 
+        inputTag$attribs$accept <- paste(accept, collapse = ",")
+    if (!is.null(capture)) {
+        inputTag$attribs$capture <- capture
+    }
+    div(class = "form-group shiny-input-container", 
+      style = htmltools::css(width = htmltools::validateCssUnit(width)), 
+        shiny:::shinyInputLabel(inputId, label), div(class = "input-group", 
+            tags$label(class = "input-group-btn input-group-prepend", 
+                span(class = "btn btn-default btn-outline-primary", buttonLabel, 
+                  inputTag)), tags$input(type = "text", class = "form-control", 
+                placeholder = placeholder, readonly = "readonly")), 
+        tags$div(id = paste(inputId, "_progress", sep = ""), 
+            class = "progress active shiny-file-input-progress", 
+            tags$div(class = "progress-bar")))
+}
