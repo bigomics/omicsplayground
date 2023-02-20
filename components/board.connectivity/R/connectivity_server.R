e8e02ab1daac3536e8b5c61c08ea8bcda7022b95
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-02-20 16:50:05 +0100

    `TableModule` implemented on `board.connectivity`

--- components/board.connectivity/R/connectivity_server.R
+++ components/board.connectivity/R/connectivity_server.R
@@ -272,6 +272,7 @@ ConnectivityBoard <- function(id, inputData) {
     }
 
     getConnectivityScores <- shiny::reactive({
+      # browser()
       ngs <- inputData()
       shiny::req(ngs, input$cmap_contrast)
       shiny::validate(shiny::need("connectivity" %in% names(ngs), "no 'connectivity' in object."))
@@ -384,87 +385,6 @@ ConnectivityBoard <- function(id, inputData) {
       PERTINFO <- read.csv(pert_info.file, sep = "\t", row.names = 1)
     }
 
-    connectivityScoreTable.RENDER <- shiny::reactive({
-      df <- getConnectivityScores()
-      if (is.null(df)) {
-        return(NULL)
-      }
-
-      kk <- c("pathway", "score", "rho", "NES", "padj", "leadingEdge")
-      kk <- intersect(kk, colnames(df))
-      df <- df[, kk]
-      df <- df[abs(df$score) > 0, , drop = FALSE]
-
-      ## --------- temporarily add LINCS descriptive name !!!!!!!!!!!!!! -----------------
-      if (DEV && input$cmap_sigdb == "sigdb-lincs.h5" && !is.null(PERTINFO)) {
-        dd <- sub("\\|.*", "", df$pathway)
-        pert_iname <- PERTINFO[match(dd, rownames(PERTINFO)), "pert_iname"]
-        df$pathway <- paste0(df$pathway, " (", pert_iname, ")")
-      }
-      ## ---------- temporarily add LINCS descriptive name !!!!!!!!!!!!!! -----------------
-
-      ## colnames(df) <- sub("padj","NES.q",colnames(df))
-      df$leadingEdge <- shortstring(sapply(df$leadingEdge, paste, collapse = ","), 40)
-      df$pathway <- shortstring(df$pathway, 100)
-      df$leadingEdge <- NULL
-
-      colnames(df) <- sub("pathway", "dataset/contrast", colnames(df))
-      score.col <- which(colnames(df) == "score")
-      numcols <- c("score", "pval", "padj", "NES.q", "ES", "NES", "rho", "R2")
-      numcols <- intersect(numcols, colnames(df))
-
-      DT::datatable(df,
-        rownames = FALSE,
-        class = "compact cell-border stripe hover",
-        extensions = c("Scroller"),
-        selection = list(mode = "single", target = "row", selected = 1),
-        fillContainer = TRUE,
-        options = list(
-          dom = "lfrtip",
-          pageLength = 99999,
-          scrollX = TRUE,
-          scrollY = tabH,
-          scroller = TRUE, deferRender = TRUE
-        ) ## end of options.list
-      ) %>%
-        DT::formatSignif(numcols, 3) %>%
-        DT::formatStyle(0, target = "row", fontSize = "11px", lineHeight = "70%") %>%
-        DT::formatStyle("score",
-          background = color_from_middle(
-            df[, "score"], "lightblue", "#f5aeae"
-          ),
-          backgroundSize = "98% 88%",
-          backgroundRepeat = "no-repeat",
-          backgroundPosition = "center"
-        )
-    })
-
-    connectivityScoreTable.RENDER2 <- shiny::reactive({
-      connectivityScoreTable.RENDER() %>%
-        DT::formatStyle(0, target = "row", fontSize = "13px", lineHeight = "90%")
-    })
-
-
-    connectivityScoreTable_info <- "<b>Similarity scores.</b> Normalized enrichment scores (NES) and Pearson correlation (rho) of reference profiles with respect to the currently selected contrast. The top 100 up/down genes are considered for the calculation of rho or NES. The score is calculated as rho^2*NES. "
-
-    connectivityScoreTable_opts <- shiny::tagList(
-      shiny::selectInput(ns("connectivityScoreTable_qsig"), "threshold (padj)",
-        c(0.01, 0.05, 0.2, 1),
-        selected = 1
-      )
-    )
-
-    connectivityScoreTable <- shiny::callModule(
-      tableModule,
-      id = "connectivityScoreTable",
-      func = connectivityScoreTable.RENDER,
-      info.text = connectivityScoreTable_info,
-      options = connectivityScoreTable_opts,
-      info.width = "300px",
-      title = "Similarity scores",
-      height = c(260, 720), width = c("auto", 1280)
-    )
-
     getTopProfiles <- shiny::reactive({
       ## Get profiles of top-enriched contrasts (not all genes...)
       ##
@@ -507,6 +427,12 @@ ConnectivityBoard <- function(id, inputData) {
       watermark = WATERMARK
     )
 
+    connectivityScoreTable <- connectivity_table_similarity_scores_server(
+      "connectivityScoreTable",
+      getConnectivityScores = getConnectivityScores,
+      cmap_sigdb = shiny::reactive(input$cmap_sigdb)
+    )
+
 
     ## ================================================================================
     ## Cumulative FC barplot
@@ -544,6 +470,11 @@ ConnectivityBoard <- function(id, inputData) {
       reactive(input$cmap_sigdb),
       getConnectivityScores,
       getEnrichmentMatrix
+    )
+
+    connectivityScoreTable2 <- connectivity_table_similarity_scores2_server(
+      "connectivityScoreTable2",
+      getConnectivityScores = getConnectivityScores
     )
 
     ## -------------------------------------------------------------------------------
@@ -592,70 +523,6 @@ ConnectivityBoard <- function(id, inputData) {
       getConnectivityScores,
       getConnectivityMatrix,
       watermark = WATERMARK
-    )
-
-    connectivityScoreTable2.RENDER <- shiny::reactive({
-      df <- getConnectivityScores()
-      if (is.null(df)) {
-        return(NULL)
-      }
-
-      kk <- c("pathway", "score", "rho", "NES", "padj", "size", "leadingEdge")
-      kk <- c("score", "pathway", "rho", "NES", "padj")
-      kk <- intersect(kk, colnames(df))
-      df <- df[, kk]
-      df <- df[abs(df$score) > 0, , drop = FALSE]
-
-      df$pathway <- shortstring(df$pathway, 110)
-      df$leadingEdge <- NULL
-
-      colnames(df) <- sub("pathway", "dataset/contrast", colnames(df))
-      score.col <- which(colnames(df) == "score")
-      numcols <- c("score", "pval", "padj", "NES.q", "ES", "NES", "rho", "R2")
-      numcols <- intersect(numcols, colnames(df))
-
-      DT::datatable(df,
-        rownames = FALSE,
-        class = "compact cell-border stripe hover",
-        extensions = c("Scroller"),
-        selection = list(mode = "single", target = "row", selected = 1),
-        fillContainer = TRUE,
-        options = list(
-          dom = "lfrtip",
-          pageLength = 99999,
-          scrollX = TRUE,
-          scrollY = "100vh",
-          scroller = TRUE, deferRender = TRUE
-        ) ## end of options.list
-      ) %>%
-        DT::formatSignif(numcols, 3) %>%
-        DT::formatStyle(0, target = "row", fontSize = "11px", lineHeight = "70%") %>%
-        DT::formatStyle("score",
-          background = color_from_middle(
-            df[, "score"], "lightblue", "#f5aeae"
-          ),
-          backgroundSize = "98% 88%",
-          backgroundRepeat = "no-repeat",
-          backgroundPosition = "center"
-        )
-    })
-
-    connectivityScoreTable2_opts <- shiny::tagList(
-      shiny::selectInput(ns("connectivityScoreTable2_qsig"), "threshold (padj)",
-        c(0.01, 0.05, 0.2, 1),
-        selected = 1
-      )
-    )
-
-    connectivityScoreTable2 <- shiny::callModule(
-      tableModule,
-      id = "connectivityScoreTable2", label = "b",
-      func = connectivityScoreTable2.RENDER,
-      info.text = connectivityScoreTable_info,
-      options = connectivityScoreTable2_opts,
-      info.width = "150px",
-      title = "Similarity scores",
-      height = c(660, 700), width = c("auto", 1280)
     )
 
     ## =============================================================================
