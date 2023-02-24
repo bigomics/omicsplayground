521325f468394efd6249546eeb23dc31b0a1d7f8
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-02-24 15:35:51 +0100

    fix: logout goes to load data
    after logging out, the load data page is selected instead of being on the one were you clicked the logout button

--- components/app/R/www/temp.js
+++ components/app/R/www/temp.js
@@ -198,7 +198,7 @@ Shiny.addCustomMessageHandler('get-subs', (msg) => {
 
 const logout = () => {
 	unloadSidebar();
-	sidebarClose();
+	$(".tab-sidebar:eq(1)").trigger('click');
 	Shiny.setInputValue('auth-userLogout', 1, {priority: 'event'});
 	Shiny.setInputValue('userLogout', 1, {priority: 'event'});
 };
