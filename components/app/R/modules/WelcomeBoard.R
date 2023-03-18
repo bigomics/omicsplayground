9961936583da27d2ffa0d9936e5a4ede46edb0ac
Mauro Masiero <mauromiguelm@hotmail.com>
2023-03-18 18:54:10 +0100

    improving load example data description for user

--- components/app/R/modules/WelcomeBoard.R
+++ components/app/R/modules/WelcomeBoard.R
@@ -80,10 +80,10 @@ WelcomeBoardUI <- function(id) {
       id = "welcome-buttons",
       div(
         class = "col-md-5",
-        h3("I am new..."),
+        h3("I want to try an example dataset!"),
         shiny::actionButton(
           ns("init_example_data"),
-          label = "Try example dataset",
+          label = "Load example dataset",
           class = "btn btn-outline-info welcome-btn"
         )
       ),
