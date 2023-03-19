9802741820b917ed87472ba15d18ae423272eb68
Mauro Masiero <mauromiguelm@hotmail.com>
2023-03-19 18:12:47 +0100

    feat: center align modal dialog pop up; remove css that gets overwritten by bs5

--- components/base/R/pgx-ui.R
+++ components/base/R/pgx-ui.R
@@ -12,9 +12,6 @@ gadgetize <- function(moduleUI, moduleSERVER, title="shiny gadget", ...)
     
     id = sub(".*file","gadget",tempfile())  ## random ID
     ui = miniUI::miniPage(
-        shiny::tags$head(shiny::tags$style(".modal-dialog{width:900px}")),
-        shiny::tags$head(shiny::tags$style(".modal-dialog.modal-lg{width:1400px}")),
-        shiny::tags$head(shiny::tags$style(".modal-dialog.modal-sm{width:400px}")),        
         ##shinyalert::useShinyalert(),
         miniUI::gadgetTitleBar(title),
         miniUI::miniContentPanel(moduleUI(id))
@@ -43,11 +40,8 @@ gadgetize2 <- function(moduleUI, moduleSERVER, title="shiny gadget",
     
     id = sub(".*file","gadget",tempfile())  ## random ID
     ui = shiny::fluidPage(
-        shiny::tags$head(shiny::tags$style(".modal-dialog{width:600px}")),
-        shiny::tags$head(shiny::tags$style(".modal-dialog.modal-lg{width:600px}")),
-        shiny::tags$head(shiny::tags$style(".modal-dialog.modal-sm{width:400px}"))
         ##shinyalert::useShinyalert()
-    )    
+    )
     server = function(input, output, session)
     {
         return_obj <- moduleSERVER(id, ...)
