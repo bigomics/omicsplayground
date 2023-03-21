62630d60d79177ff91d1f088d73c960e6dfad300
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-03-21 12:28:45 +0100

    fix: added missing code (?)

--- components/base/R/ui-code.R
+++ components/base/R/ui-code.R
@@ -86,16 +86,58 @@ inlineSelectInput <- function (inputId, label, value = "")
         shiny::tags$input(id=inputId, type="select", value=value))
 }
 
+# This JS code is responsible for activating and desactivating the class `active`,
+# defined on `scss/components/_dropdownmenu.scss` when clicking a dropdown button.
+# It also has logic implemented so that when clicking a different dropdown item,
+# the active one gets desactivated.
+jsCode <- function(id){
+  paste0(
+    "$('#", id, "').on('click', function(){
+    if($('#", id, "').hasClass('active')){
+         $('#", id,"').toggleClass('active');
+    return 0;
+      }
+      $('.dropdown-button.active').toggleClass('active');
+      if(!$('#", id, "').hasClass('active')){
+         $('#", id,"').toggleClass('active');
+      }
+    })"
+  )
+}
 
-
+DropdowMenu <- function (..., size = "default", status = "default", icon = NULL, margin = "10px")
+{
+  id <- bigdash:::make_id()
+  tags$div(tags$a(class = paste0(
+    "btn btn-", status, " action-button dropdown-button btn-circle", if(size == "default"){" "}else{paste0("-", size, " ")},
+    if (size != "default") paste0("btn-", size)
+  ),
+  id = id, type = "button", `data-bs-toggle` = "dropdown",
+  `aria-expanded` = "false", list(icon)),
+  tags$div(class = "dropdown-menu",
+           `aria-labelledby` = id,
+           tryCatch({
+             lapply(list(...), function(x){
+               htmltools::tags$li(
+                 x,
+                 style = paste0("margin-left: ",
+                                htmltools::validateCssUnit(margin), "; margin-right: ",
+                                htmltools::validateCssUnit(margin), ";")
+               )
+             })
+           }, error = function(w){})
+  ),
+  tags$script(HTML(jsCode(id)))
+  )
+}
 
 # Copied code from shiny::fileInput but changed the class of the button
-# it creates to btn-outline-primary. 
+# it creates to btn-outline-primary.
 # Maybe on future versions of that function they will add
 # a `class` argument so we can submit our own class to that button.
-fileInput2 <- function(inputId, label, multiple = FALSE, accept = NULL, width = NULL, 
-    buttonLabel = "Browse...", placeholder = "No file selected", 
-    capture = NULL) 
+fileInput2 <- function(inputId, label, multiple = FALSE, accept = NULL, width = NULL,
+    buttonLabel = "Browse...", placeholder = "No file selected",
+    capture = NULL)
 {
     restoredValue <- restoreInput(id = inputId, default = NULL)
     if (!is.null(restoredValue) && !is.data.frame(restoredValue)) {
@@ -105,24 +147,24 @@ fileInput2 <- function(inputId, label, multiple = FALSE, accept = NULL, width = 
     if (!is.null(restoredValue)) {
         restoredValue <- toJSON(restoredValue, strict_atomic = FALSE)
     }
-    inputTag <- tags$input(id = inputId, name = inputId, type = "file", 
-        style = "position: absolute !important; top: -99999px !important; left: -99999px !important;", 
+    inputTag <- tags$input(id = inputId, name = inputId, type = "file",
+        style = "position: absolute !important; top: -99999px !important; left: -99999px !important;",
         `data-restore` = restoredValue)
-    if (multiple) 
+    if (multiple)
         inputTag$attribs$multiple <- "multiple"
-    if (length(accept) > 0) 
+    if (length(accept) > 0)
         inputTag$attribs$accept <- paste(accept, collapse = ",")
     if (!is.null(capture)) {
         inputTag$attribs$capture <- capture
     }
-    div(class = "form-group shiny-input-container", 
-      style = htmltools::css(width = htmltools::validateCssUnit(width)), 
-        shiny:::shinyInputLabel(inputId, label), div(class = "input-group", 
-            tags$label(class = "input-group-btn input-group-prepend", 
-                span(class = "btn btn-default btn-outline-primary", buttonLabel, 
-                  inputTag)), tags$input(type = "text", class = "form-control", 
-                placeholder = placeholder, readonly = "readonly")), 
-        tags$div(id = paste(inputId, "_progress", sep = ""), 
-            class = "progress active shiny-file-input-progress", 
+    div(class = "form-group shiny-input-container",
+      style = htmltools::css(width = htmltools::validateCssUnit(width)),
+        shiny:::shinyInputLabel(inputId, label), div(class = "input-group",
+            tags$label(class = "input-group-btn input-group-prepend",
+                span(class = "btn btn-default btn-outline-primary", buttonLabel,
+                  inputTag)), tags$input(type = "text", class = "form-control",
+                placeholder = placeholder, readonly = "readonly")),
+        tags$div(id = paste(inputId, "_progress", sep = ""),
+            class = "progress active shiny-file-input-progress",
             tags$div(class = "progress-bar")))
 }
