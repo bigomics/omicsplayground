efb26414ef1c14e860de66d7624c6c8ea3a20e96
ivokwee <ivo.kwee@gmail.com>
2023-03-22 17:53:01 +0100

    increase nr of tissue

--- components/board.dataview/R/dataview_plot_tissue.R
+++ components/board.dataview/R/dataview_plot_tissue.R
@@ -64,7 +64,7 @@ dataview_plot_tissue_server <- function(id, pgx, r.gene, r.data_type, watermark 
         color = tissue.klr
       )
       df <- df[with(df, order(-x)), ]
-      df <- df[1:15, ] # select top 15 tissues
+      df <- df[1:20, ] # select top 15 tissues
 
       return(
         list(
