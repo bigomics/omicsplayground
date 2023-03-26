2b7dcc8009a5621e340972f29d0bdc20d2d5590e
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-03-27 00:49:43 +0200

    fix: update globally the copyright year

--- components/board.loading/R/loading_server.R
+++ components/board.loading/R/loading_server.R
@@ -1,6 +1,6 @@
 ##
 ## This file is part of the Omics Playground project.
-## Copyright (c) 2018-2022 BigOmics Analytics Sagl. All rights reserved.
+## Copyright (c) 2018-2023 BigOmics Analytics SA. All rights reserved.
 ##
 
 LoadingBoard <- function(id,
--- components/board.signature/R/signature_table_genes_in_signature.R
+++ components/board.signature/R/signature_table_genes_in_signature.R
@@ -1,6 +1,6 @@
 ##
 ## This file is part of the Omics Playground project.
-## Copyright (c) 2018-2022 BigOmics Analytics Sagl. All rights reserved.
+## Copyright (c) 2018-2023 BigOmics Analytics SA. All rights reserved.
 ##
 
 signature_table_genes_in_signature_ui <- function(id, width, height) {
