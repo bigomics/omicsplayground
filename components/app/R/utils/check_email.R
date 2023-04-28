3ffce7ea568820c03cbbe6c1b67ccc90e1429ef5
Ivo Kwee <ivo.kwee@gmail.com>
2023-04-28 19:21:09 +0200

    check new email if personal

--- components/app/R/utils/check_email.R
+++ components/app/R/utils/check_email.R
@@ -10,7 +10,7 @@ check_personal_email <- function(auth, pgx_dir, title=NULL, text=NULL) {
   user_exists <- (email %in% existing_user_dirs)
 
   if(is.null(text)) {
-    text <- "You are using a personal email adress. Please provide your business, academic or institutional email. Your login name will be changed and your data will be copied. After that please login again with your new email. Are you sure?"
+    text <- "You are using a personal email adress. Please provide a business, academic or institutional email."
   }
   if(is.null(title)) {
     title <- "Please change your email"
@@ -23,11 +23,20 @@ check_personal_email <- function(auth, pgx_dir, title=NULL, text=NULL) {
       text = text,
       type = "input",
       callbackR = function(new_email) {
+        ## check if new email is valid
+        newemail_is_personal <- grepl("gmail|ymail|outlook|yahoo|mail.com$|icloud",new_email)
+        if(newemail_is_personal) {
+          dbg("[check_personal_email] ERROR!!",new_email,"is personal")
+          title = "Please change email"
+          text <- "Your new email is invalid. Please provide a business, academic or institutional email."
+          check_personal_email(auth, pgx_dir, title=title, text=text)
+        }
+        
+        ## copy old data to new data
         old_dir_exists <- (email %in% existing_user_dirs)
         new_dir_exists <- (new_email %in% existing_user_dirs)
-        ## copy old data to new data
         if(old_dir_exists && !new_dir_exists) {
-          dbg("@@@@@ dry run @@@@@ copying data from",email,"to",new_email)
+          dbg("[check_personal_email] moving data from",email,"to",new_email)
           old_dir <- file.path( pgx_dir, email)
           new_dir <- file.path( pgx_dir, new_email)
           base::file.rename(old_dir, new_dir)
@@ -35,10 +44,10 @@ check_personal_email <- function(auth, pgx_dir, title=NULL, text=NULL) {
           shinyalert::shinyalert(title="", text="Your login name has been changed and your data have been moved. Please login again with your new email.")          
           shinyjs::runjs("logoutInApp()")
         } else if(!old_dir_exists && !new_dir_exists) {
-          shinyalert::shinyalert(title="", text="Your login name has been changed. Please login again with your new email.")          
+          shinyalert::shinyalert(title="", text="Your login name has been changed. Please login again with your new email.")
           shinyjs::runjs("logoutInApp()")          
         } else if(new_dir_exists) {
-          dbg("@@@@@ dry run @@@@@ ERROR!!",new_email,"exists")
+          dbg("[check_personal_email] ERROR!!",new_email,"exists")
           title = "Email already exists"
           text <- "This email already exists. Please provide a different business, academic or institutional email."
           check_personal_email(auth, pgx_dir, title=title, text=text)
