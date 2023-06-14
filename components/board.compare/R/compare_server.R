cecf308f1a6122222a7cb272f4d80d6df6d5bbde
Mauro Masiero <mauromiguelm@hotmail.com>
2023-06-14 16:46:08 +0200

    set row names to upper case

--- components/board.compare/R/compare_server.R
+++ components/board.compare/R/compare_server.R
@@ -102,10 +102,12 @@ CompareBoard <- function(id, pgx) {
     })
 
     getOmicsScoreTable <- shiny::reactive({
+      
+      shiny::req(pgx)
+      shiny::req(dataset2())
+      
       pgx1 <- pgx
       pgx2 <- dataset2()
-      shiny::req(pgx1)
-      shiny::req(pgx2)
 
       ct1 <- head(names(pgx1$gx.meta$meta), 2)
       ct2 <- head(names(pgx2$gx.meta$meta), 2)
@@ -123,7 +125,7 @@ CompareBoard <- function(id, pgx) {
       F1 <- playbase::pgx.getMetaMatrix(pgx1)$fc[, ct1, drop = FALSE]
       F2 <- playbase::pgx.getMetaMatrix(pgx2)$fc[, ct2, drop = FALSE]
 
-      gg <- intersect(rownames(pgx1$X), rownames(pgx2$X))
+      gg <- intersect(toupper(rownames(pgx1$X)), toupper(rownames(pgx2$X)))
       F1 <- F1[match(gg, rownames(F1)), , drop = FALSE]
       F2 <- F2[match(gg, rownames(F2)), , drop = FALSE]
       rownames(F1) <- gg
