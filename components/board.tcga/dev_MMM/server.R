792fb4a7f3bdedd81296358cb7513018dca6ec10
Mauro Masiero <mauromiguelm@hotmail.com>
2023-06-30 11:49:17 +0200

    add two possible attempt without error

--- components/board.tcga/dev_MMM/server.R
+++ components/board.tcga/dev_MMM/server.R
@@ -5,20 +5,34 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
+  
+  # attemp 1
 
-  # attempt 1, using reactive container
-  
-  loaded_pgx <- reactive({
-    req(input$pgx_path)
-    load(input$pgx_path)
-    names(pgx)
-    return(pgx)
-    }
-  )
-  
-  # loading from absolute path works, we want to pass a reactval to modify this path
-  
-  # load("C:/code//omicsplayground/data/example-data.pgx")
+      # trigger_server <- reactive({
+      #   req(input$pgx_path)
+      #   load(input$pgx_path)
+      #   server <- TcgaBoard('tcga', pgx)
+      # })
+      # observeEvent(input$pgx_path, {
+      #     trigger_server()
+      # })
 
-  server <- TcgaBoard('tcga', loaded_pgx())
+
+  # attemp 2
+
+    loaded_pgx <- reactive({
+      req(input$pgx_path)
+      load(input$pgx_path)
+      names(pgx)
+      return(pgx)
+    })
+  
+    server <- TcgaBoard('tcga', loaded_pgx())
+
+  # working version
+
+      # load("C:/code//omicsplayground/data/example-data.pgx")
+      # server <- TcgaBoard('tcga', loaded_pgx())
+
+  
 }
