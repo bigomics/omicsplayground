eea121782c7c442b9ce4fe2a8a2df34361bf0205
Mauro Masiero <mauromiguelm@hotmail.com>
2023-06-30 13:24:44 +0200

    add comments

--- dev/boards_tests.R
+++ dev/boards_tests.R
@@ -6,19 +6,27 @@ driver <- shinytest::ShinyDriver$new(
   path = "components/board.tcga/dev_MMM/"
   )
 
+
+# when I run the same thing over runApp, and place C:\\code\\omicsplayground\\data\\example-data.pgx in text input, the pgx gets loaded ;)
+shiny::runApp("components/board.tcga/dev_MMM/")
+
+# basic info for the app
 driver$getUrl()
 driver$getDebugLog()
 driver$listWidgets()$input
 driver$listWidgets()$output
 
-# changing pgx file
-
+# get pgx file path
 pgx_file <- normalizePath("data/example-data.pgx")
 
+# I added a textInput for pgx_path
 driver$getValue("pgx_path")
 
+# update pgx_path to an actual path
 driver$setValue("pgx_path", pgx_file)
 
+# check that the path is updated
 driver$getValue("pgx_path")
 
+# exit the app
 driver$finalize()
