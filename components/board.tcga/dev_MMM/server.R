842ae19a15adc4222c101874f9d380710242b418
Mauro Masiero <mauromiguelm@hotmail.com>
2023-07-01 19:03:33 +0200

    feat: shiny parent env e passed to error_log

--- components/board.tcga/dev_MMM/server.R
+++ components/board.tcga/dev_MMM/server.R
@@ -5,6 +5,15 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
+
+  options(shiny.error = function() {
+      # The error message is on the parent environment, it is
+      # not passed to the function called on error
+      parent_env <- parent.frame()
+      error <- parent_env$e
+      error_log(error)
+    })
+
   
   # attempt 1
   error_log <- reactiveVal("No error")
@@ -16,23 +25,17 @@ app_server <- function(input, output, session) {
   })
   
   observeEvent(input$pgx_path, {
-    tryCatch(
-      {
-        trigger_server()
-      },
-      error = function(e) {
-        error_log(e)
-      }
-    )
+    trigger_server()
   })
   
   # Render error log as output text
   output$error_log <- renderText({
-  logs <- error_log()
-  if (is.list(logs)) {
-    logs <- paste(logs, collapse = "\n")
-  }
-  logs
-})
+    logs <- error_log()
+    if (is.list(logs)) {
+      logs <- paste(logs, collapse = "\n")
+    }
+    pgx_logs
+    }
+  )
   
 }
