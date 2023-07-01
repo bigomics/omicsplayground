21ba5775e9e05372d03c1bd57c50872ad4d589aa
Mauro Masiero <mauromiguelm@hotmail.com>
2023-07-01 18:28:53 +0200

    get error when board is unable to load

--- dev/boards_tests.R
+++ dev/boards_tests.R
@@ -2,11 +2,21 @@
 
 ### board specific files ###
 
-pgx_logs <- list()
+pgx_logs <- list()  
 
-driver <- shinytest::ShinyDriver$new(
-  path = "components/board.tcga/dev_MMM/"
-  )
+# get error from driver and save it as error_log
+error_log <- list()
+
+driver <- tryCatch(
+      {
+        shinytest::ShinyDriver$new(
+          path = "components/board.tcga/dev_MMM/"
+        )
+      },
+      error = function(e) {
+        return(e)
+      }
+    )
 
 # basic info for the app
 driver$getUrl()
@@ -17,6 +27,9 @@ driver$listWidgets()$output
 # get pgx file path
 pgx_file <- normalizePath("data/example-data.pgx")
 
+pgx_file
+
+shiny::runApp("components/board.tcga/dev_MMM/")
 # I added a textInput for pgx_path
 driver$getValue("pgx_path")
 
@@ -31,6 +44,6 @@ driver$getValue("pgx_path")
 # get stderror for the app
 driver$getDebugLog()
 driver$listWidgets()$output
-
+driver$getValue("error_log")
 # exit the app
 driver$finalize()
