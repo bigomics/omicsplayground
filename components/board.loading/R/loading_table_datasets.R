dd29dc71dfc08e5256236b7937bc8e8357169683
ncullen93 <nicholas.cullen@med.lu.se>
2023-07-05 15:35:30 +0200

    remove double-defined function

--- components/board.loading/R/loading_table_datasets.R
+++ components/board.loading/R/loading_table_datasets.R
@@ -285,20 +285,6 @@ loading_table_datasets_server <- function(id,
         return(df)
     })
 
-    andothers <- function(s, split = " ", n = 8) {
-        if (is.na(s)) {
-            return("")
-        }
-        s <- sub("^[ ]*", "", s)
-        s <- sub("[ ]+", " ", s)
-        s1 <- strsplit(s, split = split)[[1]]
-        if (length(s1) <= n) {
-            return(s)
-        }
-        n2 <- setdiff(length(s1), n)
-        paste(paste(head(s1, n), collapse = " "), "(+", n2, "others)")
-    }
-
     pgxTable_DT <- reactive({
 
       df <- table_data()
