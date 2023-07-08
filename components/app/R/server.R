7192274f035d8d08915c070e6b9f943c4daed1c7
Ivo Kwee <ivo.kwee@gmail.com>
2023-07-08 16:08:59 +0200

    implementation of loginCodeAuth: working proto, email not tested yet

--- components/app/R/server.R
+++ components/app/R/server.R
@@ -74,7 +74,8 @@ app_server <- function(input, output, session) {
   } else if (authentication == "login-code") {
     auth <- LoginCodeAuthenticationModule(
       id = "auth",
-      mail_creds = "gmail_creds"
+      mail_creds = "dry-run"
+      ##mail_creds = "gmail_creds"      
     )
   } else if (authentication == "shinyproxy") {
     username <- Sys.getenv("SHINYPROXY_USERNAME")
