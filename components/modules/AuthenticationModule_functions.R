7c3b9e3f8618b4811c1cdcc94d5e81e7e1eaedf2
ivokwee <ivokwee@users.noreply.github.com>
2023-07-09 17:12:44 +0000

    Style code (GHA)

--- components/modules/AuthenticationModule_functions.R
+++ components/modules/AuthenticationModule_functions.R
@@ -159,8 +159,12 @@ checkAuthorizedUser <- function(email, credentials_file = NULL) {
 }
 
 checkValidEmailFormat <- function(email) {
-  if(is.null(email)) return(FALSE)
-  if(email %in% c(""," ",NA)) return(FALSE)  
+  if (is.null(email)) {
+    return(FALSE)
+  }
+  if (email %in% c("", " ", NA)) {
+    return(FALSE)
+  }
   grepl(".*@.*[.].*", email)
 }
 
@@ -187,5 +191,3 @@ checkEmail <- function(email, domain = NULL, credentials_file = NULL, check.pers
   }
   list(valid = TRUE, "email ok")
 }
-
-
