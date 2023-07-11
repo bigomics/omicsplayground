41e0ffda6ddc1ecc14b0811170dc99b10e4c7b51
Mauro Masiero <mauromiguelm@hotmail.com>
2023-07-11 14:09:57 +0200

    new attempt

--- components/ui/ui-DropDownMenu.R
+++ components/ui/ui-DropDownMenu.R
@@ -12,39 +12,16 @@
 ## second version of Carson (see email 9jul23)
 dropdown.jsCode.SAVE <- function(id) {
   paste0("
-$('#", id, "').on('click', function(){
-  if($('#", id, "').hasClass('active')){
-    $('#", id, "').toggleClass('active');
-    return 0;
-  }
-  $('.dropdown-button.active').toggleClass('active');
-  if(!$('#", id, "').hasClass('active')){
-    $('#", id, "').toggleClass('active');
-  }
-})
-function restoreDropdownMenu() {
-  var menuBody = $('body > .dropdown-menu-body');
-  if (menuBody.length > 0) {
-    menuBody.removeClass('dropdown-menu-body');
-    var id = menuBody.attr('data-toggle-id');
-    $('#' + id).parent().append(menuBody.detach());
-  }
-}
-$('#", id, "').on('shown.bs.dropdown', function () {
-  restoreDropdownMenu();
-  if (!this.id) {
-    console.error('Expected dropdown toggle to have an id attribute');
-  }
-  setTimeout(function() {
-    var menu = $(this).parent().find('.dropdown-menu');
-    menu.addClass('dropdown-menu-body');
-    menu.attr('data-toggle-id', this.id);
-    $('body').append(menu.detach());
-  },1);
-});
-$('#", id, "').on('hidden.bs.dropdown', function () {
-  restoreDropdownMenu();
-});
+    $('#", id, "').on('shown.bs.dropdown', function () {
+      restoreDropdownMenu();
+      if (!this.id) {
+        console.error('Expected dropdown toggle to have an id attribute');
+      }
+      var menu = $(this).parent().find('.dropdown-menu');
+      menu.addClass('dropdown-menu-body');
+      menu.attr('data-toggle-id', this.id);
+      menu.detach();
+      setTimeout(function() { $('body').append(menu); }, 0);
 ") ## end of paste0
 }
 
