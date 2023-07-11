6c7443e667ca27b6bbfa7e8d93ef06a60f1e1917
Mauro Masiero <mauromiguelm@hotmail.com>
2023-07-11 17:12:38 +0200

    remove duplicate function

--- components/ui/ui-DropDownMenu.R
+++ components/ui/ui-DropDownMenu.R
@@ -9,44 +9,6 @@
 # It also has logic implemented so that when clicking a different dropdown item,
 # the active one gets desactivated.
 
-## second version of Carson (see email 9jul23)
-dropdown.jsCode.SAVE <- function(id) {
-  paste0(
-    "
-    $('#", id, "').on('click', function(){
-    if($('#", id, "').hasClass('active')){
-      $('#", id, "').toggleClass('active');
-      return 0;
-    }
-    $('.dropdown-button.active').toggleClass('active');
-    if(!$('#", id, "').hasClass('active')){
-      $('#", id, "').toggleClass('active');
-    }
-    })
-    function restoreDropdownMenu() {
-      var menuBody = $('body > .dropdown-menu-body');
-      if (menuBody.length > 0) {
-        menuBody.removeClass('dropdown-menu-body');
-        var id = menuBody.attr('data-toggle-id');
-        $('#' + id).parent().append(menuBody.detach());
-      }
-    }
-    
-    $('#", id, "').on('shown.bs.dropdown', function () {
-      restoreDropdownMenu();
-      if (!this.id) {
-        console.error('Expected dropdown toggle to have an id attribute');
-      }
-      var menu = $(this).parent().find('.dropdown-menu');
-      menu.addClass('dropdown-menu-body');
-      menu.attr('data-toggle-id', this.id);
-      menu.detach();
-      setTimeout(function() { $('body').append(menu); }, 0);
-      "
-    ) ## end of paste0
-}
-
-## first version of Carson
 dropdown.jsCode <- function(id) {
   paste0("
 $('#", id, "').on('click', function(){
