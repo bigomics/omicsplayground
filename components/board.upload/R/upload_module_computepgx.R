15bbb44b161ad1aa96b9e4505403b722cac4093f
Mauro Masiero <mauromiguelm@hotmail.com>
2023-07-13 10:07:58 +0200

    remove logic from upload_module_compute_pgx

--- components/board.upload/R/upload_module_computepgx.R
+++ components/board.upload/R/upload_module_computepgx.R
@@ -423,11 +423,7 @@ upload_module_computepgx_server <- function(
 
         # Create temporary folder
 
-        temp_dir(tempfile(pattern = "log_input/pgx_", tmpdir = dirname(OPG)))
-        dir.create(temp_dir(), recursive = TRUE)
-        dbg("[compute PGX process] : tempFile", temp_dir())
-
-        
+                
 
         this.date <- format(Sys.time(), "%Y-%m-%d %H:%M:%S")
         path_to_params <- file.path(temp_dir(), "params.RData")
