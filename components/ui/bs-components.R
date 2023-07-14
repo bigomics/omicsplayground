9e90a70990a159847d6384cb61b540a47ee57473
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-07-14 17:47:33 +0200

    fix: interference with bslib removed

--- components/ui/bs-components.R
+++ components/ui/bs-components.R
@@ -19,14 +19,21 @@ layout_column_wrap_visible <- function(...) {
 }
 
 bs_alert <- function(..., conditional = TRUE, style = "primary") {
+  id <- bigdash:::make_id()
   alert_tag <- shiny::tags$div(
+    id = id,
     class = paste0("alert alert-", style, " alert-dismissible fade show"),
     role = "alert",
     ...,
     shiny::tags$button(
+      # Use display: none; instead of official boostrap close button.
+      # If not, the element interfers with the bslib::layout_column_wrap
+      # and we get extra gap on top. The second part of the `onclick` is
+      # to also close the box (hide it) when it is not placed inside a 
+      # bslib::layout_column_wrap
+      onclick = paste0('$("#', id, ' button").closest(".bslib-gap-spacing.html-fill-container").css("display", "none");$("#', id, ' button").parent().css("display", "none");'),
       type = "button",
       class = "btn-close",
-      `data-bs-dismiss` = "alert",
       `aria-label` = "Close",
       shiny::tags$span(
         `aria-hidden` = "true"
