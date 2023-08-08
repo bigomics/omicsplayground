f4ef32671ac6278038755a928a6fdc31f01e2bc7
Ivo Kwee <ivo.kwee@gmail.com>
2023-08-08 14:55:05 +0200

    comment out console.log mesg

--- components/app/R/www/temp.js
+++ components/app/R/www/temp.js
@@ -410,12 +410,12 @@ $(document).ready(function() {
 	var tabId = $(e.target).data("target");
 	let hasHsq = (typeof window._hsq !== 'undefined' && window._hsq !== null)
  	/* https://developers.hubspot.com/docs/api/events/tracking-code#tracking-in-single-page-applications */
-	console.log('[tab-trigger:click] user = ' + user);	
+//	console.log('[tab-trigger:click] user = ' + user);	
 	if(hasHsq && user !== '' && user !== 'undefined') {
 	    var _hsq = window._hsq = window._hsq || [];
 	    var orginalTitle = document.title;
 	    document.title = orginalTitle + ' > ' + tabId ;
-	    console.log('[_hsq.push] ' + document.title);	    
+//	    console.log('[_hsq.push] ' + document.title);	    
 	    _hsq.push(["identify", { email: user }]);  // set to current user
 	    _hsq.push(['setPath', '#' + tabId]);
 	    _hsq.push(['trackPageView']);
