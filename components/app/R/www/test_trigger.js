92a0b9df66c305bdc57d5a7e76d547c692ca2391
Mauro Masiero <mauromiguelm@hotmail.com>
2023-08-23 13:13:54 +0200

    attempt: server status

--- components/app/R/www/test_trigger.js
+++ components/app/R/www/test_trigger.js
@@ -5,18 +5,14 @@
 ##
 */
 
-// code included inside $(document).ready() will only run once the page is ready for JavaScript code to execute
-$(document).ready(function() {
-  
-  // initialize a counter
-  var continue_test = false;
-  
-  $("#logo-bigomics").on("click", function(){
-  
-    continue_test = true;
-    
-    // send message to Shiny
-    Shiny.onInputChange("continue_test", continue_test);
-  });
 
+$(document).on('shiny:idle', function() {
+    console.log("shiny:idle")
+    Shiny.onInputChange("continue_test", true);
 });
+
+$(document).on('shiny:busy', function() {    
+    Shiny.onInputChange("continue_test", false);
+    console.log("shiny:busy")
+});
+
