c9a1789c97bea93b6bc4738a1f53193d5df7ca5c
ncullen93 <nicholas.cullen@med.lu.se>
2023-08-29 10:46:01 -0400

    errored files are shown in preview

---
+++ components/board.upload/R/upload_module_preview.R
@@ -0,0 +1,444 @@
+# NOTES
+#SAMPLES
+#- all tabs should be the same size
+#- convert column types from numeric to categorical
+#- remove column
+#- convert columns to categorical via binning and cuts
+#
+#CONTRASTS
+#- make contrasts table editable
+#
+#- Save updated files
+#
+#- put the errors in the same tab
+#- make the modal the whole screen and put the errors on the side
+#
+
+# no ui right now because the preview is in a modal dialog
+upload_module_preview_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+
+  )
+}
+
+upload_module_preview_server <- function(id, uploaded) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+
+      # every time something is uploaded, it can be previewed
+      observeEvent(uploaded$last_uploaded, {
+
+        has_counts <- !is.null(uploaded$checklist$counts.csv$file)
+        has_samples <- !is.null(uploaded$checklist$samples.csv$file)
+        has_contrasts <- !is.null(uploaded$checklist$contrasts.csv$file)
+
+        tabs <- list(id = session$ns('preview_panel'))
+        if (has_counts) {
+          tabs <- c(
+            tabs,
+            list(tabPanel(
+              'Counts',
+              fluidRow(
+                column(
+                  width = 8,
+                  upload_table_preview_counts_ui(session$ns('counts_preview'),
+                                                 height = c("100%", TABLE_HEIGHT_MODAL),
+                                                 width = c("auto", "100%"),
+                                                 title = "Uploaded Counts",
+                                                 info.text = "This is the uploaded counts data.",
+                                                 caption = "This is the uploaded counts data.")
+                ),
+                column(
+                  width = 4,
+                  'Summary:', br(),
+                  check_to_html(uploaded$checklist$counts.csv$check,
+                                pass_msg = 'All counts checks passed'),
+                  check_to_html(uploaded$checklist$samples_counts,
+                                pass_msg = 'All samples-counts checks passed')
+                )
+              )
+            ))
+          )
+        }
+        if (has_samples) {
+          tabs <- c(
+            tabs,
+            list(tabPanel(
+              'Samples',
+              fluidRow(
+                column(
+                  width = 8,
+                  upload_table_preview_samples_ui(session$ns('samples_preview'),
+                                                 height = c("100%", TABLE_HEIGHT_MODAL),
+                                                 width = c("auto", "100%"),
+                                                 title = "Uploaded Samples",
+                                                 info.text = "This is the uploaded samples data.",
+                                                 caption = "This is the uploaded samples data.")
+                ),
+                column(
+                  width = 4,
+                  'Summary:', br(),
+                  check_to_html(uploaded$checklist$samples.csv$check,
+                                pass_msg = 'All samples checks passed'),
+                  check_to_html(uploaded$checklist$samples_counts,
+                                pass_msg = 'All samples-counts checks passed'),
+                  check_to_html(uploaded$checklist$samples_contrasts,
+                                pass_msg = 'All samples-contrasts checks passed')
+                )
+              )
+            ))
+          )
+        }
+        if (has_contrasts) {
+          tabs <- c(
+            tabs,
+            list(tabPanel(
+              'Contrasts',
+              fluidRow(
+                column(
+                  width = 8,
+                  upload_table_preview_contrasts_ui(session$ns('contrasts_preview'),
+                                                 height = c("100%", TABLE_HEIGHT_MODAL),
+                                                 width = c("auto", "100%"),
+                                                 title = "Uploaded Contrasts",
+                                                 info.text = "This is the uploaded contrasts data.",
+                                                 caption = "This is the uploaded contrasts data.")
+                ),
+                column(
+                  width = 4,
+                  'Summary:', br(),
+                  check_to_html(uploaded$checklist$contrasts.csv$check,
+                                pass_msg = 'All contrasts checks passed'),
+                  check_to_html(uploaded$samples_contrasts_check,
+                                pass_msg = 'All samples-contrasts checks passed')
+                )
+              )
+            ))
+          )
+        }
+
+        shiny::showModal(
+          shiny::modalDialog(
+            title = 'Data Upload Preview',
+            label = 'Data Upload Preview',
+            do.call(tabsetPanel, tabs),
+            footer = div(
+              style = 'float: right',
+              shiny::actionButton(
+                session$ns('cancel_upload'),
+                label = 'Cancel',
+                style = 'display: inline-block; margin-left: 15px;',
+                class = 'btn-danger'
+              ),
+              shiny::actionButton(
+                session$ns('ok_upload'),
+                label = 'OK',
+                style = 'display: inline-block',
+                class = 'btn-info'
+              )
+            ),
+            easyClose = FALSE,
+            size = 'xl'
+          ) %>%
+            tagAppendAttributes(
+              style = 'min-height: 90%; min-width: 90%',
+              .cssSelector = '.modal-dialog'
+            )
+        )
+
+      }, ignoreNULL = TRUE)
+
+      upload_table_preview_counts_server('counts_preview',
+                                         uploaded,
+                                         scrollY="calc(35vh - 140px)")
+      upload_table_preview_samples_server('samples_preview',
+                                         uploaded,
+                                         scrollY="calc(35vh - 140px)")
+      upload_table_preview_contrasts_server('contrasts_preview',
+                                         uploaded,
+                                         scrollY="calc(35vh - 140px)")
+
+      observeEvent(input$cancel_upload, {
+        shiny::removeModal()
+        has_counts <- 'counts.csv' %in% uploaded$last_uploaded
+        has_samples <- 'samples.csv' %in% uploaded$last_uploaded
+        has_contrasts <- ('contrasts.csv' %in% uploaded$last_uploaded) &
+          (!is.null(uploaded$contrasts.csv))
+        if (has_counts) { uploaded$counts.csv <- NULL }
+        if (has_samples) { uploaded$samples.csv <- NULL }
+        if (has_contrasts) { uploaded$contrasts.csv <- NULL }
+      }, ignoreInit = TRUE)
+
+      observeEvent(input$ok_upload, {
+        shiny::removeModal()
+      }, ignoreInit = TRUE)
+
+    }
+  )
+}
+
+# convert list of checks to html tags for display in the data preview modal
+check_to_html <- function(check, pass_msg='') {
+  error_list <- playbase::PGX_CHECKS
+
+  if (length(check) > 0) {
+    tagList(
+      lapply(1:length(check), function(idx) {
+        error_id <- names(check)[idx]
+        error_log <- check[[idx]]
+        error_detail <- error_list[error_list$error == error_id, ]
+        error_length <- length(error_log)
+        ifelse(length(error_log) > 5, error_log <- error_log[1:5], error_log)
+        if (error_detail$warning_type == 'warning') {
+          title_color <- 'orange'
+        } else if (error_detail$warning_type == 'error') {
+          title_color <- 'red'
+        }
+        div(
+          hr(style='border-top: 1px solid black;'),
+          span(error_detail$title, style=paste('color:',title_color)),
+          br(),
+          paste(error_detail$message, "\n", paste(error_length, "case(s) identified, examples:"), paste(error_log, collapse = " "), sep = " "),
+          br()
+        )
+      }),
+      hr(style='border-top: 1px solid black;')
+    )
+  } else {
+    if (pass_msg != '') {
+      tagList(
+        span(pass_msg, style='color: green'), br()
+      )
+    }
+  }
+
+
+}
+
+
+upload_table_preview_counts_ui <- function(
+    id,
+    width,
+    height,
+    title,
+    info.text,
+    caption) {
+  ns <- shiny::NS(id)
+
+  TableModuleUI(
+    ns("datasets"),
+    width = width,
+    height = height,
+    title = title,
+    info.text = info.text,
+    caption = caption,
+    label = "",
+    show.maximize = FALSE
+  )
+}
+
+upload_table_preview_counts_server <- function(id,
+                                               uploaded,
+                                               scrollY) {
+  moduleServer(id, function(input, output, session) {
+    table_data <- shiny::reactive({
+      shiny::req(uploaded$checklist$counts.csv$file)
+      uploaded$checklist$counts.csv$file
+    })
+
+    table.RENDER <- function() {
+      dt <- table_data()
+      req(dt)
+      DT::datatable(dt,
+                    class = "compact hover",
+                    rownames = TRUE,
+                    extensions = c("Buttons", "Scroller"),
+                    plugins = "scrollResize",
+                    selection = list(mode = "single", target = "row", selected = 1),
+                    options = list(
+                      dom = "lfrtip",
+                      scroller = TRUE,
+                      scrollX = TRUE,
+                      scrollY = scrollY,
+                      scrollResize = TRUE,
+                      deferRender = TRUE
+                    )
+      ) %>%
+        DT::formatStyle(0, target = "row", fontSize = "11px", lineHeight = "70%")
+    }
+
+    TableModuleServer(
+      "datasets",
+      func = table.RENDER,
+      selector = "none"
+    )
+  }) ## end of moduleServer
+} ## end of server
+
+upload_table_preview_samples_ui <- function(
+    id,
+    width,
+    height,
+    title,
+    info.text,
+    caption) {
+  ns <- shiny::NS(id)
+
+  TableModuleUI(
+    ns("datasets"),
+    width = width,
+    height = height,
+    title = title,
+    info.text = info.text,
+    caption = caption,
+    label = "",
+    show.maximize = FALSE
+  )
+}
+
+upload_table_preview_samples_server <- function(id,
+                                               uploaded,
+                                               scrollY) {
+  moduleServer(id, function(input, output, session) {
+    table_data <- shiny::reactive({
+      shiny::req(uploaded$checklist$samples.csv$file)
+      uploaded$checklist$samples.csv$file
+    })
+
+    table.RENDER <- function() {
+      dt <- table_data()
+      req(dt)
+      DT::datatable(dt,
+                    class = "compact hover",
+                    rownames = TRUE,
+                    extensions = c("Buttons", "Scroller"),
+                    plugins = "scrollResize",
+                    selection = list(mode = "single", target = "row", selected = 1),
+                    options = list(
+                      dom = "lfrtip",
+                      scroller = TRUE,
+                      scrollX = TRUE,
+                      scrollY = scrollY,
+                      scrollResize = TRUE,
+                      deferRender = TRUE
+                    )
+      ) %>%
+        DT::formatStyle(0, target = "row", fontSize = "11px", lineHeight = "70%")
+    }
+
+    TableModuleServer(
+      "datasets",
+      func = table.RENDER,
+      selector = "none"
+    )
+  }) ## end of moduleServer
+} ## end of server
+
+upload_table_preview_contrasts_ui <- function(
+    id,
+    width,
+    height,
+    title,
+    info.text,
+    caption) {
+  ns <- shiny::NS(id)
+
+  TableModuleUI(
+    ns("datasets"),
+    width = width,
+    height = height,
+    title = title,
+    info.text = info.text,
+    caption = caption,
+    label = "",
+    show.maximize = FALSE
+  )
+}
+
+upload_table_preview_contrasts_server <- function(id,
+                                               uploaded,
+                                               scrollY) {
+  moduleServer(id, function(input, output, session) {
+    table_data <- shiny::reactive({
+      shiny::req(uploaded$checklist$contrasts.csv$file)
+      uploaded$checklist$contrasts.csv$file
+    })
+
+    table.RENDER <- function() {
+      dt <- table_data()
+      req(dt)
+      DT::datatable(dt,
+                    class = "compact hover",
+                    rownames = TRUE,
+                    extensions = c("Buttons", "Scroller"),
+                    plugins = "scrollResize",
+                    selection = list(mode = "single", target = "row", selected = 1),
+                    options = list(
+                      dom = "lfrtip",
+                      scroller = TRUE,
+                      scrollX = TRUE,
+                      scrollY = scrollY,
+                      scrollResize = TRUE,
+                      deferRender = TRUE
+                    )
+      ) %>%
+        DT::formatStyle(0, target = "row", fontSize = "11px", lineHeight = "70%")
+    }
+
+    TableModuleServer(
+      "datasets",
+      func = table.RENDER,
+      selector = "none"
+    )
+  }) ## end of moduleServer
+} ## end of server
+
+
+# function to format datatable for highlighting and selecting columns instead of rows
+# not currently used
+make_preview_table <- function(tbl) {
+
+  # callback for highlighting column instead of row
+  js <- c("table.on('mouseenter', 'td', function () {
+                        // Remove highlight from all columns
+                        table
+                        .columns()
+                        .nodes()
+                        .flatten()  // Reduce to a 1D array
+                        .to$()      // Convert to a jQuery object
+                        .removeClass( 'highlight' );
+
+                                // Add highlight to mouseover column
+                        table
+                        .column( this )
+                        .nodes()
+                        .to$()      // Convert to a jQuery object
+                        .addClass( 'highlight' );
+                    });",
+          "table.on('mouseleave', 'td', function () {
+                        // Remove highlight from all columns
+                        table
+                        .columns()
+                        .nodes()
+                        .flatten()  // Reduce to a 1D array
+                        .to$()      // Convert to a jQuery object
+                        .removeClass( 'highlight' );
+                    });")
+
+  DT::datatable(tbl,
+                class = "compact",
+                rownames = TRUE,
+                options = list(
+                  dom = "rtp",
+                  pageLength = 20
+                ),
+                callback = DT::JS(js),
+                selection = list(target = 'column', mode = 'single')
+  ) %>%
+    DT::formatStyle(0, target = "row",
+                    fontSize = "12px", lineHeight = "70%")
+}
+
+
