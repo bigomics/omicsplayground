0473432f2afbeb531bbbf20e6c7747e47a17e926
Cano-Muniz, Santiago <33370667+phisanti@users.noreply.github.com>
2023-09-07 22:46:10 +0100

    add as.character()

--- .github/workflows/build-test-push/entrypoint.sh
+++ .github/workflows/build-test-push/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Run tests
-R --slave -e "cat('Current working directory:', getwd(), '\n')"
 R -e "shiny::runTests();all(shiny::runTests()[[2]])"
-R --slave -e "writeLines(all(shiny::runTests()[[2]]), 'test_result.txt')"
+R --slave -e "writeLines(as.character(all(shiny::runTests()[[2]])), 'test_result.txt')"
 #R --slave -e "all(shiny::runTests()[[2]])" > test_result.txt
 
 # Read test results from file
@@ -12,12 +11,3 @@ echo "Test results: $test_result"
 
 # return test result as an output
 echo ::set-output name=test_result::$test_result
-
-# Check test results and run command
-if [[ $test_result == FALSE ]]; then
-  echo "All tests passed. Running command..."
-  exec "$@"
-else
-  echo "Some tests failed. Not running command."
-  exit 0
-fi
