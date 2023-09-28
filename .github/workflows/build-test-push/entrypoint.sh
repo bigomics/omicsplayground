d8271120cd5012df40869ed579e18b3c1f36ae67
Mauro Masiero <mauromiguelm@hotmail.com>
2023-09-28 09:18:48 +0200

    dbg

--- .github/workflows/build-test-push/entrypoint.sh
+++ .github/workflows/build-test-push/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# echo current dir
+echo "current dir: $(pwd)"
+
 # Run tests
 R -e "x <- shiny::runTests(assert = FALSE); writeLines(as.character(all(x[[2]])), 'test_result.txt')"
+
+# echo getwd
+echo "getwd()" | R --vanilla --slave
 
 # Read test results from file
 test_result=$(cat test_result.txt)
