d3698509e2f038bee3a259c7a4221a9b1f730d9a
Mauro Masiero <mauromiguelm@hotmail.com>
2023-09-28 09:49:44 +0200

    define env variable

--- .github/workflows/build-test-push/entrypoint.sh
+++ .github/workflows/build-test-push/entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+# Define GITHUB_OUTPUT environment variable
+GITHUB_OUTPUT="/github/workspace/output.txt"
 
+# Set output variable with current working directory
 echo "::set-output name=working_directory::$(pwd)"
 
 # Run tests
 R -e "x <- shiny::runTests(assert = FALSE); writeLines(as.character(all(x[[2]])), 'test_result.txt')"
 
-
 # Read test results from file
 test_result=$(cat test_result.txt)
-echo "Test results: $test_result"
 
-# return test result as an output
+# Set output variable with test results
+echo "::set-output name=test_result::$test_result"
+
+# Write test result to file
+echo "$test_result" > test_result_output.txt
+
+# Return test result as an output
 echo "test_result=$test_result" >> $GITHUB_OUTPUT
