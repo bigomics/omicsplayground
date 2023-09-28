60d14d9cccf606f43f4c867947fefe9730745c07
Mauro Masiero <mauromiguelm@hotmail.com>
2023-09-28 12:33:01 +0200

    old format to push to github output

--- .github/workflows/build-test-push/entrypoint.sh
+++ .github/workflows/build-test-push/entrypoint.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-# Set output variable with current working directory
-echo "::set-output name=working_directory::$(pwd)"
-
 # Run tests
 R -e "x <- shiny::runTests(assert = FALSE); writeLines(as.character(all(x[[2]])), 'test_result.txt')"
 
 # Read test results from file
 test_result=$(cat test_result.txt)
+echo "Test results: $test_result"
 
-# Set output variable with test results
-echo "::set-output name=test_result::$test_result"
-
-# Write test result to file
-echo "$test_result" > test_result_output.txt
-
-# Return test result as an output
-echo "test_result=$test_result" >> $GITHUB_OUTPUT
+# return test result as an output
+echo ::set-output name=test_result::$test_result
+echo "{test_result}={$test_result}" >> $test_result
