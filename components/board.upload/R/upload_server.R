3d469762e91f3929078ee1f61250748b9b6597fa
Ivo Kwee <ivo.kwee@gmail.com>
2023-11-02 00:35:29 +0100

    code: cleanup pcaplot code

--- components/board.upload/R/upload_server.R
+++ components/board.upload/R/upload_server.R
@@ -587,7 +587,6 @@ UploadBoard <- function(id,
     correctedX <- upload_module_batchcorrect_server(
       id = "batchcorrect",
       X = shiny::reactive(uploaded$counts.csv),
-      ## X = normalized_counts,  !
       is.count = TRUE,
       pheno = shiny::reactive(uploaded$samples.csv),
       height = height
@@ -683,39 +682,14 @@ UploadBoard <- function(id,
       uploaded
     )
 
-    ## buttonInput <- function(FUN, len, id, ...) {
-    ##   inputs <- character(len)
-    ##   for (i in seq_len(len)) {
-    ##     inputs[i] <- as.character(FUN(paste0(id, i), ...))
-    ##   }
-    ##   inputs
-    ## }
 
-    ## output$checkTablesOutput <- DT::renderDataTable({
-    ##   ## Render the upload status table
-    ##   if (!input$advanced_mode) {
-    ##     return(NULL)
-    ##   }
-    ##   df <- checkTables()
-    ##   dt <- DT::datatable(
-    ##     df,
-    ##     rownames = FALSE,
-    ##     selection = "none",
-    ##     class = "compact cell-border",
-    ##     options = list(
-    ##       dom = "t"
-    ##     )
-    ##   ) %>%
-    ##     DT::formatStyle(0, target = "row", fontSize = "12px", lineHeight = "100%")
-    ## })
-
-    upload_plot_pcaplot_server(
-      "pcaplot",
-      phenoRT = shiny::reactive(uploaded$samples.csv),
-      countsRT = corrected_counts,
-      sel.conditions = sel.conditions,
-      watermark = WATERMARK
-    )
+    ## upload_plot_pcaplot_server(
+    ##   "pcaplot",
+    ##   phenoRT = shiny::reactive(uploaded$samples.csv),
+    ##   countsRT = corrected_counts,
+    ##   sel.conditions = sel.conditions,
+    ##   watermark = WATERMARK
+    ## )
 
     ## ------------------------------------------------
     ## Board return object
