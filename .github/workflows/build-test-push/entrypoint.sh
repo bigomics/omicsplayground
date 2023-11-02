5af6be0e6f39d0edcb7ec7b59438619e05fcc86d
Mauro Masiero <mauromiguelm@hotmail.com>
2023-11-02 11:10:28 +0100

    bring back two outputs for test_result

--- .github/workflows/build-test-push/entrypoint.sh
+++ .github/workflows/build-test-push/entrypoint.sh
@@ -8,11 +8,7 @@ R -e "x <- shiny::runTests(assert = FALSE); writeLines(as.character(all(x[[2]]))
 test_result=$(cat test_result.txt)
 echo "Test results: $test_result"
 
-# return git diff to output
-git_diff=$(git diff)
-echo "::set-output name=git_diff::$(git_diff)"
-
 # return test result as an output
 echo ::set-output name=test_result::$test_result
-#echo "{test_result}={$test_result}" >> $test_result # return test result as an output
+echo "{test_result}={$test_result}" >> $test_result # return test result as an output
 
