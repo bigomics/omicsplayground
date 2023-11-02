abbb2e6566b8f7d087f75fbfa82cd378a7c824d8
Mauro Masiero <mauromiguelm@hotmail.com>
2023-11-02 21:42:17 +0100

    test

--- .github/workflows/build-test-push/entrypoint.sh
+++ .github/workflows/build-test-push/entrypoint.sh
@@ -9,6 +9,6 @@ test_result=$(cat test_result.txt)
 echo "Test results: $test_result"
 
 # return test result as an output
-echo ::set-output name=test_result::$test_result
+#echo ::set-output name=test_result::$test_result
 echo "{test_result}={$test_result}" >> $test_result # return test result as an output
 
