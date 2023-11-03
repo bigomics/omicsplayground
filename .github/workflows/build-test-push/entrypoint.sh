448e9a2273035e91aac734bb4b46e502d0a6efe4
Mauro Masiero <mauromiguelm@hotmail.com>
2023-11-03 08:24:40 +0100

    add braces

--- .github/workflows/build-test-push/entrypoint.sh
+++ .github/workflows/build-test-push/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash
 
 # Run tests
 R -e "x <- shiny::runTests(assert = FALSE); writeLines(as.character(all(x[[2]])), 'test_result.txt')"
@@ -8,5 +8,5 @@ test_result=$(cat test_result.txt)
 
 # return test result as an output
 #echo ::set-output name=test_result::$test_result
-echo "test_result=$test_result" >> $GITHUB_OUTPUT # return test result as an output
+echo "{test_result}={$test_result}" >> $GITHUB_OUTPUT # return test result as an output
 
