6075456d3cbc1786e5344c6bb1c1674aa2f14ae8
Mauro Masiero <mauromiguelm@hotmail.com>
2023-11-03 08:38:25 +0100

    bring back old set-output

--- .github/workflows/build-test-push/entrypoint.sh
+++ .github/workflows/build-test-push/entrypoint.sh
@@ -6,7 +6,9 @@ R -e "x <- shiny::runTests(assert = FALSE); writeLines(as.character(all(x[[2]]))
 # Read test results from file
 test_result=$(cat test_result.txt)
 
-# return test result as an output
-#echo ::set-output name=test_result::$test_result
-echo "{test_result}={$test_result}" >> $env:GITHUB_OUTPUT # return test result as an output
+# return test result as an output (will be deprecated)
+echo ::set-output name=test_result::$test_result
+
+# line above should be switched to this, but for some reason it does not work
+#echo "{test_result}={$test_result}" >> $env:GITHUB_OUTPUT # return test result as an output
 
