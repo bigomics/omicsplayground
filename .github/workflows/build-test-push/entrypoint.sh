4726486adf780ade33e44e3da93119f6aa8c85d6
Mauro Masiero <mauromiguelm@hotmail.com>
2023-11-03 08:01:03 +0100

    change sheebang to match GHA reference

--- .github/workflows/build-test-push/entrypoint.sh
+++ .github/workflows/build-test-push/entrypoint.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
+#!/bin/sh -l
 
-echo "::set-output name=working_directory::$(pwd)"
 # Run tests
 R -e "x <- shiny::runTests(assert = FALSE); writeLines(as.character(all(x[[2]])), 'test_result.txt')"
 
 # Read test results from file
 test_result=$(cat test_result.txt)
-echo "Test results: $test_result"
 
 # return test result as an output
 #echo ::set-output name=test_result::$test_result
