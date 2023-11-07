9c20c369e822fb5c933651af2c871161953706e5
Ivo Kwee <ivo.kwee@gmail.com>
2023-11-08 00:43:02 +0100

    change rendering multiple subplot to layout columns
    change rendering multiple from plotly::subplot using  bslib::layout_columns. this allows better control

--- components/board.dataview/R/dataview_plot_phenoassociation.R
+++ components/board.dataview/R/dataview_plot_phenoassociation.R
@@ -14,8 +14,9 @@ dataview_plot_phenoassociation_ui <- function(
   ns <- shiny::NS(id)
 
   opts <- shiny::tagList(
-    withTooltip(shiny::checkboxInput(ns("phenoclustsamples"), "cluster samples", TRUE),
-      "Cluster samples.",
+    withTooltip(
+      shiny::selectInput(ns("vars"), "show variables:", choices=NULL, multiple=TRUE),
+      "Select phenotype variables to show.",
       placement = "top"
     )
   )
@@ -35,31 +36,42 @@ dataview_plot_phenoassociation_ui <- function(
 
 dataview_plot_phenoassociation_server <- function(id, pgx, r.samples, watermark = FALSE) {
   moduleServer(id, function(input, output, session) {
+
+    observeEvent( pgx$samples, {
+      vars <- colnames(pgx$samples)
+      vars <- unique(c(grep("^[.]|cluster", vars, invert=TRUE, value=TRUE), vars))
+      sel.vars <- head(vars,5)
+      shiny::updateSelectInput(session, "vars", choices = vars, selected = sel.vars)
+    })
+    
     plot_data <- shiny::reactive({
-      shiny::req(pgx$X, pgx$Y)
+      shiny::req(pgx$samples, input$vars)
+      vars <- input$vars
       samples <- r.samples()
       annot <- pgx$samples
-      annot <- annot[samples, , drop = FALSE]
+      if(!all(vars %in% colnames(annot))) return(NULL)
+      if(!all(samples %in% rownames(annot))) return(NULL)
+      annot <- annot[samples, vars, drop = FALSE]
       list(annot = annot)
     })
 
     plot.RENDER <- function() {
       res <- plot_data()
       shiny::req(res)
-
+      
       check_diversity_in_colums <- function(df) {
         sum(unlist(apply(df, 2, function(x) length(unique(x)) > 1))) > 1
       }
 
       if (check_diversity_in_colums(res$annot) && is.data.frame(res$annot)) {
-        ## NOTE: the package doesnt allow to change the typeface, the spacing of the legend, sizes + formatting of labels, ...
-        ## TODO: reimplement in plotly (not me as code is complex and not intuitive at all)
-        pq <- playbase::pgx.testPhenoCorrelation(
+        ## NOTE: the package doesnt allow to change the typeface, the
+        ## spacing of the legend, sizes + formatting of labels, ...
+        pq <- playbase::pgx.testPhenoCorrelation2(
           df = res$annot,
           plot = TRUE,
-          cex = 1
+          cex = 0.8
         )
-        return(pq)
+        return(NULL)
       } else {
         shiny::validate(shiny::need(nrow(res) > 0, "The filters have no diference across samples,please choose another filter."))
         return(NULL)
