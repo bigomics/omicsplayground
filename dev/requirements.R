6bce790196a2e8f0c266b29e32b6934aa5cdf7da
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2023-11-25 18:49:43 +0100

    feat: move packages to base image
    Moved bslib (fixed version) to the base image build
    
    Also moved static (we do not update them) packages to be installed on base.

--- dev/requirements.R
+++ dev/requirements.R
@@ -176,6 +176,8 @@ install.github('bigomics/PCSF', force=TRUE)
 install.github('bigomics/shinyChatR')
 install.github('bigomics/fgsea')
 install.github('ropensci/iheatmapr')
+install.github('rstudio/bslib@v0.5.1',dependencies=FALSE)
+install.github('rstudio/htmltools',dependencies=FALSE)
 
 ##---------------------------------------------------------------------
 ## ONLY DEV.MODE (single-cell trajectories)
