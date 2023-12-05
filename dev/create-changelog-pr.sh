83c7c7fd09dc4e77e242f611c2dc06af33e80534
Mauro Masiero <mauromiguelm@hotmail.com>
2023-12-05 17:24:07 +0100

    basic sh file

---
+++ dev/create-changelog-pr.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+##previous_tag=HEAD
+previous_tag=0
+FILTER="${1:-.*}"
+NHEAD="${2:-999}"
+for current_tag in $(git tag --sort=-creatordate | head -n ${NHEAD})
+do
+if [ "$previous_tag" != 0 ];then
+    tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
+    printf "### ${previous_tag} (${tag_date})\n\n"
+    git log ${current_tag}...${previous_tag} --pretty=format:'-  %s [view](https://github.com/bigomics/omicsplayground/commit/%H)' --reverse | grep -E -v "(Merge|Style)" | grep -i -E "$FILTER"
+    printf "\n\n"
+fi
+previous_tag=${current_tag}
+done
