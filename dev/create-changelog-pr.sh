6a45294f6661a663cb9e418ded498b41782b3e6a
Mauro Masiero <mauromiguelm@hotmail.com>
2023-12-05 17:38:26 +0100

    first attempt

--- dev/create-changelog-pr.sh
+++ dev/create-changelog-pr.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-##previous_tag=HEAD
 previous_tag=0
 FILTER="${1:-.*}"
 NHEAD="${2:-999}"
 for current_tag in $(git tag --sort=-creatordate | head -n ${NHEAD})
 do
-if [ "$previous_tag" != 0 ];then
-    tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
-    printf "### ${previous_tag} (${tag_date})\n\n"
-    git log ${current_tag}...${previous_tag} --pretty=format:'-  %s [view](https://github.com/bigomics/omicsplayground/commit/%H)' --reverse | grep -E -v "(Merge|Style)" | grep -i -E "$FILTER"
-    printf "\n\n"
-fi
-previous_tag=${current_tag}
+    if [ "$previous_tag" != 0 ];then
+        tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
+        printf "### ${previous_tag} (${tag_date})\n\n"
+        for pr in $(git log ${current_tag}...${previous_tag} --pretty="%s" --merges | grep pull | sed -e "s/.*#\([0-9]\+\).*/\1/g" | sort -rn | uniq); do
+            PR_title=$(curl -s https://api.github.com/repos/bigomics/omicsplayground/pulls/${pr} | jq -r '.title')
+            echo "- ${PR_title} [view](https://github.com/bigomics/omicsplayground/pull/${pr})"
+        done
+        printf "\n\n"
+    fi
+    previous_tag=${current_tag}
 done
