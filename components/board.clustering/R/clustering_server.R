471d2e01a009bc9c107e1dcec236d437690d57bc
Ivo Kwee <ivo.kwee@gmail.com>
2024-01-04 13:00:31 +0100

    set default number of selected to 6

--- components/board.clustering/R/clustering_server.R
+++ components/board.clustering/R/clustering_server.R
@@ -78,7 +78,7 @@ ClusteringBoard <- function(id, pgx) {
       }
       shiny::updateSelectInput(session, "hmpca.colvar", choices = var.types0, selected = sel)
       shiny::updateSelectInput(session, "hmpca.shapevar", choices = var.types1, selected = "<none>")
-      shiny::updateSelectInput(session, "selected_phenotypes", choices = var.types, selected = head(var.types, 8))
+      shiny::updateSelectInput(session, "selected_phenotypes", choices = var.types, selected = head(var.types, 6))
     })
 
     ## update filter choices upon change of data set
