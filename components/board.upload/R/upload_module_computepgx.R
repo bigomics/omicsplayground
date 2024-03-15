1907c9c17ed9a8390be4dd006f3651d853109592
Mauro Masiero <mauromiguelm@hotmail.com>
2024-03-15 13:06:06 +0100

    feat: shinyvalidate now works!

--- components/board.upload/R/upload_module_computepgx.R
+++ components/board.upload/R/upload_module_computepgx.R
@@ -278,12 +278,20 @@ upload_module_computepgx_server <- function(
         )
       })
 
-
-      #TODO validate text inputs breaks wizard sequence
-      # iv <- shinyvalidate::InputValidator$new()
-      # iv$add_rule("selected_name", shinyvalidate::sv_required())
-      # iv$add_rule("selected_description", shinyvalidate::sv_required())
-
+      # add shinyvalidate when upload_wizard == "Dataset description"
+      observeEvent(upload_wizard(), {
+        browser()
+        req(upload_wizard() == "Dataset description")
+        
+        if (upload_wizard() == "Dataset description") {
+          browser()
+          #TODO validate text inputs breaks wizard sequence
+          iv <- shinyvalidate::InputValidator$new()
+          iv$add_rule("selected_name", shinyvalidate::sv_required())
+          iv$add_rule("selected_description", shinyvalidate::sv_required())
+          iv$enable()
+        }
+      })
 
 
       shiny::outputOptions(output,
