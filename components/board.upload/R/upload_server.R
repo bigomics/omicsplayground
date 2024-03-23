a6bd0da06958d1a4679eee7c8a7f8a8a9369aab3
Mauro Masiero <mauromiguelm@hotmail.com>
2024-03-23 08:00:36 +0100

    remove print statement

--- components/board.upload/R/upload_server.R
+++ components/board.upload/R/upload_server.R
@@ -670,7 +670,6 @@ UploadBoard <- function(id,
     # lock wizard it compute step
     observeEvent(
       list(input$upload_wizard, upload_name(), upload_datatype(), upload_description(), upload_organism()), {
-        print(input$upload_wizard)
         req(input$upload_wizard == "Dataset description")
         if (is.null(upload_name()) || upload_name() == "" || upload_description() == "" || is.null(upload_description())){
           wizardR::lock("upload_wizard")
