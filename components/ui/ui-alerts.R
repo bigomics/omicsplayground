cf563f77a71ea5009fdaf8e4079bee5512a9f2a7
ivokwee <ivokwee@users.noreply.github.com>
2024-04-26 17:42:25 +0000

    Style code (GHA)

--- components/ui/ui-alerts.R
+++ components/ui/ui-alerts.R
@@ -4,19 +4,21 @@
 ##
 
 
-shinyalert_storage_full <- function(numpgx=NULL, maxpgx=NULL) {
+shinyalert_storage_full <- function(numpgx = NULL, maxpgx = NULL) {
   msg <- paste("You have reached your dataset limit.")
-  if(!is.null(numpgx) && !is.null(maxpgx)) {
+  if (!is.null(numpgx) && !is.null(maxpgx)) {
     msg <- paste(
-      "You currently have",numpgx,"datasets.",
-      "You have reached your dataset limit of",maxpgx,".")
+      "You currently have", numpgx, "datasets.",
+      "You have reached your dataset limit of", maxpgx, "."
+    )
   }
   msg <- paste(
-    msg,"Please <a href='https://events.bigomics.ch/upgrade' target='_blank'>",
+    msg, "Please <a href='https://events.bigomics.ch/upgrade' target='_blank'>",
     "<b><u>upgrade</u></b></a> or get free extra datasets by entering our",
     "<a href='https://bigomics.ch/contact-us' target='_blank'><b><u>Ambassador",
-    "or Promoter</u></b></a> program.")
-    
+    "or Promoter</u></b></a> program."
+  )
+
   shinyalert::shinyalert(
     title = "Your storage is full!",
     text = HTML(msg),
