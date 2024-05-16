737738851c842ca892f19aedf46da401507b0090
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2024-05-16 13:24:32 +0200

    fix: extra time + connectivity more duration

--- tests/testthat/test-snapshot.R
+++ tests/testthat/test-snapshot.R
@@ -45,6 +45,7 @@ test_that("example data loads with no error",{
     App$set_inputs("pgx_path" = pgx_file)
     if(board == "enrichment") {
       App$set_inputs("enrichment-gs_fdr" = 0.5)
+      App$wait_for_idle(duration = 3000, timeout = 35000)
     }
     tabs <- searchTabs(board)
     if (!is.null(tabs)){
@@ -52,10 +53,12 @@ test_that("example data loads with no error",{
         App$run_js(generate_js_click_code(tab))
         if(board == "connectivity") {
           duration <- 1000000
+          App$wait_for_idle(duration = 10000, timeout = duration)
         } else {
           duration <- 50000
+          App$wait_for_idle(duration = 3000, timeout = duration)
         }
-        App$wait_for_idle(duration = 3000, timeout = duration)
+        
         App$expect_screenshot(cran = TRUE, name = paste0(board, "_", tab), threshold = 10, selector = "viewport")
       })
     } else {
