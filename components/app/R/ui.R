bbde16ca381a24e9bb08cdfa28ce9207199af5f6
ESCRI11 <xavier.escriba.montagut@gmail.com>
2024-07-08 13:08:11 +0200

    feat: cog as other elements

--- components/app/R/ui.R
+++ components/app/R/ui.R
@@ -277,13 +277,20 @@ app_ui <- function(x) {
             ),
             logout.tab
           ),
-          bigdash::hover_dropdown(
-            bslib::input_switch("enable_beta", "Enable beta features"),
-            bslib::input_switch("enable_info", "Show info alerts", value = TRUE),
-            selector_switch(
-              class = "card-footer-checked",
-              label = "show captions",
-              is.checked = FALSE
+          bigdash::navbarDropdown(
+            shiny::icon("cog"),
+            bigdash::navbarDropdownItem(
+              bslib::input_switch("enable_beta", "Enable beta features")
+            ),
+            bigdash::navbarDropdownItem(
+              bslib::input_switch("enable_info", "Show info alerts", value = TRUE)
+            ),
+            bigdash::navbarDropdownItem(
+              selector_switch(
+                class = "card-footer-checked",
+                label = "show captions",
+                is.checked = FALSE
+              )
             )
           )
         ),
