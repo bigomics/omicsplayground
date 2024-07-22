f612fbc57384bfc681e12c397820a3bad5f70261
zitoa <zito.a@hotmail.com>
2024-07-22 12:53:31 +0200

    Use expression.csv in initial dialog box

--- components/board.upload/R/upload_module_initial_settings.R
+++ components/board.upload/R/upload_module_initial_settings.R
@@ -44,7 +44,7 @@ upload_module_initial_settings_server <- function(
                 style = "display: flex; flex-direction: column; align-items: center; gap: 20px;",
                 div(
                   style = "margin-top: 0px; width: 40%;",
-                  bs_alert(HTML("This dialog will guide you to upload your own data into Omics Playground. You should prepare at the minimum two CSV files: a <b>counts.csv</b> file (containing your transcriptomics or proteomics data) and a <b>samples.csv</b> file (containing your sample information). A third <b>contrasts.csv</b> file (describing your comparisons) is optional. Read more about data preparation <a href='https://omicsplayground.readthedocs.io/en/latest/dataprep/dataprep/'><u>here</u></a>."), closable = FALSE)
+                  bs_alert(HTML("This dialog will guide you to upload your own data into Omics Playground. You should prepare at the minimum two CSV files: an <b>expression.csv</b> file (containing your transcriptomics or proteomics data) and a <b>samples.csv</b> file (containing your sample information). A third <b>contrasts.csv</b> file (describing your comparisons) is optional. Read more about data preparation <a href='https://omicsplayground.readthedocs.io/en/latest/dataprep/dataprep/'><u>here</u></a>."), closable = FALSE)
                 ),
                 br(),
                 div(
