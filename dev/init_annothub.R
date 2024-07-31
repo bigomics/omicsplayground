3fe25ffa8715321e3cfc4d3753bc89c9c0637df0
Ivo Kwee <ivo.kwee@gmail.com>
2024-07-31 11:06:36 +0200

    enable org.eg.db instead of AnnotHub

--- dev/init_annothub.R
+++ dev/init_annothub.R
@@ -6,24 +6,30 @@
 ## Copyright (c) 2018-2024 BigOmics Analytics SA. All rights reserved.
 ##
 
-##dir("~/.cache/R/AnnotationHub")
-ah <- AnnotationHub::AnnotationHub()
+if(1) {
+  if(!require("org.Hs.eg.db")) BiocManager::install("org.Hs.eg.db")
+  if(!require("org.Mm.eg.db")) BiocManager::install("org.Mm.eg.db")
+  if(!require("org.Rn.eg.db")) BiocManager::install("org.Rn.eg.db")
+}
 
-species <- c(
-  "Homo sapiens",
-  "Mus musculus",
-  "Rattus norvegicus"
-#  "Saccharomyces cerevisiae",
-#  "Drosophila melanogaster",
-#  "Caenorhabditis elegans"
-)
-
-s <- species[1]
-for(s in species) {
-  cat("Retrieving OrgDb for",s,"...\n")
-  res <- AnnotationHub::query(ah, pattern = c(s, "OrgDb"))  
-  suppressMessages(
-    db <- try(res[[length(res)]])
+if(0) {
+  ##dir("~/.cache/R/AnnotationHub")
+  ah <- AnnotationHub::AnnotationHub()  
+  species <- c(
+    "Homo sapiens",
+    "Mus musculus",
+    "Rattus norvegicus"
+    ##  "Saccharomyces cerevisiae",
+    ##  "Drosophila melanogaster",
+    ##  "Caenorhabditis elegans"
   )
+  s <- species[1]
+  for(s in species) {
+    cat("Retrieving OrgDb for",s,"...\n")
+    res <- AnnotationHub::query(ah, pattern = c(s, "OrgDb"))  
+    suppressMessages(
+      db <- try(res[[length(res)]])
+    )
+  }
 }
 
