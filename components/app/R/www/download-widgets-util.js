72ff15e5bd065aeeaa2392e0f16c8272cf1bedc2
ESCRI11 <xavier.escriba.montagut@gmail.com>
2024-09-10 22:37:13 +0200

    update download JS

--- components/app/R/www/download-widgets-util.js
+++ components/app/R/www/download-widgets-util.js
@@ -1,21 +1,19 @@
 Shiny.addCustomMessageHandler("export-widget", function(message) {
     var widgetId = message.id;
+    var plotName = message.name;
     var widgetElement = document.getElementById(widgetId);
   
     if (widgetElement) {
-      html2canvas(widgetElement, {
-        onrendered: function(canvas) {
-          var imgData = canvas.toDataURL("image/png");
-          
-          var link = document.createElement('a');
-          link.href = imgData;
-          link.download = 'widget.png';
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-        }
-      });
+      html2canvas(widgetElement).then(canvas => {
+        var imgData = canvas.toDataURL("image/png");
+        var link = document.createElement('a');
+        link.href = imgData;
+        link.download = plotName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
     } else {
       console.error("Element with ID " + widgetId + " not found.");
     }
-  });
+  })
