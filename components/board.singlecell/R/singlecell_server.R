b30ee37788ae23b3a93731d3f99fe0e78fccdeb7
ESCRI11 <xavier.escriba.montagut@gmail.com>
2024-10-10 14:25:28 +0200

    feat: remove server
    with server true, given we update them on the server, not on the board,
    sometimes they get updated before being on the DOM, therefore the plot
    ends up blank

--- components/board.singlecell/R/singlecell_server.R
+++ components/board.singlecell/R/singlecell_server.R
@@ -133,9 +133,10 @@ SingleCellBoard <- function(id, pgx) {
       if (length(g1) == 0) g1 <- genes[1]
       if (length(g2) == 0) g2 <- genes[2]
 
-      shiny::updateSelectizeInput(session, "cytovar1", choices = genes, selected = g1, server = TRUE)
-      shiny::updateSelectizeInput(session, "cytovar2", choices = genes, selected = g2, server = TRUE)
-      shiny::updateSliderInput(session, "nbins", min = 0, max = 50, value = 5, step = 5)
+      browser()
+      shiny::updateSelectizeInput(session, "cytovar1", choices = genes, selected = g1)#, server = TRUE)
+      shiny::updateSelectizeInput(session, "cytovar2", choices = genes, selected = g2)#, server = TRUE)
+      ## shiny::updateSliderInput(session, "nbins", min = 0, max = 50, value = 5, step = 5)
     })
 
     # Observe tabPanel change to update Settings visibility
