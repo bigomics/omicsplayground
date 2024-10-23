da53989186aeeb9aa26a97556377be00192b2eb3
ESCRI11 <xavier.escriba.montagut@gmail.com>
2024-10-23 21:48:06 +0200

    wip

--- components/app/R/www/plotModule.js
+++ components/app/R/www/plotModule.js
@@ -1,15 +1,20 @@
-$(document).on('click', '#toggle_button', function() {
+$(document).on('click', '[id$="toggle_button"]', function() {
       var $card = $(this).closest('.card');
       var isFullScreen = $card.attr('data-full-screen') === 'true';
+
+      var elementId = $(this).attr('id');
+      var prefix = elementId.split('toggle_button')[0];
 
       if (!isFullScreen) {
         // Entering full screen: add `bslib-full-screen-overlay` as sibling before card
         var $overlay = $('<div>', { id: 'bslib-full-screen-overlay' });
         $card.before($overlay);  // Insert the overlay before the card
         $card.attr('data-full-screen', 'true');
+        Shiny.setInputValue(prefix + 'fullscreen', true);
       } else {
         // Exiting full screen: remove the `bslib-full-screen-overlay`
         $card.prev('#bslib-full-screen-overlay').remove();  // Remove the overlay
         $card.attr('data-full-screen', 'false');
+        Shiny.setInputValue(prefix + 'fullscreen', false);
       }
 });
