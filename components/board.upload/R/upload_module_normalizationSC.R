801409548f7258b52138d87eece166e7b62510e6
zitoa <zito.a@hotmail.com>
2024-11-19 15:28:32 +0100

    wip

--- components/board.upload/R/upload_module_normalizationSC.R
+++ components/board.upload/R/upload_module_normalizationSC.R
@@ -20,165 +20,12 @@ upload_module_normalizationSC_server <- function(id,
                                                  upload_datatype,
                                                  is.count = FALSE,
                                                  height = 720) {
+
   shiny::moduleServer(
     id,
     function(input, output, session) {
       ns <- session$ns
       
-      ## ------------------------------------------------------------------
-      ## Object reactive chain
-      ## ------------------------------------------------------------------
-
-      ## downsampled and normalized data
-      ## downsample or still use full dataset if ncells < threshold
-      ds_norm_Counts <- shiny::reactive({ 
-
-        shiny::req(r_counts())
-        shiny::req(r_samples())
-        counts <- r_counts()
-        samples <- r_samples()
-        if (is.null(counts)) { return(NULL) }
-
-        kk <- intersect(colnames(counts), rownames(samples))
-        counts <- counts[, kk, drop = FALSE]
-        samples <- samples[kk, , drop = FALSE]
-        
-        ncells <- ncol(counts)
-        cells_trs <- 2000
-        if (ncells > cells_trs) {
-          dbg("[normalizationSC_server:ds_norm_Counts:] N.cells in dataset ", ncells)
-          dbg("[normalizationSC_server:ds_norm_Counts:] Performing random sampling of 1K cells.")
-          kk <- sample(colnames(counts), 1000)
-          counts <- counts[, kk, drop = FALSE] 
-          samples <- samples[kk, , drop = FALSE]
-        }
-
-        celltype_compute <- TRUE
-        ref_tissue <- input$ref_atlas
-        if (!is.null(ref_tissue) && celltype_compute) {
-          ## if (input$infercelltypes) {
-          dbg("[normalizationSC_server:ds_norm_Counts:] Inferring cell types with Azimuth!")
-          dbg("[normalizationSC_server:ds_norm_Counts:] Reference atlas:", ref_tissue)
-          shiny::withProgress(
-            message = "Inferring cell types with Azimuth...",
-            value = 0.4,
-            {
-              azm <- playbase::pgx.runAzimuth(counts = counts, reference = ref_tissue)
-              dbg("[normalizationSC_server:ds_norm_Counts:] Cell types inference completed.")
-              celltype.azm <- azm[,grep("^predicted.*l2$", colnames(azm))]
-              samples <- cbind(samples, celltype.azm = celltype.azm)
-            }
-          )
-        } else {
-          dbg("[normalizationSC_server:ds_norm_Counts:] Reference missing or cell types presents.")
-        }
-
-        nX <- playbase::logCPM(as.matrix(counts), 1, total = 1e4)
-        return(list(counts = nX, samples = samples))
-
-      })
-
-      ## Dim reductions: top 1000 features
-      dimred_norm_Counts <- shiny::reactive({ 
-        
-        shiny::req(dim(ds_norm_Counts()$counts), dim(ds_norm_Counts()$samples)) 
-        counts <- ds_norm_Counts()$counts
-        samples <- ds_norm_Counts()$samples
-
-        counts <- as.matrix(counts)
-        jj <- head(order(-matrixStats::rowSds(counts, na.rm = TRUE)), 1000)
-        counts1 <- counts[jj, , drop = FALSE]
-        counts1 <- counts1 - rowMeans(counts1, na.rm = TRUE)
-        nb <- ceiling(min(15, dim(counts) / 8))
-        pos.list <- list()
-        shiny::withProgress(
-          message = "Performing PCA, tSNE, UMAP...",
-          value = 0.4,
-          {
-            pos.list[["pca"]] <- irlba::irlba(counts1, nv = 2, nu = 0)$v
-            pos.list[["tsne"]] <- Rtsne::Rtsne(t(counts1), perplexity = 2*nb, check_duplicates=F)$Y
-            pos.list[["umap"]] <- uwot::umap(t(counts1), n_neighbors = max(2, nb))
-            pos.list <- lapply(pos.list, function(x) { rownames(x)=colnames(counts1); return(x) })
-          }
-        )
-        LL <- list(
-          samples = samples,
-          pos.pca = pos.list[["pca"]],
-          pos.tsne = pos.list[["tsne"]],
-          pos.umap = pos.list[["umap"]]
-        )
-
-        return(LL)
-
-      })
-
-      ## ------------------------------------------------------------------
-      ## Plot functions
-      ## ------------------------------------------------------------------
-
-      plot1 <- function() {
-        shiny::req(
-          dim(dimred_norm_Counts()$samples),
-          dim(dimred_norm_Counts()$pos.pca),
-          dim(dimred_norm_Counts()$pos.tsne),
-          dim(dimred_norm_Counts()$pos.umap)
-        ) 
-        samples <- dimred_norm_Counts()$samples
-        pos.list <-  list(
-          pca = dimred_norm_Counts()$pos.pca,
-          tsne = dimred_norm_Counts()$pos.tsne,
-          umap = dimred_norm_Counts()$pos.umap
-        )
-        vars <- input$clusterBy
-        m <- tolower(input$dimred_plottype)
-        if (length(vars) <= 2) {
-          par(mfrow = c(1, length(vars)))
-        } else if (length(vars) > 2 & length(vars) <= 4) {
-          par(mfrow = c(2, 2))
-        } else if (length(vars) > 4) {
-          par(mfrow = c(2, length(vars)))
-        }
-        i <- 1
-        for(i in 1:length(vars)) {
-          v <- samples[, vars[i]]
-          playbase::pgx.scatterPlotXY.BASE(
-            pos = pos.list[[m]], var = v, title = m,
-            xlab = "Dim1", ylab = "Dim2"
-          )
-        }
-      }
-      
-      plot2 <- function() {
-        shiny::req(r_counts())
-        counts <- r_counts()
-        shiny::req(dim(ds_norm_Counts()$samples)) 
-        samples <- ds_norm_Counts()$samples
-        kk <- intersect(colnames(counts), rownames(samples))
-        counts <- counts[, kk, drop = FALSE]
-        samples <- samples[kk, , drop = FALSE]
-        options(Seurat.object.assay.calcn = TRUE)
-        getOption("Seurat.object.assay.calcn")
-        shiny::withProgress(
-          message = "Creating and pre-processing Seurat Object...",
-          value = 0.4,
-          {
-            SO <- Seurat::CreateSeuratObject(counts = counts, meta.data = samples)
-            SO <- Seurat::PercentageFeatureSet(SO, pattern = "^MT-|^Mt-", col.name = "percent.mt")
-            SO <- Seurat::PercentageFeatureSet(SO, pattern = "^HB|^Hb", col.name = "percent.hb")
-            SO <- playbase::seurat.preprocess(SO, sct = FALSE, tsne = FALSE, umap = FALSE)
-          }
-        )
-        kk <- setdiff(colnames(samples), colnames(SO@meta.data))
-        if (length(kk) > 1) {
-          SO@meta.data <- cbind(SO@meta.data, samples[, kk])
-        }
-        require(scplotter)
-        scplotter::FeatureStatPlot(SO,
-          features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.hb"),
-          ident = "celltype.azm", facet_scales = "free_y",
-          theme_args = list(base_size = 18))
-      }
-
       ## ------------------------------------------------------------------
       ## Plot UI
       ## ------------------------------------------------------------------
@@ -186,8 +33,11 @@ upload_module_normalizationSC_server <- function(id,
 
         shiny::req(dim(ds_norm_Counts()$samples))
         samples <- ds_norm_Counts()$samples
-        metadata_vars <- colnames(samples)
-                
+        metadata_vars <- c("celltype.azm", 
+          "orig.ident", "nCount_RNA", "nFeature_RNA",
+          "percent.mt", "percent.ribo", "seurat_clusters")
+        metadata_vars <- unique(c(metadata_vars, colnames(samples)))
+
         dimred.infotext <- "Dimensionality reduction enables to simplify large datasets by computing representative data points capable of preserving the biological information while reducing the dimensionality of the data. Here we employ the two most widely used non-linear methods for dimensional reduction of single-cell RNA-seq data: T-distributed stochastic neighbor embedding (t-SNE), and Unifold Manifold Approximation and Projection (UMAP). https://omicsplayground.readthedocs.io/en/latest/methods/#clustering"
 
         dotplot.infotext <- "Dot plot of top 10 highly variable features in the data. The dot plot allows to visualize the expression changes across different user-defined identity classes (e.g., cell clusters, cell types, phenotype classes). The size of each dot reflects the percentage of cells within a class; the color of each dot indicates the average expression level (computed using Seurat's AverageExpression function) across all cells within a class (blue is high)."
@@ -247,7 +97,7 @@ upload_module_normalizationSC_server <- function(id,
                   label = "Visualize cell cluster by",
                   choices = metadata_vars, ## reactive
                   multiple = TRUE,
-                  selected = "celltype"
+                  selected = "celltype.azm"
                 ),
                 shiny::br()
               )
@@ -274,22 +124,22 @@ upload_module_normalizationSC_server <- function(id,
               heights_equal = "row",
               PlotModuleUI(
                 ns("plot1"),
-                title = "Dimensional reduction",
-                info.text = dimred.infotext,
-                caption = dimred.infotext,
-                options = dimred.options,
-                height = c("auto", "100%"),
-                show.maximize = FALSE
-              ),
-              PlotModuleUI(
-                ns("plot2"),
                 title = "Sequencing depth & samples' QC by cell type",
                 ## info.text = dotplot.infotext, ## update
                 ## caption = dotplot.infotext,
                 ## options = dotplot.options,
                 height = c("auto", "100%"),
                 show.maximize = FALSE,
-                )
+                ),
+              PlotModuleUI(
+                ns("plot2"),
+                title = "Dimensional reduction",
+                info.text = dimred.infotext,
+                caption = dimred.infotext,
+                options = dimred.options,
+                height = c("auto", "100%"),
+                show.maximize = FALSE
+              )
             )
           ),
           div(shiny::checkboxInput(ns("normalizationUI"), NULL, TRUE),
@@ -297,7 +147,169 @@ upload_module_normalizationSC_server <- function(id,
         )            
         return(ui)
       })
+
+      ## ------------------------------------------------------------------
+      ## Object reactive chain
+      ## ------------------------------------------------------------------
+
+      ## downsampled and normalized data
+      ## downsample or still use full dataset if ncells < threshold
+      ds_norm_Counts <- shiny::reactive({ 
+
+        shiny::req(r_counts())
+        shiny::req(r_samples())
+        counts <- r_counts()
+        samples <- r_samples()
+        if (is.null(counts)) { return(NULL) }
+
+        kk <- intersect(colnames(counts), rownames(samples))
+        counts <- counts[, kk, drop = FALSE]
+        samples <- samples[kk, , drop = FALSE]
+        
+        ncells <- ncol(counts)
+        cells_trs <- 2000
+        if (ncells > cells_trs) {
+          dbg("[normalizationSC_server:ds_norm_Counts:] N.cells in dataset ", ncells)
+          dbg("[normalizationSC_server:ds_norm_Counts:] Performing random sampling of 1K cells.")
+          kk <- sample(colnames(counts), 1000)
+          counts <- counts[, kk, drop = FALSE] 
+          samples <- samples[kk, , drop = FALSE]
+        }
+
+        celltype_compute <- TRUE
+        ref_tissue <- input$ref_atlas
+        if (!is.null(ref_tissue) && celltype_compute) {
+          ## if (input$infercelltypes) {
+          dbg("[normalizationSC_server:ds_norm_Counts:] Inferring cell types with Azimuth!")
+          dbg("[normalizationSC_server:ds_norm_Counts:] Reference atlas:", ref_tissue)
+          shiny::withProgress(
+            message = "Inferring cell types with Azimuth...",
+            value = 0.4,
+            {
+              azm <- playbase::pgx.runAzimuth(counts = counts, reference = ref_tissue)
+              dbg("[normalizationSC_server:ds_norm_Counts:] Cell types inference completed.")
+              celltype.azm <- azm[,grep("^predicted.*l2$", colnames(azm))]
+              samples <- cbind(samples, celltype.azm = celltype.azm)
+            }
+          )
+        } else {
+          dbg("[normalizationSC_server:ds_norm_Counts:] Using available cell types.")
+        }
+
+        nX <- playbase::logCPM(as.matrix(counts), 1, total = 1e4)
+        return(list(counts = nX, samples = samples))
+
+      })
+
+      ## Dim reductions: top 1000 features
+      dimred_norm_Counts <- shiny::reactive({ 
+        
+        shiny::req(dim(ds_norm_Counts()$counts))
+        shiny::req(dim(ds_norm_Counts()$samples))
+        counts <- ds_norm_Counts()$counts
+        counts <- as.matrix(counts)
+        samples <- ds_norm_Counts()$samples
+        
+        jj <- head(order(-matrixStats::rowSds(counts, na.rm = TRUE)), 1000)
+        counts1 <- counts[jj, , drop = FALSE]
+        counts1 <- counts1 - rowMeans(counts1, na.rm = TRUE)
+        nb <- ceiling(min(15, dim(counts) / 8))
+        pos.list <- list()
+        shiny::withProgress(
+          message = "Performing PCA, tSNE, UMAP...",
+          value = 0.4,
+          {
+            pos.list[["pca"]] <- irlba::irlba(counts1, nv = 2, nu = 0)$v
+            pos.list[["tsne"]] <- Rtsne::Rtsne(t(counts1), perplexity = 2*nb, check_duplicates=F)$Y
+            pos.list[["umap"]] <- uwot::umap(t(counts1), n_neighbors = max(2, nb))
+            pos.list <- lapply(pos.list, function(x) { rownames(x)=colnames(counts1); return(x) })
+          }
+        )
+
+        options(Seurat.object.assay.calcn = TRUE)
+        getOption("Seurat.object.assay.calcn")
+        shiny::withProgress(
+          message = "Creating & preprocessing Seurat object...",
+          value = 0.4,
+          {
+            SO <- Seurat::CreateSeuratObject(counts = counts, meta.data = samples)
+            SO <- Seurat::PercentageFeatureSet(SO, pattern = "^MT-|^Mt-", col.name = "percent.mt")
+            SO <- Seurat::PercentageFeatureSet(SO, pattern = "^RP[LS]|^Rp[ls]", col.name = "percent.ribo")
+            SO <- playbase::seurat.preprocess(SO, sct = FALSE, tsne = FALSE, umap = FALSE)
+          }
+        )
+        kk <- setdiff(colnames(samples), colnames(SO@meta.data))
+        if (length(kk) > 1) {
+          SO@meta.data <- cbind(SO@meta.data, samples[, kk, drop = FALSE])
+        }
+
+        LL <- list(
+          SO = SO,
+          samples = SO@meta.data,
+          pos.pca = pos.list[["pca"]],
+          pos.tsne = pos.list[["tsne"]],
+          pos.umap = pos.list[["umap"]]
+        )
+
+        return(LL)
+
+      })
+
+      ## ------------------------------------------------------------------
+      ## Plot functions
+      ## ------------------------------------------------------------------
+
+      plot1 <- function() {
+        shiny::req(dim(dimred_norm_Counts()$SO))
+        SO <- dimred_norm_Counts()$SO
+        require(scplotter)
+        scplotter::FeatureStatPlot(
+          SO,
+          features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.ribo"),
+          ident = "celltype.azm", facet_scales = "free_y",
+          theme_args = list(base_size = 18)
+        )
+      }
+
+      plot2 <- function() {
+        shiny::req(
+          dim(dimred_norm_Counts()$samples),
+          dim(dimred_norm_Counts()$pos.pca),
+          dim(dimred_norm_Counts()$pos.tsne),
+          dim(dimred_norm_Counts()$pos.umap)
+        )
+        pos.list <-  list(
+          pca = dimred_norm_Counts()$pos.pca,
+          tsne = dimred_norm_Counts()$pos.tsne,
+          umap = dimred_norm_Counts()$pos.umap
+        )
+        samples <- dimred_norm_Counts()$samples
+        vars <- input$clusterBy
+        shiny::validate(shiny::need(
+          !is.null(vars),
+          "For clustering, please select a metadata variable from the menu on the left."
+        ))        
+        m <- tolower(input$dimred_plottype)
+        if (length(vars) <= 2) {
+          par(mfrow = c(1, length(vars)))
+        } else if (length(vars) > 2 & length(vars) <= 4) {
+          par(mfrow = c(2, 2))
+        } else if (length(vars) > 4 & length(vars) <= 6) {
+          par(mfrow = c(2, 3))
+        } else  if (length(vars) > 6 & length(vars) <= 8) {
+          par(mfrow = c(3, 3))
+        }
+        i <- 1
+        for(i in 1:length(vars)) {
+          playbase::pgx.scatterPlotXY.BASE(
+            pos = pos.list[[m]], var = samples[, vars[i]],
+            title = paste0(m, "; ", vars[i]),
+            xlab = "Dim1", ylab = "Dim2"
+          )
+        }
+      }
       
+
       ## ------------------------------------------------------------------
       ## Plot modules
       ## ------------------------------------------------------------------
