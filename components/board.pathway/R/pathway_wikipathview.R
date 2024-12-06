f714ca2c07ee899908498c5317c692a70faaa4f8
ESCRI11 <xavier.escriba.montagut@gmail.com>
2024-12-06 09:56:30 +0100

    add header

--- components/board.pathway/R/pathway_wikipathview.R
+++ components/board.pathway/R/pathway_wikipathview.R
@@ -1,4 +1,7 @@
-#
+##
+## This file is part of the Omics Playground project.
+## Copyright (c) 2018-2024 BigOmics Analytics SA. All rights reserved.
+##
 
 wikipathview <- function(wp, val) {
   require(xml2)
