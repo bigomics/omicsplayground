3a255aff7242f607348dc6ed909c3ee47f5a90e8
ESCRI11 <xavier.escriba.montagut@gmail.com>
2024-12-09 11:21:43 +0100

    fix: fisher test

--- components/board.clustering/R/clustering_server.R
+++ components/board.clustering/R/clustering_server.R
@@ -679,7 +679,7 @@ ClusteringBoard <- function(id, pgx, labeltype = shiny::reactive("feature")) {
       idxx <- setdiff(idx, c(NA, " ", "   "))
       rho <- matrix(NA, nrow(ref), length(idxx))
       colnames(rho) <- idxx
-      rownames(rho) <- rownames(ref)
+      rownames(rho) <- sub("_", ":", sub(".*:", "", rownames(ref)))
 
       i <- 1
       if (nrow(ref) > 0) {
@@ -696,7 +696,7 @@ ClusteringBoard <- function(id, pgx, labeltype = shiny::reactive("feature")) {
       if (input$hm_level == "gene" && ann.level == "geneset" && clusterannot$xann_odds_weighting()) {
         table(idx)
         grp <- tapply(toupper(rownames(zx)), idx, list) ## toupper for mouse!!
-        gmt <- playdata::getGSETS(rownames(rho))
+        gmt <- playbase::getGSETS_playbase(rownames(rho))
         bg.genes <- toupper(rownames(X))
         P <- c()
         for (i in 1:ncol(rho)) {
@@ -706,13 +706,13 @@ ClusteringBoard <- function(id, pgx, labeltype = shiny::reactive("feature")) {
             fdr = 1, min.genes = 0, max.genes = Inf,
             background = bg.genes
           )
-          res <- res[rownames(rho), ]
           r <- res[, "odd.ratio"]
           odd.prob <- r / (1 + r)
           P <- cbind(P, odd.prob)
         }
         colnames(P) <- colnames(rho)
-        rownames(P) <- rownames(rho)
+        rownames(P) <- names(gmt)
+        rho <- rho[rownames(P),]
         rho <- rho * (P / max(P))
       }
 
