b8a5979007a7a3649f52034977a5bcf2653d35cd
ESCRI11 <xavier.escriba.montagut@gmail.com>
2025-04-22 17:39:48 +0200

    comment

--- components/app/R/www/close-message.js
+++ components/app/R/www/close-message.js
@@ -7,8 +7,8 @@ Shiny.addCustomMessageHandler('warnOnExit', function(message) {
 // Before the user closes the tab
 window.addEventListener('beforeunload', function (e) {
 if (shouldWarnOnExit) {
-        var confirmationMessage = 'Dataset computation is running. Are you sure you want to leave? Leaving will cancel the computation.';
-        (e || window.event).returnValue = confirmationMessage; // For older browsers
-        return confirmationMessage; // For modern browsers
+        var confirmationMessage = 'Dataset computation is running. Are you sure you want to leave? Leaving will cancel the computation.'; // Wont be displayed on modern browsers
+        (e || window.event).returnValue = confirmationMessage;
+        return confirmationMessage;
     }
 });
