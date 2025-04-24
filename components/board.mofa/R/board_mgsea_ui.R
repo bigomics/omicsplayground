924e14b5e02236b29fad427002dcc758ee7e1e3a
zitoa <zito.a@hotmail.com>
2025-04-24 10:43:00 +0200

    mgsea info

--- components/board.mofa/R/board_mgsea_ui.R
+++ components/board.mofa/R/board_mgsea_ui.R
@@ -19,8 +19,7 @@ MGseaUI <- function(id) {
   rowH2 <- 440 ## row 2 height
 
   shiny::div(
-    boardHeader(title = "Multi-Omics GSEA",
-                info_link = ns("info")),
+    boardHeader(title = "Multi-Omics GSEA", info_link = ns("info")),
     shiny::tabsetPanel(
       id = ns("tabs"),
 
@@ -35,13 +34,14 @@ MGseaUI <- function(id) {
             height = "calc(100vh - 181px)",
             col_widths = bslib::breakpoints(
               sm = c(12, 12, 12, 12),
-              xl = c(5, 3, 4, 7, 5),              
+              xl = c(5, 3, 4, 7, 5),
               xxxl = c(4, 3, 5, 4)
             ),
             mgsea_plot_enrichment_ui(
               ns("menrichment"),
               title = "multiGSEA enrichment",
-              info.text = "Upon selection of a contrast from the 'Contrast' drop-down menu on the right, an ordered bar plot of cumulative absolute enrichment score (x-axis) of each gene set is shown. Please select a gene set from the 'multiGSEA scores' table to view a Volcano plot of Effect-size (log2FC) vs Significance (-log10q) for each feature mapped in the selected gene set.",
+              info.text = "Barplot of enrichment score of enriched gene sets for each contrast and Volcano plot of effect sizes. Certain pathways can be enriched on multiple omics layers and are therefore of interest as potentially capturing pathways altered at multi-omics level and/or associated with conditions/traits of interest.",
+              info.methods = "Upon selection of a contrast from the 'Contrast' drop-down menu on the right, an ordered bar plot of cumulative absolute enrichment score (x-axis) of each gene set is shown. Please select a gene set from the 'multiGSEA scores' table to view a Volcano plot of Effect-size (log2FC) vs Significance (-log10q) for each feature mapped in the selected gene set.",
               caption = "Multi-omics gene set enrichment analysis.",
               height = c("100%", TABLE_HEIGHT_MODAL),
               width = c("100%", "100%")
@@ -49,7 +49,8 @@ MGseaUI <- function(id) {
             mofa_plot_mgsea_ui(
               ns("mgsea_plot"),
               title = "MultiGSEA plot",
-              info.text = "MultiGSEA plot. Scatter plot of enrichment scores of two omics data types. Pathway/genesets that are enriched in multiple datatypes are expected to exhibit higher multi.score values. To aid improved visualization a small random noise is added as standard normal values to the enrichment scores. Pathways enriched in both datatypesa are colored in red.",
+              info.text = "Scatter plot of enrichment scores of two omics data types. Pathway/genesets that are enriched in multiple datatypes are expected to exhibit higher multi.score values.",
+              info.methods = "To aid improved visualization a small random noise is added as standard normal values to the enrichment scores. Pathways enriched in both datatypesa are colored in red.",
               caption = "Scatter plot of enrichment scores of two omics data types. Pathway/genesets that are enriched in multiple datatypes are expected to exhibit higher multi.score values. To aid improved visualization a small random noise is added as standard normal values to the enrichment scores.",
               height = c("100%", TABLE_HEIGHT_MODAL),
               width = c("auto", "100%")
@@ -58,8 +59,7 @@ MGseaUI <- function(id) {
               ns("pathwayheatmap"),
               title = "Pathway heatmap",
               info.text = "Heatmap of hierarchical clustering of z-score of features mapped in the selected gene set from the 'multiGSEA scores' table. Metadata variables present in the uploaded samples.csv are shown in the annotation bars at the top of the heatmap. A 'prefix' annotates each feature's datatype.",
-              #info.methods = ".....",
-              #info.references = list(list("X et al")),
+              info.methods = "Top 40 most variable features (by standard deviation) are selected. Features similarities are determined using pearson correlation. Samples distances are determined using Euclidean distance and a dendrogram of sample clusters is generated.",
               caption = "Integrated Multi-omics heatmap.",
               height = c("100%", TABLE_HEIGHT_MODAL),
               width = c("auto", "100%")
