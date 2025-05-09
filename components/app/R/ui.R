8e5fe068fdf38ecee6d98d58f9c1e14d9e5a29a7
ESCRI11 <xavier.escriba.montagut@gmail.com>
2025-05-09 20:37:31 +0200

    wait for logout

--- components/app/R/ui.R
+++ components/app/R/ui.R
@@ -91,7 +91,7 @@ app_ui <- function(x) {
 
       logout.tab <- bigdash::navbarDropdownItem(
         "Logout",
-        onClick = "logoutInApp(); window.location.reload();"
+        onClick = "logoutInApp().then(() => window.location.reload());"
       )
 
       if (opt$AUTHENTICATION == "shinyproxy") {
