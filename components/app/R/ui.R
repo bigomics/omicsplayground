7169bb440af079a4f64cfade832f1ce9df883099
ESCRI11 <xavier.escriba.montagut@gmail.com>
2025-05-09 19:51:12 +0200

    reload after logout

--- components/app/R/ui.R
+++ components/app/R/ui.R
@@ -91,7 +91,7 @@ app_ui <- function(x) {
 
       logout.tab <- bigdash::navbarDropdownItem(
         "Logout",
-        onClick = "logoutInApp()"
+        onClick = "logoutInApp(); window.location.reload();"
       )
 
       if (opt$AUTHENTICATION == "shinyproxy") {
