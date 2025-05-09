f1652259955e600647ea444ce1dea6037f6287cb
ESCRI11 <xavier.escriba.montagut@gmail.com>
2025-05-09 16:23:27 +0200

    feat: relative path

--- components/app/R/ui.R
+++ components/app/R/ui.R
@@ -33,7 +33,7 @@ app_ui <- function(x) {
       expiration = -1,
       http_only = TRUE,
       secure_only = TRUE,
-      redirect = "/close"
+      redirect = "close"
     ))
   } else if (identical("/close", x$PATH_INFO)) {} else if (identical("/", x$PATH_INFO)) {
     #-------------------------------------------------------
