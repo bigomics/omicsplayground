e3206dc7cb94d5d1a00903e4c60e2745b83232ae
ESCRI11 <xavier.escriba.montagut@gmail.com>
2025-10-28 16:38:53 +0100

    fix: add hardcoded default file (control deploy errors)

--- components/app/R/global.R
+++ components/app/R/global.R
@@ -199,8 +199,23 @@ message("************* SETTING DEFAULTS ***************")
 message("************************************************")
 
 defaults.file <- file.path(ETC, "DEFAULTS.yml")
-if (!file.exists(defaults.file)) stop("FATAL ERROR: cannot find DEFAULTS.yml file")
-DEFAULTS <<- yaml::read_yaml(defaults.file)
+if (file.exists(defaults.file)) {
+  DEFAULTS <<- yaml::read_yaml(defaults.file)
+} else {
+  message("[GLOBAL] DEFAULTS.yml not found, using default configuration")
+  DEFAULTS <<- list(
+    computation_options = list(
+      probe_filtering = list(
+        default = c(
+          "remove.notexpressed",
+          "remove.unknown",
+          "only.proteincoding"
+        ),
+        proteomics = list()
+      )
+    )
+  )
+}
 
 ## Check and set authentication method
 if (Sys.getenv("PLAYGROUND_AUTHENTICATION") != "") {
