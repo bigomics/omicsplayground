1e4a7c561b408c488b46d7d7ed8e24f3b60d6989
Ivo Kwee <ivo.kwee@gmail.com>
2026-08-14 13:56:39 +0200

    add port option

--- dev/run_app_headless.R
+++ dev/run_app_headless.R
@@ -3,13 +3,14 @@ source('global.R')
 source('ui.R')
 source('server.R')
 options(shiny.otel.collect = "none") 
-shinyApp(
-    ui = app_ui,
-    server = app_server,
-    uiPattern = '.*',
-    options = list(
-        launch.browser = FALSE,
-        host = "0.0.0.0",
-        port = 3838
-    )
+
+# Accept optional port argument (default 3838). Usage: Rscript dev/run_app_headless.R [PORT]
+args <- commandArgs(trailingOnly = TRUE)
+port <- if (length(args) > 0) as.integer(args[1]) else 3838
+
+shiny::runApp(
+    appDir = ".",
+    launch.browser = FALSE,
+    host = "0.0.0.0",
+    port = port
 )
