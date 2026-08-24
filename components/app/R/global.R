03f2e20569bc1051fd5b0c5b338c43a411791f61
Ivo Kwee <ivo.kwee@gmail.com>
2026-08-24 18:17:55 +0200

    cleanup: remove commented-out future plan in global.R

--- components/app/R/global.R
+++ components/app/R/global.R
@@ -137,8 +137,7 @@ library(grid)
 library(magrittr)
 library(future)
 library(promises)
-future::plan(future::multicore) 
-##future::plan(future::multisession)  ## multisession often hangs!
+future::plan(future::multicore)
 
 source(file.path(APPDIR, "utils/utils.R"), local = TRUE)
 .opg_require_omicsai_catalog_api()
