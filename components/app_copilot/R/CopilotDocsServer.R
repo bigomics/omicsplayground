bbdeeab457bf8590305c39f50b24eaece93dbcf7
Ivo Kwee <ivo.kwee@gmail.com>
2026-08-30 02:44:42 +0200

    edit text

--- components/app_copilot/R/CopilotDocsServer.R
+++ components/app_copilot/R/CopilotDocsServer.R
@@ -87,7 +87,7 @@ CopilotDocsUI <- function(id) {
   shiny::div(
     class = "copilot-context-panel",
     style = "padding: 12px 0px;",
-    div("Obi knows your current dataset but here you can upload documents as extra context. You can augment or ask Obi to compare findings in the documents."),
+    div("Obi knows your current dataset but here you can upload additional documents as extra context. You can ask Obi to summarize or compare findings in the documents."),
     br(),
     br(),    
     shiny::fileInput(
